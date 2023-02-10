b1275d415bc80a512eba6fa86d97d2d67cbb74c1
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-10 17:10:52 +0300

    move to .h

--- shelkovnikov.dmitrii/S1/queue.h
+++ shelkovnikov.dmitrii/S1/queue.h
@@ -1,14 +1,46 @@
 #ifndef SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
 #define SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
 #include <cstddef>
+#include <stdexcept>
+namespace
+{
+  template< typename T >
+  T* expand(T *data, size_t old_size, size_t new_size)
+  {
+    if (new_size <= old_size)
+    {
+      throw std::logic_error("New size must be greater than old");
+    }
+    T *new_data = new T[new_size];
+    for (size_t i = 0; i < old_size; i++)
+    {
+      new_data[i] = data[i];
+    }
+    return new_data;
+  }
+}
 namespace dimkashelk
 {
   template < typename T >
   class Queue
   {
   public:
-    Queue();
-    void push(T rhs);
+    Queue():
+      size_(0),
+      capacity_(10),
+      data_(new T[capacity_])
+    {}
+    void push(T rhs)
+    {
+      if (size_ == capacity_)
+      {
+        T *new_data = expand< T >(data_, size_, capacity_ + 10);
+        delete[] data_;
+        data_ = new_data;
+        capacity_ += 10;
+      }
+      data_[size_++] = rhs;
+    }
     T drop();
   private:
     size_t size_;
