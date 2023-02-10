62fa1c71f0caa1686247a9641eae7b72c9e6ab4e
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-10 16:40:01 +0300

    add size and capacity

--- shelkovnikov.dmitrii/S1/queue.h
+++ shelkovnikov.dmitrii/S1/queue.h
@@ -1,5 +1,6 @@
 #ifndef SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
 #define SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
+#include <cstddef>
 namespace dimkashelk
 {
   template <typename T>
@@ -10,6 +11,8 @@ namespace dimkashelk
     void push(T rhs);
     T drop();
   private:
+    size_t size;
+    size_t capacity;
     T *data_;
   };
 }
