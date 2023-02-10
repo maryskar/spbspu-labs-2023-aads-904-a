55785b229e3262fa7609464f1cf825a297172318
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-10 16:35:06 +0300

    move class to dimkashelk namespace

--- shelkovnikov.dmitrii/S1/queue.h
+++ shelkovnikov.dmitrii/S1/queue.h
@@ -1,12 +1,16 @@
 #ifndef SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
 #define SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
-template< typename T >
-class Queue {
-public:
-  Queue();
-  void push(T rhs);
-  T drop();
-private:
-  T* data_;
-};
+namespace dimkashelk
+{
+  template <typename T>
+  class Queue
+  {
+  public:
+    Queue();
+    void push(T rhs);
+    T drop();
+  private:
+    T *data_;
+  };
+}
 #endif
