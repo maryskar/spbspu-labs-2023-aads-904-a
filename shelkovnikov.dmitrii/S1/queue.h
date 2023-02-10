76be6ffe85b841d73d1d62897dc9f139a014016d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-10 16:34:15 +0300

    create Queue class

---
+++ shelkovnikov.dmitrii/S1/queue.h
@@ -0,0 +1,12 @@
+#ifndef SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
+#define SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
+template< typename T >
+class Queue {
+public:
+  Queue();
+  void push(T rhs);
+  T drop();
+private:
+  T* data_;
+};
+#endif
