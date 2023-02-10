e38f2137c6120ded1769620d0d91af1bd65f6c13
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-10 17:30:16 +0300

    create expand.h

--- shelkovnikov.dmitrii/S1/queue.h
+++ shelkovnikov.dmitrii/S1/queue.h
@@ -2,23 +2,7 @@
 #define SPBSPU_LABS_2023_AADS_904_A_QUEUE_H
 #include <cstddef>
 #include <stdexcept>
-namespace
-{
-  template< typename T >
-  T* expand(T *data, size_t old_size, size_t new_size)
-  {
-    if (new_size <= old_size)
-    {
-      throw std::logic_error("New size must be greater than old");
-    }
-    T *new_data = new T[new_size];
-    for (size_t i = 0; i < old_size; i++)
-    {
-      new_data[i] = data[i];
-    }
-    return new_data;
-  }
-}
+#include "expand.h"
 namespace dimkashelk
 {
   template < typename T >
