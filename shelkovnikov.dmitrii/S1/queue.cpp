483d365c9f32bfc601a5a6cf49adc261853a0dcf
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-10 16:52:10 +0300

    add expand func

--- shelkovnikov.dmitrii/S1/queue.cpp
+++ shelkovnikov.dmitrii/S1/queue.cpp
@@ -1,4 +1,22 @@
 #include "queue.h"
+#include <stdexcept>
+namespace
+{
+  template< typename T >
+  T* expand(T *data, size_t old_size, size_t new_size)
+  {
+    if (new_size <= old_size)
+    {
+      throw std::logic_error("New size must be greater than old");
+    }
+    T *new_data = new T[new_size];
+    for (size_t i = 0; i < old_size; i++)
+    {
+      new_data[i] = data[i];
+    }
+    return new_data;
+  }
+}
 template< typename T>
 dimkashelk::Queue<T>::Queue():
   size_(0),
