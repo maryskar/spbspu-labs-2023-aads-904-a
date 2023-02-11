98e7bf30dc033384b5c67a40eaefd38a3269a803
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-12 01:14:25 +0300

    add copying constructor for Stack

---
+++ shelkovnikov.dmitrii/S1/getResultArithmeticExpression.h
@@ -0,0 +1,31 @@
+#ifndef T1_GETRESULTARITHMETICEXPRESSION_H
+#define T1_GETRESULTARITHMETICEXPRESSION_H
+#include "queue.h"
+#include "part.h"
+#include "stack.h"
+#include "functions.h"
+namespace dimkashelk
+{
+  template< typename T >
+  int getResultArithmeticExpression(Queue< T > polandExpression)
+  {
+    namespace dsk = dimkashelk;
+    dsk::Stack< dsk::part > remains;
+    while (!polandExpression.empty())
+    {
+      dsk::part p = polandExpression.drop();
+      if (p.isDigit_)
+      {
+        remains.push(p);
+      }
+      else
+      {
+        dsk::part p2 = remains.drop();
+        dsk::part p1 = remains.drop();
+        remains.push(dsk::makePart(dsk::getResult(p1.operand_, p2.operand_, p.operator_)));
+      }
+    }
+    return remains.drop().operand_;
+  }
+}
+#endif
