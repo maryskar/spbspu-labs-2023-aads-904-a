ae6c8034e2265e1ee14722146c8da6ab2e424171
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-12 00:54:17 +0300

    create getPolandArithmeticExpression.h

---
+++ shelkovnikov.dmitrii/S1/getPolandArithmeticExpression.h
@@ -0,0 +1,77 @@
+#ifndef SPBSPU_LABS_2023_AADS_904_A_GETPOLANDARITHMETICEXPRESSION_H
+#define SPBSPU_LABS_2023_AADS_904_A_GETPOLANDARITHMETICEXPRESSION_H
+#include <string>
+#include "queue.h"
+#include "part.h"
+#include "stack.h"
+#include "functions.h"
+namespace dimkashelk
+{
+  template< typename T >
+  Queue< T > getPolandArithmeticExpression(Queue< T > data)
+  {
+    Queue< part > queue;
+    Stack< part > stack;
+    while (!data.empty())
+    {
+      part p = data.drop();
+      if (!p.isDigit_)
+      {
+        if (p.operator_ == ')')
+        {
+          if (stack.empty())
+          {
+            throw std::logic_error("Check expression");
+          }
+          part p1 = stack.drop();
+          while (p1.operator_ != '(')
+          {
+            queue.push(p1);
+            if (stack.empty())
+            {
+              throw std::logic_error("Check expression");
+            }
+            p1 = stack.drop();
+          }
+        }
+        else if (stack.empty())
+        {
+          stack.push(p);
+        }
+        else
+        {
+          part p1 = stack.drop();
+          stack.push(p1);
+          if (isPriorityOperation(p1.operator_, p.operator_) || p1.operator_ == '(')
+          {
+            stack.push(p);
+          }
+          else
+          {
+            p1 = stack.drop();
+            while (!isPriorityOperation(p1.operator_, p.operator_))
+            {
+              queue.push(p1);
+              if (stack.empty())
+              {
+                break;
+              }
+              p1 = stack.drop();
+            }
+            stack.push(p);
+          }
+        }
+      }
+      else
+      {
+        queue.push(p);
+      }
+    }
+    while (!stack.empty())
+    {
+      queue.push(stack.drop());
+    }
+    return queue;
+  }
+}
+#endif
