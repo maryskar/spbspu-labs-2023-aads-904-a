85d67cbd9d8311ae18bf537dd918dbebeb464a25
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-12 00:52:24 +0300

    create getQueueOfArithmeticExpression.h

---
+++ shelkovnikov.dmitrii/S1/getQueueOfArithmeticExpression.h
@@ -0,0 +1,22 @@
+#ifndef SPBSPU_LABS_2023_AADS_904_A_GETQUEUEOFARITHMETICEXPRESSION_H
+#define SPBSPU_LABS_2023_AADS_904_A_GETQUEUEOFARITHMETICEXPRESSION_H
+#include <string>
+#include "queue.h"
+#include "part.h"
+#include "parser.h"
+namespace dimkashelk
+{
+  template< typename T >
+  Queue< T > getQueueOfArithmeticExpression(std::string element)
+  {
+    Parser parser(element);
+    Queue< part > data;
+    while (parser.hasNext())
+    {
+      data.push(makePart(parser()));
+    }
+    return data;
+  }
+
+}
+#endif
