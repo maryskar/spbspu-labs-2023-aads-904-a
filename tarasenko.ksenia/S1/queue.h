90fb36a5f14de5eb34babf9910ffd35d750a1ee5
xeniaTa <kseniatarasenko1@gmail.com>
2023-02-15 20:10:08 +0300

    start

---
+++ tarasenko.ksenia/S1/queue.h
@@ -0,0 +1,12 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+template< typename T>
+class Queue
+{
+public:
+ void push(T& rhs); // в конец
+ T& drop();  // из начала
+private:
+
+};
+#endif
---
+++ tarasenko.ksenia/S1/stack.h
@@ -0,0 +1,12 @@
+#ifndef STACK_H
+#define STACK_H
+template< typename T >
+class Stack
+{
+public:
+ void push(T& rhs); // наверх
+ T& drop(); // сверху
+private:
+
+};
+#endif
