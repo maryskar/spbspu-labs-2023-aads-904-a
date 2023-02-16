2f576a4810b1b71be308d32b47143e5a9b037c3f
xeniaTa <kseniatarasenko1@gmail.com>
2023-02-16 19:26:32 +0300

    realize queue

--- tarasenko.ksenia/S1/queue.h
+++ tarasenko.ksenia/S1/queue.h
@@ -6,10 +6,54 @@ template< typename T>
 class Queue
 {
 public:
- void push(T& rhs); // в конец
- T& drop();  // из начала
+ Queue();
+ ~Queue();
+ void push(T rhs);
+ T drop();
 private:
-  Node<T>* head;
+  Node< T >* head;
 };
+
+template< typename T >
+Queue< T >::Queue():
+ head(nullptr)
+{}
+
+template< typename T >
+Queue< T >::~Queue()
+{
+  while (head)
+  {
+    drop();
+  }
+}
+
+template< typename T >
+void Queue< T >::push(T rhs)
+{
+  if (head == nullptr)
+  {
+    head = new Node< T >(rhs);
+  }
+  else
+  {
+    Node< T >* current = head;
+    while (current->p_next != nullptr)
+    {
+      current = current->p_next;
+    }
+    current->p_next = new Node< T >(rhs);
+  }
+}
+
+template< typename T >
+T Queue< T >::drop()
+{
+  Node< T >* temp = head;
+  head = head->p_next;
+  T temp_val = temp->value;
+  delete temp;
+  return temp_val;
+}
 
 #endif
