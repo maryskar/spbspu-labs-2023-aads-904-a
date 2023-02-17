d2d7ca7f82014943831c462528b520cb3150060f
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-17 16:28:01 +0300

    update forwardlist.h

--- shelkovnikov.dmitrii/S2/forwardlist.h
+++ shelkovnikov.dmitrii/S2/forwardlist.h
@@ -1,6 +1,9 @@
-#ifndef S2_FORWARDLIST_H
-#define S2_FORWARDLIST_H
-class ForwardList
+#ifndef SPBSPU_LABS_2023_AADS_904_A_FORWARDLIST_H
+#define SPBSPU_LABS_2023_AADS_904_A_FORWARDLIST_H
+namespace dimkashelk
 {
-};
+  template < typename T >
+  class ForwardList
+  {};
+}
 #endif
