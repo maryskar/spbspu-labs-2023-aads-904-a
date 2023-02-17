925f5e8acda977466956ca48a5c56ae5e6231fa4
Artem <artrom170@gmail.com>
2023-02-17 20:09:17 +0300

    inappropriate params number case

--- romanovich.artem/S1/main.cpp
+++ romanovich.artem/S1/main.cpp
@@ -1,4 +1,10 @@
-int main()
+#include <iostream>
+int main(int argc, char **argv)
 {
+  if ((argc != 1) && (argc != 2))
+  {
+    std::cerr << "Not appropriate parameters number.\n";
+    return 1;
+  }
   return 0;
 }
