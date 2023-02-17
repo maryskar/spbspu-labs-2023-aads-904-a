c3cec8ea76e7f50fe2db4e44c6d31e089a955378
Artem <artrom170@gmail.com>
2023-02-17 20:48:41 +0300

    reading from file and std::cin

--- romanovich.artem/S1/main.cpp
+++ romanovich.artem/S1/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <fstream>
 int main(int argc, char **argv)
 {
   if ((argc != 1) && (argc != 2))
   {
     std::cerr << "Not appropriate parameters number.\n";
     return 1;
+  }
+  else if (argc == 2)
+  {
+    std::string fileIn = argv[1];
+    std::fstream fileInput(fileIn);
+    if (!fileInput.is_open())
+    {
+      std::cerr << "Error while opening file " << fileIn << ".\n";
+      return 1;
+    }
+    for (std::string line; std::getline(fileInput, line);)
+    {
+      std::cout << line << "\n";
+    }
+  } else {
+    for (std::string line; std::getline(std::cin, line);)
+    {
+      std::cout << line << "\n";
+    }
   }
   return 0;
 }
