669b40d7798a7628bac3842b41fe13b3d6f3a4ea
xeniaTa <kseniatarasenko1@gmail.com>
2023-02-18 12:44:29 +0300

    add Types

--- tarasenko.ksenia/S1/main.cpp
+++ tarasenko.ksenia/S1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+#include "fstream"
+#include "node.h"
+#include "queue.h"
+#include "stack.h"
 
-int main()
+union Types
 {
+  int operand;
+  char operation;
+};
 
+int main(int argc, char *argv[])
+{
+  Queue<Types> q;
+  Stack<Types> s;
+  std::string str = "";
+  std::fstream input(argv[1]);
+  // строка -> очередь(инфикс)
+  // (с помощью стека) берем элементы из очереди(инфикс.) и "собираем" очередь(постфикс)
+  // функция инфикс -> постфик
+  // (с помощью стека) вычисляем результат
+  // выводим результат
 }
