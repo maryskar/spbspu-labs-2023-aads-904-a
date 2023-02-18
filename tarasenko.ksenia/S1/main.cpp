8617bb7a267fa0c2cc0385083c3d5a8e0370e16e
xeniaTa <kseniatarasenko1@gmail.com>
2023-02-18 19:04:44 +0300

    realize infix in postfix

--- tarasenko.ksenia/S1/main.cpp
+++ tarasenko.ksenia/S1/main.cpp
@@ -17,10 +17,15 @@ struct Elem{
   Types union_elem;
 } elem;
 
+int getPriority(char op)
+{
+  // ...
+}
+
 int main(int argc, char *argv[])
 {
-  Queue<Types> q;
-  Stack<Types> s;
+  Queue<Elem> q_infix;
+  Stack<Elem> s;
   std::string str = "";
   std::fstream input(argv[1]);
   // строка -> очередь(инфикс)
@@ -40,22 +45,57 @@ int main(int argc, char *argv[])
           elem.union_elem.operation = str[i];
           elem.is_int = false;
         }
-        q.push(elem.union_elem);
+        q_infix.push(elem);
       }
     }
   }
   // (с помощью стека) берем элементы из очереди(инфикс.) и "собираем" очередь(постфикс)
-  while (!q.isEmpty())
+  Queue<Elem> q_postfix;
+  while (!q_infix.isEmpty())
   {
-    elem.union_elem = q.drop();
+    elem = q_infix.drop();
     if (elem.is_int)
     {
-      //"кладем" в очередь
+      q_postfix.push(elem);
     }
     else
     {
-      // ...
+      if (s.isEmpty())
+      {
+        s.push(elem);
+      }
+      else
+      {
+        if (elem.union_elem.operation == '(')
+        {
+          s.push(elem);
+        }
+        else if (elem.union_elem.operation == ')')
+        {
+          while (!s.isEmpty())
+          {
+            if (s.getTopElem().union_elem.operation == '(')
+            {
+              s.pop();
+              break;
+            }
+            q_postfix.push(s.drop());
+          }
+        }
+        else
+        {
+          while (!s.isEmpty() && (getPriority(elem.union_elem.operation) >= getPriority(s.getTopElem().union_elem.operation)))
+          {
+            q_postfix.push(s.drop());
+          }
+          s.push(elem);
+        }
+      }
     }
+  }
+  while (!s.isEmpty())
+  {
+    q_postfix.push(s.drop());
   }
   // (с помощью стека) вычисляем результат
   // выводим результат
