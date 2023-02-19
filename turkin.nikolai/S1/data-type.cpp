f07692395a44f559deb28e9cd1e852cc95a111ae
Lagushka1984 <nikolai.turkin.main@gmail.com>
2023-02-20 00:17:27 +0300

    data-type.h/.cpp - new data type for calc

---
+++ turkin.nikolai/S1/data-type.cpp
@@ -0,0 +1,34 @@
+#include "data-type.h"
+#include <stdexcept>
+
+calc_t::operator long long() const
+{
+  if (!isgigit)
+  {
+    throw std::runtime_error("isnt digit");
+  }
+  return calc.num;
+}
+
+calc_t::operator char() const
+{
+  if (isgigit)
+  {
+    throw std::runtime_error("isnt char");
+  }
+  return calc.sign;
+}
+
+calc_t calc_t::operator=(long long num)
+{
+  calc.num = num;
+  isgigit = true;
+  return * this;
+}
+
+calc_t calc_t::operator=(char sign)
+{
+  calc.sign = sign;
+  isgigit = false;
+  return * this;
+}
---
+++ turkin.nikolai/S1/data-type.h
@@ -0,0 +1,18 @@
+#ifndef DATA_TYPE
+#define DATA_TYPE
+
+struct calc_t
+{
+  operator long long() const;
+  operator char() const;
+  calc_t operator=(long long num);
+  calc_t operator=(char sign);
+  union
+  {
+    long long num;
+    char sign;
+  } calc;
+  bool isgigit;
+};
+
+#endif
