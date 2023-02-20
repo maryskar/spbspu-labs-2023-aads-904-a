231b156506cb26b767d7567ac4fd50c87319f20d
Chemodurovart <chemodurovart@gmail.com>
2023-02-20 10:22:25 +0300

    add realisation of constructor and destructor, changed names of class fields

--- chemodurov.artem/S1/stack.hpp
+++ chemodurov.artem/S1/stack.hpp
@@ -14,10 +14,23 @@ namespace chemodurov
     void pop();
     T & getLast();
    private:
-    T * value;
-    size_t size;
-    size_t capacity;
+    T * value_;
+    size_t size_;
+    size_t capacity_;
   };
+}
+
+template< typename T >
+chemodurov::Stack< T >::Stack():
+ value_(new T[1]),
+ size_(0),
+ capacity_(1)
+{}
+
+template< typename T >
+chemodurov::Stack< T >::~Stack()
+{
+  delete [] value_;
 }
 
 #endif
