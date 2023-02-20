2a6af2edc3bf8317aded72901fd2563dda659181
Chemodurovart <chemodurovart@gmail.com>
2023-02-20 12:30:28 +0300

    add interface for Queue

--- chemodurov.artem/S1/queue.hpp
+++ chemodurov.artem/S1/queue.hpp
@@ -1,4 +1,19 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
+#include <cstddef>
+
+namespace chemodurov
+{
+  template< typename T >
+  class Queue
+  {
+   public:
+    Queue(size_t capacity);
+    ~Queue();
+    void push(const T & rhs);
+    void pop();
+    T & getFromQueue();
+  };
+}
 
 #endif
