267d825023eaf06814e9158e9f118ad47c42e88e
xeniaTa <kseniatarasenko1@gmail.com>
2023-02-26 16:52:40 +0300

    fix func transformStringToInfixQueue

--- tarasenko.ksenia/S1/transformStringToInfixQueue.h
+++ tarasenko.ksenia/S1/transformStringToInfixQueue.h
@@ -1,9 +1,23 @@
 #ifndef TRANSFORMSTRINGTOINFIXQUEUE_H
 #define TRANSFORMSTRINGTOINFIXQUEUE_H
 #include <string>
-#include "queue.h"
 #include <cctype>
+#include <stdexcept>
+#include "queue.h"
 #include "elem.h"
+
+bool isdigit(std::string str)
+{
+  if (str.size() == 1)
+  {
+    if (std::isdigit(str[0])) return true;
+  }
+  else if (isdigit(str[0]) || (str[0] == '-' || str[0] == '+') && isdigit(str.erase(0,1)))
+  {
+    return true;
+  }
+  return false;
+}
 
 template< typename T >
 Queue< T > transformStringToInfixQueue(std::string str)
@@ -11,34 +25,31 @@ Queue< T > transformStringToInfixQueue(std::string str)
   Queue< T > q_infix;
   Elem elem;
   size_t i = 0;
+
   while (i < str.size())
   {
-    if (!std::isspace(str[i]))
+    std::string element = "";
+    while (!std::isspace(str[i]) && i < str.size())
     {
-      std::string digit = "";
-      if (!std::isdigit(str[i]))
-      {
-        elem.union_elem.operation = str[i];
-        elem.is_int = false;
-        i++;
-      }
-      else
-      {
-        do
-        {
-          digit += str[i];
-          i++;
-        }
-        while (std::isdigit(str[i]));
-        elem.union_elem.operand = std::stoi(digit);
-        elem.is_int = true;
-      }
-      q_infix.push(elem);
+      element += str[i];
+      i++;
+    }
+    if (isdigit(element))
+    {
+      elem.union_elem.operand = std::stoi(element);
+      elem.is_int = true;
+    }
+    else if (element.size() == 1 && element.find_first_of("()-+/*%") != std::string::npos)
+    {
+      elem.union_elem.operation = element[0];
+      elem.is_int = false;
     }
     else
     {
-      i++;
+      throw std::logic_error("Incorrect input");
     }
+    q_infix.push(elem);
+    i++;
   }
   return q_infix;
 }
