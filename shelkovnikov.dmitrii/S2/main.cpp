46c1f96fdd4d9c739de434f4a23d17a779cf6a40
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-02-27 19:20:25 +0300

    input of dict

--- shelkovnikov.dmitrii/S2/main.cpp
+++ shelkovnikov.dmitrii/S2/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <functional>
+#include <fstream>
+#include "dictionary.h"
+#include "forwardlist.h"
 int main(int argc, char *argv[])
 {
+  namespace dsk = dimkashelk;
   if (argc != 2)
   {
     std::cerr << "No filename";
     return 1;
+  }
+  dsk::ForwardList< dimkashelk::Dictionary< int, std::string, std::less< > > > list;
+  std::ifstream in(argv[1]);
+  if (!in.is_open())
+  {
+    std::cerr << "File not open";
+    return 1;
+  }
+  while (in)
+  {
+    std::string dict_name = "";
+    in >> dict_name;
+    if (!in)
+    {
+      std::cerr << "Check file";
+      return 1;
+    }
+    dsk::Dictionary< int, std::string, std::less< > > dict(std::less< int >);
+    int key = 0;
+    std::string value = "";
+    while (in)
+    {
+      in >> key >> value;
+    }
   }
 }
