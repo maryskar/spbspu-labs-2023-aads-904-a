fe801bc3da86c2a2f8dfb0965da69d19f9b1b885
KiraGelaz <kiragelaz@yandex.ru>
2023-03-02 18:14:28 +0300

    add methods of push and drop

--- kryuchkova.kira/S0/S1/queue.h
+++ kryuchkova.kira/S0/S1/queue.h
@@ -47,7 +47,38 @@ Queue< T >::~Queue()
 template< typename T >
 void Queue< T >::push(T rhs)
 {
-  
+  Node< T > *new_node = new Node< T >(rhs);
+  if (head_ == nullptr)
+  {
+    head_ = new_node;
+    tail_ = new_node;
+  }
+  else
+  {
+    new_node->next_ = tail_;
+    tail_ = new_node;
+  }
+}
+
+template< typename T >
+T Queue< T >::drop()
+{
+  Node< T > *current = tail_;
+  if (head_ == nullptr)
+  {
+    throw std::logic_error("Empty queue");
+  }
+  else
+  {
+    while (current->next_ != head_)
+    {
+      current = current->next;
+    }
+    T data = head_->data_;
+    head_ = current;
+    delete current->next_;
+    return data;
+  }
 }
 
 #endif
