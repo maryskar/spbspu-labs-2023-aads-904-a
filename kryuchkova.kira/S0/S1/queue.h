97405a22a2b495d6de2c5c344bdd36707ca5e018
KiraGelaz <kiragelaz@yandex.ru>
2023-03-02 03:26:55 +0300

    add methods to queue

--- kryuchkova.kira/S0/S1/queue.h
+++ kryuchkova.kira/S0/S1/queue.h
@@ -5,13 +5,49 @@ template< typename T >
 class Queue
 {
   public:
-    Queue():
-      begin_(nullptr),
-      end_(nullptr)
-    {}
-    Queue()
+    Queue();
+    Queue(const Queue< T > &queue);
+    ~Queue();
     void push(T rhs);
     T drop();
+    bool isEmpty();
+  private:
+    Node< T > *head_;
+    Node< T > *tail_;
 };
+
+template< typename T >
+Queue< T >::Queue():
+  head_(nullptr),
+  tail_(nullptr)
+{}
+
+template< typename T >
+Queue< T >::Queue(const Queue< T > &queue):
+  head_(nullptr),
+  tail_(nullptr)
+{
+  Node< T > *first = queue.head_;
+  while (first)
+  {
+    push(first->data);
+    first = first->next_;
+  }
+}
+
+template< typename T >
+Queue< T >::~Queue()
+{
+  while (head_)
+  {
+    drop();
+  }
+}
+
+template< typename T >
+void Queue< T >::push(T rhs)
+{
+  
+}
 
 #endif
