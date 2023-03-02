cd40b0f4784d54d802211ddf4dd2cd06618dfb60
KiraGelaz <kiragelaz@yandex.ru>
2023-03-02 20:59:39 +0300

    add namespace and empty check

--- kryuchkova.kira/S0/S1/queue.h
+++ kryuchkova.kira/S0/S1/queue.h
@@ -1,83 +1,93 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include "node.h"
 
-template< typename T >
-class Queue
+namespace gelaz
 {
-  public:
-    Queue();
-    Queue(const Queue< T > &queue);
-    ~Queue();
-    void push(T rhs);
-    T drop();
-    bool isEmpty();
-  private:
-    Node< T > *head_;
-    Node< T > *tail_;
-};
+  template< typename T >
+  class Queue
+  {
+    public:
+      Queue();
+      Queue(const Queue< T > &queue);
+      ~Queue();
+      void push(T rhs);
+      T drop();
+      bool isEmpty();
+    private:
+      Node< T > *head_;
+      Node< T > *tail_;
+  };
 
-template< typename T >
-Queue< T >::Queue():
-  head_(nullptr),
-  tail_(nullptr)
-{}
+  template< typename T >
+  Queue< T >::Queue():
+    head_(nullptr),
+    tail_(nullptr)
+  {}
 
-template< typename T >
-Queue< T >::Queue(const Queue< T > &queue):
-  head_(nullptr),
-  tail_(nullptr)
-{
-  Node< T > *first = queue.head_;
-  while (first)
+  template< typename T >
+  Queue< T >::Queue(const Queue< T > &queue):
+    head_(nullptr),
+    tail_(nullptr)
   {
-    push(first->data);
-    first = first->next_;
-  }
-}
-
-template< typename T >
-Queue< T >::~Queue()
-{
-  while (head_)
-  {
-    drop();
-  }
-}
-
-template< typename T >
-void Queue< T >::push(T rhs)
-{
-  Node< T > *new_node = new Node< T >(rhs);
-  if (head_ == nullptr)
-  {
-    head_ = new_node;
-    tail_ = new_node;
-  }
-  else
-  {
-    new_node->next_ = tail_;
-    tail_ = new_node;
-  }
-}
-
-template< typename T >
-T Queue< T >::drop()
-{
-  Node< T > *current = tail_;
-  if (head_ == nullptr)
-  {
-    throw std::logic_error("Empty queue");
-  }
-  else
-  {
-    while (current->next_ != head_)
+    Node< T > *first = queue.head_;
+    while (first)
     {
-      current = current->next;
+      push(first->data);
+      first = first->next_;
     }
-    T data = head_->data_;
-    head_ = current;
-    delete current->next_;
-    return data;
+  }
+
+  template< typename T >
+  Queue< T >::~Queue()
+  {
+    while (head_)
+    {
+     drop();
+    }
+  }
+
+  template< typename T >
+  void Queue< T >::push(T rhs)
+  {
+    Node< T > *new_node = new Node< T >(rhs);
+    if (head_ == nullptr)
+    {
+      head_ = new_node;
+      tail_ = new_node;
+    }
+    else
+    {
+     new_node->next_ = tail_;
+     tail_ = new_node;
+    }
+  }
+
+  template< typename T >
+  T Queue< T >::drop()
+  {
+    Node< T > *current = tail_;
+    if (head_ == nullptr)
+    {
+      throw std::logic_error("Empty queue");
+    }
+    else
+    {
+     while (current->next_ != head_)
+     {
+       current = current->next;
+     }
+      T data = head_->data_;
+      head_ = current;
+      delete current->next_;
+      return data;
+    }
+  }
+
+  template< typename T >
+  bool Queue< T >::isEmpty()
+  {
+   return head_ == nullptr;
   }
 }
 
