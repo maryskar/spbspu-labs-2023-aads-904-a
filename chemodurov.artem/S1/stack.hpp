b8d59d89826924df082b1da34f079516cfde53e1
Chemodurovart <chemodurovart@gmail.com>
2023-03-04 23:07:16 +0300

    change realisation of Stack (push(), pop(), getFromStack())

--- chemodurov.artem/S1/stack.hpp
+++ chemodurov.artem/S1/stack.hpp
@@ -39,53 +39,30 @@ chemodurov::Stack< T >::~Stack()
 template< typename T >
 void chemodurov::Stack< T >::push(const T & rhs)
 {
-  if (capacity_ == size_)
-  {
-    T * new_value = new T[capacity_ + 1];
-    try
-    {
-      for (size_t i = 0; i < size_; ++i)
-      {
-        new_value[i] = value_[i];
-      }
-      value_[size_] = rhs;
-    }
-    catch (...)
-    {
-      delete [] new_value;
-      throw;
-    }
-    delete [] value_;
-    value_ = new_value;
-    ++capacity_;
-    ++size_;
-  }
-  else
-  {
-    value_[size_] = rhs;
-    ++size_;
-  }
-}
-
-template< typename T >
-void chemodurov::Stack< T >::pop()
-{
-  if (!size_)
-  {
-    throw std::logic_error("Empty stack");
-  }
-  value_[size_ - 1] = T();
-  --size_;
+  List< T > * temp = new List< T >{rhs, head_};
+  head_ = temp;
 }
 
 template< typename T >
 T & chemodurov::Stack< T >::getFromStack()
 {
-  if (!size_)
+  if (!head_)
   {
     throw std::logic_error("Empty stack");
   }
-  return *value_;
+  return *head_;
+}
+
+template< typename T >
+void chemodurov::Stack< T >::pop()
+{
+  if (!head_)
+  {
+    throw std::logic_error("Empty stack");
+  }
+  List< T > * temp = head_->next;
+  delete head_;
+  head_ = temp;
 }
 
 #endif
