6f40dc77102ffea59bfe517defe6266ed380a3cb
Artem <artrom170@gmail.com>
2023-03-04 22:15:09 +0300

    attempt to check pipelines

--- romanovich.artem/S1/main.cpp
+++ romanovich.artem/S1/main.cpp
@@ -3,7 +3,126 @@
 #include "stack.h"
 #include "queue.h"
 #include "node.h"
-#include "notations.h"
+///#include "notations.h"
+//
+//
+//
+//
+//
+//
+//
+bool isOperator(const std::string &c)
+{
+  return (c == "+" || c == "-" || c == "*" || c == "/" || c == "%");
+}
+bool isDigit(const std::string &str)
+{
+  return std::all_of(str.begin(), str.end(), ::isdigit);
+}
+bool operationStackPopCondition(const std::string &q, const std::string &s)
+{
+  return ((q == "+" || q == "-") && (s == "*" || s == "/" || s == "%"));
+}
+Queue< std::string > getPostfixFromInfix(Queue< std::string > queue)
+{
+  Queue< std::string > postfixQueue;
+  Stack< std::string > *stack = new Stack< std::string >;
+  while (!queue.isEmpty() or !stack->isEmpty())
+  {
+    if (!queue.isEmpty())
+    {
+      std::string qEl = queue.pop();
+      if (qEl == "(")
+      {
+        stack->push(qEl);
+      }
+      if (qEl == ")")
+      {
+        while (stack->top_->data_ != "(")
+        {
+          postfixQueue.push(stack->pop());
+          if (stack->isEmpty())
+          {
+            break;
+          }
+        }
+        stack->pop();
+      }
+      if (std::all_of(qEl.begin(), qEl.end(), ::isdigit))
+      {
+        postfixQueue.push(qEl);
+      }
+      if (isOperator(qEl))
+      {
+        if (!stack->isEmpty())
+        {
+          if (operationStackPopCondition(qEl, stack->top_->data_))
+          {
+            postfixQueue.push(stack->pop());
+          }
+        }
+        stack->push(qEl);
+      }
+    }
+    else
+    {
+      while (!stack->isEmpty())
+      {
+        postfixQueue.push(stack->pop());
+      }
+    }
+    //std::cout << "queue: ";
+    //queue.print();
+    //std::cout << "stack: ";
+    //stack->print();
+    //std::cout << "result: ";
+    //postfixQueue.print();
+    //std::cout << "\n";
+  }
+  delete stack;
+  return postfixQueue;
+}
+Queue< std::string > splitLine(const std::string &string)
+{
+  Queue< std::string > queue;
+  int begin = 0;
+  int end = string.find(' ');
+  while (end != -1)
+  {
+    queue.push(string.substr(begin, end - begin));
+    begin = end + 1;
+    end = string.find(' ', begin);
+  }
+  queue.push(string.substr(begin, end - begin));
+  return queue;
+}
+std::string doOperation(long long b, long long a, const std::string &oper)
+{
+  if (oper == "+")
+  {
+    return std::to_string(a + b);
+  }
+  if (oper == "-")
+  {
+    return std::to_string(a - b);
+  }
+  if (oper == "*")
+  {
+    return std::to_string(a * b);
+  }
+  if (oper == "/")
+  {
+    return std::to_string(a / b);
+  }
+  return std::to_string(a % b);
+}
+//
+//
+//
+//
+//
+//
+//
 int main(int argc, char **argv)
 {
   if ((argc != 1) && (argc != 2))
--- romanovich.artem/S1/notations.h
+++ romanovich.artem/S1/notations.h
@@ -3,9 +3,9 @@
 #include "queue.h"
 bool isOperator(const std::string &c);
 bool operationStackPopCondition(const std::string &q, const std::string &s);
-Queue< std::string > getPostfixFromInfix(Queue< std::string > queue);
-std::basic_string< char > calcPostfixExpression(Queue< std::string > postfixQueue);
-Queue< std::string > splitLine(const std::string &string);
-bool isDigit(const std::string &str);
-std::string doOperation(long long a, long long b, const std::string &oper);
+//Queue< std::string > getPostfixFromInfix(Queue< std::string > queue);
+//std::basic_string< char > calcPostfixExpression(Queue< std::string > postfixQueue);
+//Queue< std::string > splitLine(const std::string &string);
+//bool isDigit(const std::string &str);
+//std::string doOperation(long long a, long long b, const std::string &oper);
 #endif
