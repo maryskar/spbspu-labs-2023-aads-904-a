c0ee6aad2025e11b3178ce5e27cb8d3bbc04942e
Chemodurovart <chemodurovart@gmail.com>
2023-03-04 21:40:26 +0300

    add realisation for Queue()

--- chemodurov.artem/S1/queue.hpp
+++ chemodurov.artem/S1/queue.hpp
@@ -1,6 +1,7 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 #include <cstddef>
+#include "list.hpp"
 
 namespace chemodurov
 {
@@ -8,12 +9,21 @@ namespace chemodurov
   class Queue
   {
    public:
-    Queue(size_t capacity);
+    Queue();
     ~Queue();
     void push(const T & rhs);
     void pop();
     T & getFromQueue();
+   private:
+    List< T > * head_;
+    size_t size_;
   };
 }
+
+template< typename T >
+chemodurov::Queue< T >::Queue():
+ head_(nullptr),
+ size_(0)
+{}
 
 #endif
