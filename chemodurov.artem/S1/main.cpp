8b80dd9cc89ae6963a97f273d6ca9abb857bd65f
Chemodurovart <chemodurovart@gmail.com>
2023-03-04 23:24:01 +0300

    fixed checking argc

--- chemodurov.artem/S1/main.cpp
+++ chemodurov.artem/S1/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char ** argv)
 {
-  if (argc != 1 || argc != 2)
+  if (argc != 1 && argc != 2)
   {
     std::cerr << "Error\n";
     return 1;
+  }
+  if (argc == 1)
+  {
+
+  }
+  else
+  {
+
   }
   return 0;
 }
