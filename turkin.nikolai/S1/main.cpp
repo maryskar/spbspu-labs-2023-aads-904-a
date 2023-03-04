17b12e3494a8a1616d478af86cd5cad74149150f
Lagushka1984 <nikolai.turkin.main@gmail.com>
2023-03-04 16:21:54 +0300

    main.cpp - skip empty lines

--- turkin.nikolai/S1/main.cpp
+++ turkin.nikolai/S1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <exception>
 #include <queue.h>
 #include <stack.h>
 #include "data-type.h"
@@ -8,55 +9,40 @@
 
 int main(int argc, char * argv[])
 {
-  Queue< calc_t > input;
-  Queue< calc_t > output;
-  std::string dirt, temp;
-  calc_t data;
   if (argc == 2)
   {
-    std::fstream file(argv[1]);
+    std::ifstream file(argv[1]);
     if (!file.is_open())
     {
       std::cerr << "cannot open file\n";
       return 1;
     }
-    std::getline(file, dirt);
+    std::cin.rdbuf(file.rdbuf());
   }
-  else
-  {
-    std::getline(std::cin, dirt);
-  }
-  dirt += '\n';
-  for (auto symbol : dirt)
-  {
-    if (symbol == ' ' || symbol == '\n')
-    {
-      try
-      {
-        data = std::stoll(temp);
-      }
-      catch (...)
-      {
-        data = temp.c_str()[0];
-      }
-      input.push(data);
-      temp = "";
-    }
-    else
-    {
-      temp += symbol;
-    }
-  }
+
+  Queue< calc_t > input, output;
+  std::string dirt;
+  long long result = 0;
   try
   {
-    long long result = 0;
-    inf2Post(input, output);
-    post2Result(output, result);
-    std::cout << result << "\n";
+    while (std::cin)
+    {
+      std::getline(std::cin, dirt);
+      if (!dirt.length())
+      {
+        continue;
+      }
+      dirt += "\n";
+      str2Inf(dirt, input);
+      inf2Post(input, output);
+      post2Result(output, result);
+      std::cout << result << " ";
+    }
+    std::cout << "\n";
   }
-  catch (...)
+  catch (const std::exception & error)
   {
-    std::cerr << "ERROR AAAAAAAAAAAA\n";
+    std::cerr << error.what() << "\n";
     return 1;
   }
   return 0;
