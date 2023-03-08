6f0d19dc21a5afec9ce5eef4564ab2dd418356cf
xeniaTa <kseniatarasenko1@gmail.com>
2023-03-08 12:11:40 +0300

    fix func calculate

---
+++ tarasenko.ksenia/S1/calculate.cpp
@@ -0,0 +1,24 @@
+#include "calculate.h"
+#include "mathfunc.h"
+#include <stdexcept>
+
+long long tarasenko::calculate(long long a, long long b, char op)
+{
+  switch (op)
+  {
+    case '+':
+      return summarize(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+    {
+      return divide(a, b);
+    }
+    case '%':
+      return mod(a, b);
+    default:
+      throw std::invalid_argument("Incorrect input");
+  }
+}
--- tarasenko.ksenia/S1/calculate.h
+++ tarasenko.ksenia/S1/calculate.h
@@ -1,27 +1,7 @@
 #ifndef CALCULATE_H
 #define CALCULATE_H
-#include "mathfunc.h"
 namespace tarasenko
 {
-  long long calculate(long long a, long long b, char op)
-  {
-    switch (op)
-    {
-      case '+':
-        return summarize(a, b);
-      case '-':
-        return subtract(a, b);
-      case '*':
-        return multiply(a, b);
-      case '/':
-      {
-        return divide(a, b);
-      }
-      case '%':
-        return mod(a, b);
-      default:
-        throw std::invalid_argument("Incorrect input");
-    }
-  }
+  long long calculate(long long a, long long b, char op);
 }
 #endif
