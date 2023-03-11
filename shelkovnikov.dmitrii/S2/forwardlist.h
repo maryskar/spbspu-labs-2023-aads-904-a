a324bf71eaea40a3de6ee7f75d025b90bc1fdf89
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-03-11 13:46:30 +0300

    override operator>

--- shelkovnikov.dmitrii/S2/forwardlist.h
+++ shelkovnikov.dmitrii/S2/forwardlist.h
@@ -189,6 +189,27 @@ namespace dimkashelk
     {
       return Iterator(nullptr);
     }
+    friend bool operator>(const ForwardList< T > &first, const ForwardList< T > &second)
+    {
+      auto first_start = first.begin();
+      auto first_end = first.end();
+      auto second_start = second.begin();
+      auto second_end = second.end();
+      while (first_start != first_end && second_start != second_end && (*first_start) == (*second_start))
+      {
+        first_start++;
+        second_start++;
+      }
+      if (first_start != first_end)
+      {
+        if (second_start != second_end)
+        {
+          return (*first_start) > (*second_start);
+        }
+        return true;
+      }
+      return second_start != second_end;
+    }
   private:
     details::Node< T > *begin_;
     details::Node< T > *end_;
