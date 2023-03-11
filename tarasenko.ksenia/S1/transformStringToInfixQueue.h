53d0cda39292108feff9ab821d3f1bd10c09267a
xeniaTa <kseniatarasenko1@gmail.com>
2023-03-11 14:00:22 +0300

    fix func transformStringToInfixQueue rm func isDigit

--- tarasenko.ksenia/S1/transformStringToInfixQueue.h
+++ tarasenko.ksenia/S1/transformStringToInfixQueue.h
@@ -2,27 +2,10 @@
 #define TRANSFORMSTRINGTOINFIXQUEUE_H
 #include <string>
 #include <cctype>
-#include <stdexcept>
 #include "queue.h"
 #include "elem.h"
 namespace tarasenko
 {
-  bool isDigit(std::string str)
-  {
-    if (str.size() == 1)
-    {
-      if (std::isdigit(str[0]))
-      {
-        return true;
-      }
-    }
-    else if ((std::isdigit(str[0]) || str[0] == '-' || str[0] == '+') && isDigit(str.erase(0, 1)))
-    {
-      return true;
-    }
-    return false;
-  }
-
   Queue< Elem > transformStringToInfixQueue(std::string str)
   {
     Queue< Elem > q_infix;
@@ -35,12 +18,7 @@ namespace tarasenko
         element += str[i];
         i++;
       }
-      if (isDigit(element))
-      {
-        Elem elem(std::stoll(element));
-        q_infix.push(elem);
-      }
-      else if (element.size() == 1 && element.find_first_of("()-+/*%") != std::string::npos)
+      if (element.size() == 1 && element.find_first_of("()-+/*%") != std::string::npos)
       {
         Operation op(element[0]);
         Elem elem(op);
@@ -48,7 +26,8 @@ namespace tarasenko
       }
       else
       {
-        throw std::logic_error("Incorrect input");
+        Elem elem(std::stoll(element));
+        q_infix.push(elem);
       }
       i++;
     }
