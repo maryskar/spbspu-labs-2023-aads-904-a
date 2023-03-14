b413d0700ec426950c1bf8c8488ab8c42e259385
xeniaTa <kseniatarasenko1@gmail.com>
2023-03-14 10:09:45 +0300

    add operator ++

--- tarasenko.ksenia/S2/forward_list_iterator.h
+++ tarasenko.ksenia/S2/forward_list_iterator.h
@@ -1,26 +1,45 @@
 #ifndef FORWARD_LIST_ITERATOR_H
 #define FORWARD_LIST_ITERATOR_H
 #include "forward_list.h"
+namespace tarasenko
+{
+  template< typename T >
+  class ForwardListIterator {
+  public:
+   using this_t = ForwardListIterator< T >;
 
-template< typename T >
-struct ForwardListIterator {
-  ForwardList< T > * node;
-  using this_t = ForwardListIterator< T >;
+   ForwardListIterator(): node(nullptr) {}
+   ~ForwardListIterator() = default;
+   ForwardListIterator(const this_t &) = default;
+   this_t & operator=(const this_t &) = default;
 
-  ForwardListIterator(): node(nullptr) {}
-  ~ForwardListIterator() = default;
-  ForwardListIterator(const this_t &) = default;
-  this_t & operator=(const this_t &) = default;
+   this_t & operator++();
+   this_t operator++(int);
 
-  this_t & operator++();
-  this_t operator++(int);
+   T & operator*(); //+ const-версия
+   T * operator->(); //+ const-версия
 
-  T & operator*(); //+ const-версия
-  T * operator->(); //+ const-версия
+   bool operator!=(const this_t &) const;
+   bool operator==(const this_t &) const;
 
-  bool operator!=(const this_t &) const;
-  bool operator==(const this_t &) const;
-};
+  private:
+   ForwardList< T > * node;
+  };
 
+  template< typename T >
+  ForwardListIterator< T >::this_t & ForwardListIterator< T >::operator++()
+  {
+    assert(node != nullptr);
+    node = node->next;
+  }
 
+  template< typename T >
+  ForwardListIterator< T >::this_t ForwardListIterator< T >::operator++(int)
+  {
+    assert(node != nullptr);
+    this_t result(*this);
+    ++(*this);
+    return result;
+  }
+}
 #endif
