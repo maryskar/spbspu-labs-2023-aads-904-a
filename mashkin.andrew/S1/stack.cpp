0c67de2d706c43040d82b79e4571260dfd7164bd
Машкин Андрей <beedzibood@gmail.com>
2023-03-15 09:00:05 +0300

    defined method push

--- mashkin.andrew/S1/stack.cpp
+++ mashkin.andrew/S1/stack.cpp
@@ -1,11 +1,22 @@
 #include "stack.h"
 #include <cstddef>
+#include <stdexcept>
 
 template< typename T >
 Stack< T >::Stack():
-  stack_(0),
-  top_{0}
+  stack_{0},
+  top_(0)
 {}
+
+template< typename T >
+void Stack< T >::push(T rhs)
+{
+  if (top_ == OVERFLOW)
+  {
+    throw std::overflow_error("Stackoverflow");
+  }
+  stack_[top_++] = rhs;
+}
 
 template< typename T >
 bool Stack< T >::isEmpty() const
