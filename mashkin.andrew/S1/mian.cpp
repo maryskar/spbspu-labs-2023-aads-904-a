fd20802565f802069b34665bc15c7b572c4e1e94
Машкин Андрей <beedzibood@gmail.com>
2023-03-15 08:12:49 +0300

    added arguments to main and have done checking to correct quantity of argc

--- mashkin.andrew/S1/mian.cpp
+++ mashkin.andrew/S1/mian.cpp
@@ -1,4 +1,12 @@
-int main()
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char ** argv)
 {
+  if (argc != 2 || argc != 3)
+  {
+    std::cerr << "Incorrect arguments\n";
+    return 1;
+  }
   return 0;
 }
