9b6aa5bebdbf08aff079e1377a6e14bcc74e8d89
Машкин Андрей <beedzibood@gmail.com>
2023-03-15 08:36:42 +0300

    declared some new methods and 1 constructor

--- mashkin.andrew/S1/stack.h
+++ mashkin.andrew/S1/stack.h
@@ -1,12 +1,18 @@
 #ifndef STACK_H
 #define STACK_H
+#include <cstddef>
 
 template< typename T >
 class Stack
 {
 public:
+  explicit Stack();
   void push(T rhs);
   void pop();
+  bool isEmpty() const;
   T drop();
+private:
+  T* stack_;
+  size_t top_[50];
 };
 #endif
