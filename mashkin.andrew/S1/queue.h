46d9299ef98378ab9ae7b2846a08591755756724
Машкин Андрей <beedzibood@gmail.com>
2023-03-15 09:15:39 +0300

    declared a couple more methods and declared constructor

--- mashkin.andrew/S1/queue.h
+++ mashkin.andrew/S1/queue.h
@@ -1,12 +1,20 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <cstddef>
 
 template< typename T >
 class Queue
 {
 public:
+  explicit Queue();
   void enqueue(T rhs);
   void dequeue();
   T drop();
+  bool isEmpty() const;
+private:
+  static const size_t OVERFLOW = 50;
+  T* queue_[OVERFLOW];
+  size_t head_;
+  size_t tail_;
 };
 #endif
