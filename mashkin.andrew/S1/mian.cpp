1441a5640a368a8b6c595e011cc06ae31625a45f
Машкин Андрей <beedzibood@gmail.com>
2023-03-15 23:02:52 +0300

    added checking if arguments equal three

--- mashkin.andrew/S1/mian.cpp
+++ mashkin.andrew/S1/mian.cpp
@@ -8,5 +8,23 @@ int main(int argc, char ** argv)
     std::cerr << "Incorrect arguments\n";
     return 1;
   }
+  else if (argc == 3)
+  {
+    std::ifstream inpFile;
+    inpFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+    try
+    {
+      inpFile.open(argv[2]);
+    }
+    catch (const std::exception& ex)
+    {
+      std::cerr << ex.what() << "\n";
+      return 1;
+    }
+  }
+  else
+  {
+
+  }
   return 0;
 }
