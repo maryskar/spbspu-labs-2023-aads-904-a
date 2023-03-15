177e962523b3b6da42914038303da33cb593f770
Машкин Андрей <beedzibood@gmail.com>
2023-03-15 08:37:31 +0300

    defined constructor and method isEmpty

--- mashkin.andrew/S1/stack.cpp
+++ mashkin.andrew/S1/stack.cpp
@@ -1,1 +1,14 @@
 #include "stack.h"
+#include <cstddef>
+
+template< typename T >
+Stack< T >::Stack():
+  stack_(0),
+  top_{0}
+{}
+
+template< typename T >
+bool Stack< T >::isEmpty() const
+{
+  return top_;
+}
