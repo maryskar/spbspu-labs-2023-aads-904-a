7f6f7a779525670bd28e1e41c898bae4e65e5262
Машкин Андрей <beedzibood@gmail.com>
2023-03-16 10:35:29 +0300

    added 2 block catch and function convertToPostfixNotation also include stack.h and queue.h

--- mashkin.andrew/S1/mian.cpp
+++ mashkin.andrew/S1/mian.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
+#include "convertToPostfixNotation.h"
+#include "stack.h"
+#include "queue.h"
 
 int main(int argc, char ** argv)
 {
@@ -10,11 +14,24 @@ int main(int argc, char ** argv)
   }
   else if (argc == 3)
   {
+    Stack< char > stc;
+    Queue< char > que;
     std::ifstream inpFile;
     inpFile.exceptions(std::ifstream::badbit | std::ifstream::failbit);
     try
     {
       inpFile.open(argv[2]);
+      convertToPostfixNotation(inpFile, stc, que);
+    }
+    catch (const std::overflow_error& ex)
+    {
+      std::cerr << ex.what() << "\n";
+      return 1;
+    }
+    catch (const std::underflow_error& ex)
+    {
+      std::cerr << ex.what() << "\n";
+      return 1;
     }
     catch (const std::exception& ex)
     {
