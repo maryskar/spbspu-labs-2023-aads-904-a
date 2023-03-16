718f61e7ce96764549aeb3cc52a808038b48b2ef
Chemodurovart <chemodurovart@gmail.com>
2023-03-16 23:55:06 +0300

    i think reading is correct now

--- chemodurov.artem/S1/read-infix-expr.cpp
+++ chemodurov.artem/S1/read-infix-expr.cpp
@@ -1,56 +1,49 @@
 #include "read-infix-expr.hpp"
 #include <string>
 #include <stdexcept>
-#include <iostream>
 
-chemodurov::InfixExpr * chemodurov::readInfixExpr(std::istream & in, size_t & inf_size)
+chemodurov::InfixExpr * chemodurov::readInfixExpr(std::string & line, size_t & inf_size)
 {
-  std::string line;
-  std::getline(in, line);
-  if (!in)
-  {
-    throw std::invalid_argument("Nothing to read");
-  }
   chemodurov::InfixExpr * inf = new chemodurov::InfixExpr[line.size() / 2 + 1];
   inf_size = 0;
   size_t size1 = 0;
   size_t size2 = 0;
-  while (size2 < line.size())
+  while (size2 < line.size() - 1)
   {
     if (line[size2] == '(')
     {
       inf[inf_size++] = {chemodurov::BRACE_LEFT, false, false};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == ')')
     {
       inf[inf_size++] = {chemodurov::BRACE_RIGHT, false, false};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == '+')
     {
       inf[inf_size++] = {chemodurov::OPERATION_PLUS, false, true};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == '-')
     {
       inf[inf_size++] = {chemodurov::OPERATION_MINUS, false, true};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == '*')
     {
       inf[inf_size++] = {chemodurov::OPERATION_MULTIPLICATION, false, true};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == '/')
     {
       inf[inf_size++] = {chemodurov::OPERATION_DIVIDE, false, true};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == '%')
     {
       inf[inf_size++] = {chemodurov::OPERATION_REMINDER_OF_DIVISION, false, true};
-      ++size2;
+      size2 += 2;
     }
     else if (line[size2] == ' ')
     {
@@ -60,7 +53,7 @@ chemodurov::InfixExpr * chemodurov::readInfixExpr(std::istream & in, size_t & in
     {
       try
       {
-        int a = std::stoi(line, std::addressof(size1));
+        int a = std::stoi(line.substr(size2), std::addressof(size1));
         size1 += size2;
         size2 = size1;
         inf[inf_size++] = {a, true, false};
