c082738f6238b44f9788e3207cbf9157f664bd79
Chemodurovart <chemodurovart@gmail.com>
2023-03-17 10:00:32 +0300

    add operation-and-brace.hpp

---
+++ chemodurov.artem/S1/operation-and-brace.hpp
@@ -0,0 +1,22 @@
+#ifndef OPERATION_AND_BRACE_HPP
+#define OPERATION_AND_BRACE_HPP
+
+namespace chemodurov
+{
+  enum Operation
+  {
+    OPERATION_PLUS = '+',
+    OPERATION_MINUS = '-',
+    OPERATION_MULTIPLICATION = '*',
+    OPERATION_DIVIDE = '/',
+    OPERATION_REMINDER_OF_DIVISION = '%'
+  };
+
+  enum Brace
+  {
+    BRACE_LEFT = '(',
+    BRACE_RIGHT = ')'
+  };
+}
+
+#endif
