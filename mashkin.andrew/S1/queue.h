27f89548cd823fa45cc8735ab27cd8761f416da7
Машкин Андрей <beedzibood@gmail.com>
2023-03-17 18:49:04 +0300

    deleted stack.cpp, queue.cpp and replaced definition of all that was there in stack.h and queue.h

--- mashkin.andrew/S1/queue.h
+++ mashkin.andrew/S1/queue.h
@@ -1,19 +1,58 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 #include <cstddef>
+#include <stdexcept>
 #include "list.h"
 
 template< typename T >
 class Queue
 {
 public:
-  explicit Queue(T symb);
+  explicit Queue();
   void enqueue(T rhs);
   void dequeue();
   T drop();
 
 private:
-  list_t< T > head_;
-  list_t< T > tail_;
+  list_t< T >* head_;
+  list_t< T >* tail_;
 };
+
+template< typename T >
+Queue< T >::Queue():
+  head_(nullptr),
+  tail_(head_)
+{
+}
+
+template< typename T >
+void Queue< T >::enqueue(T rhs)
+{
+  if (!tail_->data)
+  {
+    tail_ = new list_t< T >{rhs, nullptr};
+  }
+  tail_->next = new list_t< T >{rhs, nullptr};
+  tail_ = tail_->next;
+}
+
+template< typename T >
+void Queue< T >::dequeue()
+{
+  if (tail_ == head_)
+  {
+    throw std::underflow_error("Queue underflow");
+  }
+  list_t< T > var = head_->next;
+  delete head_;
+  head_ = var;
+}
+
+template< typename T >
+T Queue< T >::drop()
+{
+  T var = head_->data;
+  dequeue();
+  return var;
+}
 #endif
--- mashkin.andrew/S1/stack.h
+++ mashkin.andrew/S1/stack.h
@@ -1,20 +1,63 @@
 #ifndef STACK_H
 #define STACK_H
 #include <cstddef>
+#include <stdexcept>
 #include "list.h"
 
 template< typename T >
 class Stack
 {
 public:
-  explicit Stack(T symb);
+  explicit Stack();
 
   void push(T rhs);
   void pop();
   T drop();
 
 private:
-  list_t< T > stack_;
-  list_t< T > top_;
+  list_t< T >* stack_;
+  list_t< T >* top_;
 };
+
+
+template< typename T >
+Stack< T >::Stack():
+  stack_(nullptr),
+  top_(stack_)
+{
+}
+
+template< typename T >
+void Stack< T >::push(T rhs)
+{
+  if (!top_->data)
+  {
+    top_ = new list_t< T >{rhs, nullptr};
+  }
+  top_->next = new list_t< T >{rhs, nullptr};
+  top_ = top_->next;
+}
+
+template< typename T >
+void Stack< T >::pop()
+{
+  if (!stack_->data)
+  {
+    throw std::underflow_error("Stack underflow");
+  }
+  delete top_;
+  top_ = stack_->next;
+  while (top_->next)
+  {
+    top_ = top_->next;
+  }
+}
+
+template< typename T >
+T Stack< T >::drop()
+{
+  T var = top_->data;
+  pop();
+  return var;
+}
 #endif
