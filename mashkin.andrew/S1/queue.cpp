eadc4be38522088741fb92294be842285734738e
Машкин Андрей <beedzibood@gmail.com>
2023-03-17 16:43:02 +0300

    redefined constructor and all methods

--- mashkin.andrew/S1/queue.cpp
+++ mashkin.andrew/S1/queue.cpp
@@ -3,36 +3,35 @@
 #include <stdexcept>
 
 template< typename T >
-Queue< T >::Queue():
-  queue_{0},
-  head_(0),
-  tail_(0)
+Queue< T >::Queue(T symb):
+  head_(new list_t< T >{symb, nullptr}),
+  tail_(head_)
 {
 }
 
 template< typename T >
 void Queue< T >::enqueue(T rhs)
 {
-  if (tail_ == OVERFLOW)
-  {
-    throw std::overflow_error("Queue overflow");
-  }
-  queue_[tail_++];
+  tail_.next = new list_t< T >{rhs, nullptr};
+  tail_ = tail_.next;
 }
 
 template< typename T >
 void Queue< T >::dequeue()
 {
-  if (tail_ + 1 == head_)
+  if (tail_ == head_)
   {
     throw std::underflow_error("Queue underflow");
   }
-  queue_[head_++];
+  list_t< T > var = head_.next;
+  delete head_;
+  head_ = var;
 }
 
 template< typename T >
 T Queue< T >::drop()
 {
+  T var = head_.data;
   dequeue();
-  return queue_[head_ - 1];
+  return var;
 }
