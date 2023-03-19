85649680b187b32099ed22ea301bf8479a9d4123
Kozyrin Semen <garbagemilitia@gmail.com>
2023-03-20 02:10:18 +0300

    Stack implemented

---
+++ kozyrin.semen/S1/stack.h
@@ -0,0 +1,56 @@
+#ifndef STACK_H
+#define STACK_H
+
+template< typename T >
+struct sBox {
+  T value_ = 0;
+  sBox< T >* prev_ = nullptr;
+};
+
+template< typename T >
+class Stack {
+public:
+  Stack();
+  ~Stack();
+  void push(T rhs);
+  T drop();
+private:
+  sBox< T >* top_;
+};
+
+template< typename T >
+Stack< T >::Stack():
+top_(nullptr)
+{}
+
+template< typename T >
+Stack< T >::~Stack()
+{
+  while (top_->prev_ != nullptr) {
+    sBox< T >* next = top_->prev_;
+    delete top_;
+    top_ = next;
+  }
+}
+
+template< typename T >
+void Stack< T >::push(T rhs)
+{
+  top_ = new sBox< T >{rhs, top_};
+}
+
+template< typename T >
+T Stack< T >::drop()
+{
+  if (top_ == nullptr) {
+    throw std::length_error("Nothing to drop");
+  }
+
+  sBox< T >* top = top_->prev_;
+  T res = top_->value_;
+  delete top_;
+  top_ = top;
+  return res;
+}
+
+#endif
