18334ba269ef73c77554d1f7a0225bc7c8f1fd99
Chemodurovart <chemodurovart@gmail.com>
2023-03-19 13:28:53 +0300

    complete calcPostfixExpr and some fixes

--- chemodurov.artem/S1/read-infix-expr.cpp
+++ chemodurov.artem/S1/read-infix-expr.cpp
@@ -4,7 +4,7 @@
 
 chemodurov::InfixExpr * chemodurov::readInfixExpr(std::string & line, size_t & inf_size)
 {
-  chemodurov::InfixExpr * inf = new chemodurov::InfixExpr[line.size() / 2 + 1];
+  InfixExpr * inf = new InfixExpr[line.size() / 2 + 1];
   inf_size = 0;
   size_t size1 = 0;
   size_t size2 = 0;
@@ -12,37 +12,37 @@ chemodurov::InfixExpr * chemodurov::readInfixExpr(std::string & line, size_t & i
   {
     if (line[size2] == '(')
     {
-      inf[inf_size++] = {chemodurov::BRACE_LEFT, false, false};
+      inf[inf_size++] = {BRACE_LEFT, false, false};
       size2 += 2;
     }
     else if (line[size2] == ')')
     {
-      inf[inf_size++] = {chemodurov::BRACE_RIGHT, false, false};
+      inf[inf_size++] = {BRACE_RIGHT, false, false};
       size2 += 2;
     }
     else if (line[size2] == '+')
     {
-      inf[inf_size++] = {chemodurov::OPERATION_PLUS, false, true};
+      inf[inf_size++] = {OPERATION_PLUS, false, true};
       size2 += 2;
     }
     else if (line[size2] == '-')
     {
-      inf[inf_size++] = {chemodurov::OPERATION_MINUS, false, true};
+      inf[inf_size++] = {OPERATION_MINUS, false, true};
       size2 += 2;
     }
     else if (line[size2] == '*')
     {
-      inf[inf_size++] = {chemodurov::OPERATION_MULTIPLICATION, false, true};
+      inf[inf_size++] = {OPERATION_MULTIPLICATION, false, true};
       size2 += 2;
     }
     else if (line[size2] == '/')
     {
-      inf[inf_size++] = {chemodurov::OPERATION_DIVIDE, false, true};
+      inf[inf_size++] = {OPERATION_DIVIDE, false, true};
       size2 += 2;
     }
     else if (line[size2] == '%')
     {
-      inf[inf_size++] = {chemodurov::OPERATION_REMINDER_OF_DIVISION, false, true};
+      inf[inf_size++] = {OPERATION_REMINDER_OF_DIVISION, false, true};
       size2 += 2;
     }
     else if (line[size2] == ' ')
@@ -60,7 +60,7 @@ chemodurov::InfixExpr * chemodurov::readInfixExpr(std::string & line, size_t & i
       }
       catch (const std::invalid_argument & e)
       {
-        delete [] inf;
+        delete[] inf;
         throw;
       }
     }
