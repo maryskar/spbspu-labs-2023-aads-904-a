12f92b799faedd5e4d39b6ec4650a24525169a9d
Chemodurovart <chemodurovart@gmail.com>
2023-03-19 12:08:22 +0300

    one iteration of conversion complete mb

--- chemodurov.artem/S1/convert-infix-to-postfix.cpp
+++ chemodurov.artem/S1/convert-infix-to-postfix.cpp
@@ -1,5 +1,16 @@
 #include "convert-infix-to-postfix.hpp"
 #include "stack.hpp"
+#include "compare-operations-priority.hpp"
+namespace chemodurov
+{
+  chemodurov::OperationAndBrace makeOperationAndBrace(chemodurov::Operation op)
+  {
+    OperationAndBrace temp;
+    temp.isBrace = false;
+    temp.data.operation = op;
+    return temp;
+  }
+}
 
 chemodurov::Queue< chemodurov::PostfixExpr > chemodurov::convertInfixToPostfix(Queue< InfixExpr > inf)
 {
@@ -9,7 +20,6 @@ chemodurov::Queue< chemodurov::PostfixExpr > chemodurov::convertInfixToPostfix(Q
   if (next.isOperand)
   {
     post.push({next.data.operand, true});
-    inf.pop();
   }
   else if (!next.isOperand && !next.isOperation)
   {
@@ -29,9 +39,27 @@ chemodurov::Queue< chemodurov::PostfixExpr > chemodurov::convertInfixToPostfix(Q
         throw std::invalid_argument("Incorrect infix expression");
       }
       stack.pop();
-      inf.pop();
     }
   }
-  //
+  else if (next.isOperation)
+  {
+    if (stack.empty())
+    {
+      stack.push(makeOperationAndBrace(next.data.operation));
+    }
+    else
+    {
+      int prior_diff = compareOperationsPriority(next.data.operation, stack.getFromStack().data.operation);
+      while (!stack.empty() && !stack.getFromStack().isBrace && prior_diff >= 0)
+      {
+        post.push({stack.getFromStack().data.operation, false});
+        stack.pop();
+        prior_diff = compareOperationsPriority(next.data.operation, stack.getFromStack().data.operation);
+      }
+
+      stack.push(makeOperationAndBrace(next.data.operation));
+    }
+  }
+  inf.pop();
   return post;
 }
