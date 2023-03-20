5602c370081ed9939696b037671b73f741a7fd55
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-03-20 14:39:58 +0300

    create Node struct

--- shelkovnikov.dmitrii/common/BinarySearchTree.h
+++ shelkovnikov.dmitrii/common/BinarySearchTree.h
@@ -4,6 +4,21 @@ namespace dimkashelk
 {
   template< typename Key, typename Value, typename Compare >
   class BinarySearchTree
-  {};
+  {
+  private:
+    struct Node {
+      T data;
+      Node *left;
+      Node *right;
+      int height;
+      Node(const T &data):
+        data(data),
+        left(nullptr),
+        right(nullptr),
+        height(0)
+      {}
+    };
+    Node *root;
+  };
 }
 #endif
