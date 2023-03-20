49fc23aa858850d7f4796acef4ab17eb825b52a9
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-03-20 14:36:22 +0300

    create BinarySearchTree class

---
+++ shelkovnikov.dmitrii/common/BinarySearchTree.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_BINARYSEARCHTREE_H
+#define COMMON_BINARYSEARCHTREE_H
+namespace dimkashelk
+{
+  template< typename Key, typename Value, typename Compare >
+  class BinarySearchTree
+  {};
+}
+#endif
