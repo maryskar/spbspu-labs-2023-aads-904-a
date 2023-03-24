6eb08152f34753c1c11f8b6ed3fa3ed862e6f6f1
Kirill Odintsov <ksodintsov03@gmail.com>
2023-03-24 23:49:09 +0300

    Empty queue errors

--- odintsov.kirill/S1/Queue.cpp
+++ odintsov.kirill/S1/Queue.cpp
@@ -1,17 +1,28 @@
 #include "Queue.hpp"
 
+#include <stdexcept>
+
 #include "AppendableList.hpp"
 
 template< typename T >
 void odintsov::Queue< T >::pop()
 {
+  if (!this->head_) {
+    throw std::runtime_error("Cannot pop empty queue");
+  }
   typename AppendableList< T >::Node* next = this->head_->next;
   delete this->head_;
   this->head_ = next;
+  if (!this->head_) {
+    this->tail_ = nullptr;
+  }
 }
 
 template< typename T >
 T& odintsov::Queue< T >::head()
 {
+  if (!this->head_) {
+    throw std::runtime_error("Attempt to get head of empty queue");
+  }
   return this->head_->data_;
 }
