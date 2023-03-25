55215f0a195710319fdc075210f21220ec290aff
Kirill Odintsov <ksodintsov03@gmail.com>
2023-03-26 01:02:55 +0300

    StringSplitter realization

---
+++ odintsov.kirill/S1/StringSplitter.cpp
@@ -0,0 +1,24 @@
+#include "StringSplitter.hpp"
+
+#include <cstddef>
+#include <stdexcept>
+
+odintsov::StringSplitter::StringSplitter(const std::string& str):
+  str_(str)
+{}
+
+odintsov::StringSplitter& odintsov::StringSplitter::operator>>(std::string& ostr)
+{
+  if (str_.empty()) {
+    throw std::runtime_error("Read error");
+  }
+  size_t spaceIndex = str_.find_first_of(' ');
+
+  ostr = str_.substr(0, spaceIndex);
+  if (spaceIndex == str_.npos) {
+    str_.clear();
+  } else {
+    str_ = str_.substr(spaceIndex + 1);
+  }
+  return *this;
+}
