9fab0d9fe5c0cbfe2d888ca7bbded270e26aed31
Kirill Odintsov <ksodintsov03@gmail.com>
2023-03-25 11:48:58 +0300

    Stack realization

---
+++ odintsov.kirill/S1/Stack.cpp
@@ -0,0 +1,58 @@
+#include "Stack.hpp"
+
+#include <stdexcept>
+
+template< typename T >
+odintsov::Stack< T >::Stack():
+  head_(nullptr),
+  tail_(nullptr)
+{}
+
+template< typename T >
+odintsov::Stack< T >::~Stack()
+{
+  while (!empty()) {
+    Node* oldTail = tail_;
+    tail_ = tail_->prev;
+    delete oldTail;
+  }
+}
+
+template< typename T >
+T& odintsov::Stack< T >::tail()
+{
+  if (empty()) {
+    throw std::runtime_error("Attempt to get tail of empty stack");
+  }
+  return tail_->data;
+}
+
+template< typename T >
+const T& odintsov::Stack< T >::tail() const
+{
+  if (empty()) {
+    throw std::runtime_error("Attempt to get tail of empty stack");
+  }
+  return tail_->data;
+}
+
+template< typename T >
+void odintsov::Stack< T >::push(const T& data)
+{
+  Node* newNode = new Node{data, tail_};
+  if (empty()) {
+    head_ = newNode;
+  }
+  tail_ = newNode;
+}
+
+template< typename T >
+void odintsov::Stack< T >::pop()
+{
+  if (empty()) {
+    throw std::runtime_error("Attempt to pop empty stack");
+  }
+  Node* oldTail = tail_;
+  tail_ = tail_->prev;
+  delete oldTail;
+}
