5e8f0372fb4591b56fe0df8e2b97ef42d1bb6663
Kirill Odintsov <ksodintsov03@gmail.com>
2023-03-25 11:48:40 +0300

    Stack composition

---
+++ odintsov.kirill/S1/Stack.hpp
@@ -0,0 +1,28 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+namespace odintsov {
+  template< typename T >
+  class Stack {
+   public:
+    Stack();
+    ~Stack();
+
+    T& tail();
+    const T& tail() const;
+    void push(const T& data);
+    void pop();
+    bool empty() const;
+
+   private:
+    struct Node {
+      T data;
+      Node* prev;
+    };
+
+    Node* head_;
+    Node* tail_;
+  };
+}
+
+#endif
