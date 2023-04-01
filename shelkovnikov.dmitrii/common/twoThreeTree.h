63c2696bf6a6ac03c82afe490d93be119efb7373
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-04-01 23:59:20 +0300

    add operator>

--- shelkovnikov.dmitrii/common/twoThreeTree.h
+++ shelkovnikov.dmitrii/common/twoThreeTree.h
@@ -332,6 +332,34 @@ namespace dimkashelk
     {
       return Iterator();
     }
+    friend bool operator>(const two_three_tree_type &first, const two_three_tree_type &second)
+    {
+      auto first_start = first.begin();
+      auto second_start = second.begin();
+      auto first_end = first.end();
+      auto second_end = second.end();
+      while (first_start != first_end && second_start != second_end)
+      {
+        if (*first_start != *second_start)
+        {
+          return *first_start > *second_start;
+        }
+        first_start++;
+        second_start++;
+      }
+      if (first_start == first_end && second_start != second_end)
+      {
+        return true;
+      }
+      else if (first_start != first_end && second_start == second_end)
+      {
+        return false;
+      }
+      else
+      {
+        return false;
+      }
+    }
   private:
     node_type *root_;
     Compare compare_;
