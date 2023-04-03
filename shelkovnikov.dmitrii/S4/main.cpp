fda49fcdea6b41ca01ac84cfffb7c2582cb64183
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-04-03 20:29:47 +0300

    add main

--- shelkovnikov.dmitrii/S4/main.cpp
+++ shelkovnikov.dmitrii/S4/main.cpp
@@ -1,4 +1,15 @@
-int main()
+#include <iostream>
+#include <labList1.h>
+int main(int argc, char *argv[])
 {
+  try
+  {
+    dimkashelk::labList1(std::cin, std::cout, argc, argv);
+  }
+  catch (const std::runtime_error &e)
+  {
+    std::cerr << e.what();
+    return 1;
+  }
   return 0;
 }
