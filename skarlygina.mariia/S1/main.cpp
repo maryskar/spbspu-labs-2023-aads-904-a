5b69db0c33a14b532486ec18c516577837a64ee6
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-03 14:29:42 +0300

    adding argc argv

--- skarlygina.mariia/S1/main.cpp
+++ skarlygina.mariia/S1/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-int main()
+#include <cstring>
+#include <fstream>
+
+int main(int argc, char* argv[])
 {
-  return 0;
+  if (argc!=2)
+  {
+    std::cerr << "Error while reading name of file";
+    return 1;
+  }
+
+  std::ifstream fin(argv[1]);
+  if (!fin.is_open())
+  {
+    std::cerr << "Reading File could not be opened\n";
+    return 1;
+  }
+
 }
