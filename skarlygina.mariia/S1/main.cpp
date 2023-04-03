104926fc6065f0f25cad88bcf12550ccfbe0e423
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-03 14:47:40 +0300

    adding queue

--- skarlygina.mariia/S1/main.cpp
+++ skarlygina.mariia/S1/main.cpp
@@ -4,17 +4,13 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc!=2)
+  if (argc == 2)
   {
-    std::cerr << "Error while reading name of file";
-    return 1;
+    std::ifstream fin(argv[1]);
+    if (!fin.is_open())
+    {
+      std::cerr << "Reading File could not be opened\n";
+      return 1;
+    }
   }
-
-  std::ifstream fin(argv[1]);
-  if (!fin.is_open())
-  {
-    std::cerr << "Reading File could not be opened\n";
-    return 1;
-  }
-
 }
---
+++ skarlygina.mariia/S1/queue.h
@@ -0,0 +1,12 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+template< typename T >
+class Queue
+{
+public:
+  void push(T rhs);
+  T drop();
+};
+#endif
+
