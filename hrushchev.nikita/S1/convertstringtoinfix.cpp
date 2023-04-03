ab30d13e567e6c2561200e97e9b8c31d0c03d019
Shuzamada <shuzamada@inbox.ru>
2023-04-04 02:13:40 +0300

    convertStringToInfix

---
+++ hrushchev.nikita/S1/convertstringtoinfix.cpp
@@ -0,0 +1,15 @@
+#include "convertstringtoinfix.hpp"
+#include <string>
+#include <cstring>
+#include <cstddef>
+#include "queue.hpp"
+
+Queue< std::string > convertStringToInfix(std::string str)
+{
+  Queue< std::string > infix_queue;
+  for(size_t i = 0; str[i] != '\0'; i++)
+  {
+    infix_queue.push(str.substr(i, 1));
+  }
+  return infix_queue;
+}
---
+++ hrushchev.nikita/S1/convertstringtoinfix.hpp
@@ -0,0 +1,9 @@
+#ifndef CONVERTSTRINGTOINFIX_HPP
+#define CONVERTSTRINGTOINFIX_HPP
+#include <string>
+#include "queue.hpp"
+
+Queue< std::string > convertStringToInfix(std::string str);
+
+#endif
+
