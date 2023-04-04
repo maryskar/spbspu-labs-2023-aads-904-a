b0da26a43bbf52030843b9c5bad34873fd992b1a
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-04-04 17:04:54 +0300

    move freeList to dimkashelk namespace

--- shelkovnikov.dmitrii/S1/freeList.h
+++ shelkovnikov.dmitrii/S1/freeList.h
@@ -1,16 +1,19 @@
 #ifndef SPBSPU_LABS_2023_AADS_904_A_FREELIST_H
 #define SPBSPU_LABS_2023_AADS_904_A_FREELIST_H
 #include "nodeOfDataClass.h"
-namespace details
+namespace dimkashelk
 {
-  template< typename T >
-  void freeList(NodeOfDataClass< T > *start)
+  namespace details
   {
-    while (start)
+    template <typename T>
+    void freeList(NodeOfDataClass<T> *start)
     {
-      auto node = start;
-      start = start->next;
-      delete node;
+      while (start)
+      {
+        auto node = start;
+        start = start->next;
+        delete node;
+      }
     }
   }
 }
