7a2a6a307ceb856ed1d516b8f025bc74b35d3f42
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-04 23:54:29 +0300

    adding node and modified template queue

--- skarlygina.mariia/S1/queue.h
+++ skarlygina.mariia/S1/queue.h
@@ -1,12 +1,15 @@
 #ifndef QUEUE_H
 #define QUEUE_H
-
+#include "node.h"
 template< typename T >
 class Queue
 {
 public:
+  Queue();
   void push(T rhs);
   T drop();
+  ~Queue();
+private:
+  Node< T >* head;
 };
 #endif
-
