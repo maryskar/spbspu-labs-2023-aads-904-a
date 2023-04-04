356eb640d3420fcf1639ff7388ba175dab4c496d
Shuzamada <shuzamada@inbox.ru>
2023-04-04 12:37:12 +0300

    fix main

--- hrushchev.nikita/S1/main.cpp
+++ hrushchev.nikita/S1/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <stdexcept>
 #include "convertstringtoinfix.hpp"
 #include "convertinfixtopostfix.hpp"
 #include "calcucalepostfix.hpp"
@@ -15,30 +16,66 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  std::ifstream input(argv[1]);
-  if (!input)
-  {
-    std::cerr << "Unable to open file: " << argv[1] << "\n";
-    return 1;
-  }
-
-  std::string line;
-  while (getline(input, line))
-  {
-    try
+  if(argc == 2)
     {
-      Queue< std::string > infix = convertStringToInfix(line);
-      Queue< std::string > postfix = convertInfixToPostfix(infix);
-      std::cout << calculatePostfix(postfix) << "\n";
-    }
-    catch (...)
+    std::ifstream input(argv[1]);
+    if (!input)
     {
-      return 2;
+      std::cerr << "Unable to open file: " << argv[1] << "\n";
+      return 1;
     }
-  }
 
+    std::string line;
+    while (getline(input, line))
+    {
+      if (!std::cin)
+      {
+        break;
+      }
+      if (line.empty())
+      {
+        continue;
+      }      
+      try
+      {
+        Queue< std::string > infix = convertStringToInfix(line);
+        Queue< std::string > postfix = convertInfixToPostfix(infix);
+        std::cout << calculatePostfix(postfix) << "\n";
+      }
+      catch (const std::exception& e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 2;
+      }
+    }
   input.close();
-
-  return 0;
+  }
+  else
+  {
+    std::string line;
+    while (std::cin)
+    {
+      getline(std::cin, line)
+      if (!std::cin)
+      {
+        break;
+      }
+      if (line.empty())
+      {
+        continue;
+      }
+      try
+      {
+        Queue< std::string > infix = convertStringToInfix(line);
+        Queue< std::string > postfix = convertInfixToPostfix(infix);
+        std::cout << calculatePostfix(postfix) << "\n";
+      }
+      catch (const std::exception& e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 2;
+      }
+    } 
+  }
 }
 
