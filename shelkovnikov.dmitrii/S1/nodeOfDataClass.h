88cfe1bb1c09f33a85345d837582b0d288deccee
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-04-04 17:04:18 +0300

    move nodeOfDataClass to dimkashelk namespace

--- shelkovnikov.dmitrii/S1/nodeOfDataClass.h
+++ shelkovnikov.dmitrii/S1/nodeOfDataClass.h
@@ -1,16 +1,20 @@
 #ifndef SPBSPU_LABS_2023_AADS_904_A_NODE_H
 #define SPBSPU_LABS_2023_AADS_904_A_NODE_H
-namespace details
+namespace dimkashelk
 {
-  template< typename T >
-  struct NodeOfDataClass
+  namespace details
   {
-    T data;
-    NodeOfDataClass *next;
-    explicit NodeOfDataClass(const T &rhs):
-      data(rhs),
-      next(nullptr)
-    {}
-  };
+    template <typename T>
+    struct NodeOfDataClass
+    {
+      T data;
+      NodeOfDataClass *next;
+      explicit NodeOfDataClass(const T &rhs) :
+        data(rhs),
+        next(nullptr)
+      {
+      }
+    };
+  }
 }
 #endif
