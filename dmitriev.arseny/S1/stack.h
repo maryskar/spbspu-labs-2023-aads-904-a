455651e7e5fbf849f25ab48e62db3c37595d2527
ArsDi <abstractlevl@gmail.com>
2023-04-05 20:42:23 +0300

    basic realization

--- dmitriev.arseny/S1/stack.h
+++ dmitriev.arseny/S1/stack.h
@@ -1,14 +1,37 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include "list.h"
+
 template< typename T >
 class Stack
 {
 public:
+  Stack();
+  ~Stack();
 
+  void push(T rhs);
+  T drop();
+
+  bool isEmpty() const;
+  T top() const;
 
 private:
+  List< T >* top;
 
 };
+
+template< typename T >
+inline Stack< T >::Stack():
+  top(nullptr)
+{
+
+}
+
+template< typename T >
+inline Stack< T >::~Stack()
+{
+
+}
 
 #endif
