6631d446600945c148675e576ec56a4e3a77725c
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-08 00:00:22 +0300

    header for calculator

---
+++ skarlygina.mariia/S1/calculator.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+namespace calculator
+{
+  long long sum(long long a, long long b);
+  long long substraction(long long a, long long b);
+  long long multiplication(long long a, long long b);
+  long long division(long long a, long long b);
+}
+#endif
