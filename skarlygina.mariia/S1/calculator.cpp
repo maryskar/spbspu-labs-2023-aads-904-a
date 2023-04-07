c1f732a516fce20dc19c7c88050b774559027dbc
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-08 00:52:44 +0300

    operations

--- skarlygina.mariia/S1/calculator.cpp
+++ skarlygina.mariia/S1/calculator.cpp
@@ -3,10 +3,12 @@
 #include <cmath>
 #include <stdexcept>
 
+constexpr long long limit_min = std::numeric_limits< long long >::min();
+constexpr long long limit_max = std::numeric_limits< long long >::max();
+
 long long calculator::sum(long long a, long long b)
 {
-  constexpr long long limit = std::numeric_limits< long long >::max();
-  if (limit - a < b)
+  if (limit_max - a < b)
   {
     throw std::overflow_error("Overflow numbers");
   }
@@ -15,10 +17,24 @@ long long calculator::sum(long long a, long long b)
 
 long long calculator::substraction(long long a, long long b)
 {
-  constexpr long long limit = std::numeric_limits< long long >::min();
-  if (limit + b > a)
+  if (limit_min + b > a)
   {
     throw std::overflow_error("Overflow numbers");
   }
   return a - b;
+}
+
+long long calculator::multiplication(long long a, long long b)
+{
+  return a * b;
+}
+
+long long calculator::division(long long a, long long b)
+{
+  return a / b;
+}
+
+long long calculator::remainder(long long a, long long b)
+{
+  return a % b;
 }
