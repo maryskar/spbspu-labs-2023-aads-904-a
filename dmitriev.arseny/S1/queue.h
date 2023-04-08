653261bcc5c1156f9114a075a451075848d2be4e
ArsDi <abstractlevl@gmail.com>
2023-04-08 14:05:45 +0300

    queue basic realization

--- dmitriev.arseny/S1/queue.h
+++ dmitriev.arseny/S1/queue.h
@@ -2,6 +2,7 @@
 #define QUEUE_H
 
 #include "list.h"
+#include <iomanip>
 
 template< typename T >
 class Queue
@@ -10,8 +11,85 @@ public:
 	Queue();
 	~Queue();
 
+	void push(T rhs);
+	void popBack();
+	T getTopData();
+
+	T unsafeGetTopAndPop();
+
+	bool isEmpty();
+
 private:
+	List< T >* head;
+	List< T >* tail;
 
 };
+
+template<typename T>
+Queue<T>::Queue() :
+	head(nullptr),
+	tail(nullptr)
+{
+
+}
+
+template<typename T>
+Queue<T>::~Queue()
+{
+
+}
+
+template< typename T >
+void Queue< T >::push(T rhs)
+{
+	if (head == nullptr)
+	{
+		head = new List< T >(rhs);
+		tail = head;
+	}
+	else
+	{
+		tail->otherList = new List< T >(rhs);
+		tail = tail->otherList;
+	}
+}
+
+template< typename T >
+void Queue< T >::popBack()
+{
+	if (head == nullptr)
+	{
+		throw std::underflow_error("underflow_error");
+	}
+	List< T >* newHead = head->otherList;
+	delete head;
+
+	head = newHead;
+}
+
+template<typename T>
+T Queue<T>::getTopData()
+{
+	if (head == nullptr)
+	{
+		throw std::underflow_error("underflow_error");
+	}
+	return head->data;
+}
+
+template<typename T>
+T Queue<T>::unsafeGetTopAndPop()
+{
+	T currentData = getTopData();
+	popBack();
+
+	return T;
+}
+
+template<typename T>
+inline bool Queue<T>::isEmpty()
+{
+	return head == nullptr;
+}
 
 #endif
