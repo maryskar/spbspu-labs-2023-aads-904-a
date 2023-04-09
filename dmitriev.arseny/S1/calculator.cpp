0c0ef64ff199a855c88cd955b34ae1adb66a8084
ArsDi <abstractlevl@gmail.com>
2023-04-09 13:17:04 +0300

    calcul init

---
+++ dmitriev.arseny/S1/calculator.cpp
@@ -0,0 +1,10 @@
+#include "calculator.h"
+#include "stack.h"
+#include "queue.h"
+
+double calculateTheExpression(std::istream& in)
+{
+
+
+  return;
+}
---
+++ dmitriev.arseny/S1/calculator.h
@@ -0,0 +1,8 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <iostream>
+
+double calculateTheExpression(std::istream& in);
+
+#endif
