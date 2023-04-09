5e8c58e08a98e38af728491704f7ad06774ea327
Потапова <marina-potapova04@mail.ru>
2023-04-10 00:10:31 +0300

    add realisation of Queue

--- potapova.marina/S1/queue.h
+++ potapova.marina/S1/queue.h
@@ -1,20 +1,66 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-template< typename T >
-class Queue
+namespace potapova
 {
+  template< typename T >
+  class Queue
+  {
   public:
-    Queue();
-    ~Queue() {}
-    void push(const T& elem);
-    T drop();
+    Queue():
+      begin_ptr(nullptr),
+      end_ptr(nullptr)
+    {
+
+    }
+
+    ~Queue()
+    {
+      while (begin_ptr != nullptr)
+      {
+        pop();
+      }
+    }
+
+    void push(const T& elem)
+    {
+      if (begin_ptr == nullptr)
+      {
+        begin_ptr = end_ptr = new Node(elem);
+        return;
+      }
+      end_ptr = end_ptr->next_node_ptr = new Node(elem);
+    }
+
+    T drop()
+    {
+      T prev_begin_data = begin_ptr->data;
+      pop();
+      return prev_begin_data;
+    }
   private:
     struct Node
     {
-      T* data;
+      Node(const T& data):
+        data(data),
+        next_node_ptr(nullptr)
+      {
+
+      }
+
+      T data;
+      Node* next_node_ptr;
     };
-};
- 
+    Node* begin_ptr;
+    Node* end_ptr;
+      
+    void pop()
+    {
+      Node* prev_begin_ptr = begin_ptr;
+      begin_ptr = begin_ptr->next_node_ptr;
+      delete prev_begin_ptr;
+    }
+  };
+}
 
 #endif
