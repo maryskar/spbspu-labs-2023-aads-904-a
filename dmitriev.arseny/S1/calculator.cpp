30d3ceb1f08298d2b10378fd07cf6cce9eef03f0
ArsDi <abstractlevl@gmail.com>
2023-04-09 13:29:43 +0300

    input geting

--- dmitriev.arseny/S1/calculator.cpp
+++ dmitriev.arseny/S1/calculator.cpp
@@ -2,9 +2,37 @@
 #include "stack.h"
 #include "queue.h"
 
+#include <string>
+
+Queue< std::string > getQueueFromInput(std::istream& input);
+
 double calculateTheExpression(std::istream& in)
 {
 
 
   return;
+}
+
+Queue< std::string > getQueueFromInput(std::istream& istr)
+{
+  Queue< std::string > infQueue;
+  std::string inp = "";
+  std::string curr = "";
+
+  std::getline(istr, inp);
+  for (size_t i = 0; inp[i] != '\0'; i++)
+  {
+    if (inp[i] == ' ')
+    {
+      infQueue.push(curr);
+      curr = "";
+    }
+    else
+    {
+      curr = curr + inp[i];
+    }
+  }
+  infQueue.push(curr);
+
+  return infQueue;
 }
