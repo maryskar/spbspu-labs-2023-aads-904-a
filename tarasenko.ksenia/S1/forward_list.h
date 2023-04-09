7ac3a85310eb25700f4e965726bd99e4867d0390
xeniaTa <kseniatarasenko1@gmail.com>
2023-04-09 11:47:01 +0300

    add funcs forward list

--- tarasenko.ksenia/S1/forward_list.h
+++ tarasenko.ksenia/S1/forward_list.h
@@ -1,6 +1,7 @@
 #ifndef FORWARD_LIST_H
 #define FORWARD_LIST_H
 #include <memory>
+#include <algorithm>
 #include "node.h"
 namespace tarasenko
 {
@@ -15,12 +16,7 @@ namespace tarasenko
    ForwardList(const ForwardList< T >& other):
      first(nullptr)
    {
-     ForwardList< T >* copy = other.first;
-     while (copy)
-     {
-       pushBack(std::addressof(first), copy->data);
-       copy = copy->next;
-     }
+
    }
    ForwardList(ForwardList< T >&& other):
      first(other.first)
@@ -28,16 +24,61 @@ namespace tarasenko
      other.first = nullptr;
    }
    ForwardList< T >& operator=(const ForwardList< T >& q)
-   {}
+   {
+
+   }
    ForwardList< T >& operator=(ForwardList< T >&& q)
-   {}
+   {
+
+   }
    ~ForwardList()
    {
      details::clear(std::addressof(first));
    };
-
+   bool isEmpty() const;
+   void pushFront(T& data);
+   void pushBack(T& data);
+   T getFront() const;
+   void popFront();
+   void clear();
   private:
    details::NodeOfList< T >* first;
   };
+
+  template< typename T >
+  bool ForwardList< T >::isEmpty() const
+  {
+    return details::isEmpty(first);
+  }
+
+  template< typename T >
+  void ForwardList< T >::pushFront(T& data)
+  {
+    details::pushFront(&first, data);
+  }
+
+  template< typename T >
+  void ForwardList< T >::pushBack(T& data)
+  {
+    details::pushBack(&first, data);
+  }
+
+  template< typename T >
+  T ForwardList< T >::getFront() const
+  {
+    return details::getFront(first);
+  }
+
+  template< typename T >
+  void ForwardList< T >::popFront()
+  {
+    details::popFront(&first);
+  }
+
+  template< typename T >
+  void ForwardList< T >::clear()
+  {
+    details::clear(&first);
+  }
 }
 #endif
