236165bec3cef4b3ea8277fea7f27de5d69c37c1
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-10 17:41:55 +0300

    base example

---
+++ skarlygina.mariia/S1/count.cpp
@@ -0,0 +1,19 @@
+#include "count.h"
+long long SwitchSymbol(char operation, long long a, long long b)
+{
+  switch (operation)
+  {
+  case '+':
+    return a + b;
+  case '-':
+      return a + b;
+  case '*':
+      return a + b;
+  case '/':
+      return a + b;
+  case '%':
+      return a + b;
+  default:
+      throw;
+  }
+}
