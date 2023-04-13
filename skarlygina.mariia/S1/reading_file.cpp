5e91dfa7416853b56a1614fe69ab8703e0794dae
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-13 14:42:29 +0300

    function to read elements

--- skarlygina.mariia/S1/reading_file.cpp
+++ skarlygina.mariia/S1/reading_file.cpp
@@ -2,7 +2,30 @@
 #include <stdexcept>
 #include <iostream>
 #include <string>
+#include "list.h"
 
-void readElements()
+template< typename T>
+List< T > readElements(std::string filename)
 {
+  std:ifstream file(filename);
+  std::string line;
+  List< T > list = nullptr;
+  if (file.is_open())
+  {
+    while (getline(file, line))
+	{
+	  size_t pos = 0;
+	  std::string token;
+	  while ((pos = line.find(" ") != std::string::npos))
+	  {
+	    token = line.substr(0, pos);
+		list->data = token;
+		line.erase(0, pos + 1);
+	  }
+	  //adding the last elem later
+	}
+	file.close();
+  }
+  //change list into stack or queue
+  return list;
 }
