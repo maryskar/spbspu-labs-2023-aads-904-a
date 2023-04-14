1786a7ac058b3cc2e60e0ebeaebf866c8db1a439
Kirill Odintsov <ksodintsov03@gmail.com>
2023-04-14 17:31:09 +0300

    MathNode changes
    Destructor was unneeded because Operator has trivial constructor

---
+++ odintsov.kirill/S1/MathNode.cpp
@@ -0,0 +1,76 @@
+#include "MathNode.hpp"
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include "Operator.hpp"
+
+odintsov::MathNode::MathNode(long long operand):
+  tag_(Tag::Operand),
+  data_{.operand = operand}
+{}
+
+odintsov::MathNode::MathNode(const odintsov::Operator& oper):
+  tag_(Tag::Operator),
+  data_{.oper = oper}
+{}
+
+odintsov::MathNode::MathNode(char paren):
+  tag_(paren == '(' ? Tag::OpenParen : Tag::CloseParen),
+  data_{0}
+{
+  if (paren != '(' && paren != ')') {
+    throw std::invalid_argument("Incorrect parenthesis");
+  }
+}
+
+odintsov::MathNode::MathNode(const std::string& str):
+  data_{0}
+{
+  try {
+    data_.operand = stoll(str);
+    tag_ = Tag::Operand;
+    return;
+  } catch (const std::invalid_argument& e) {
+  }
+  if (str.size() != 1) {
+    throw std::invalid_argument("Incorrect operator/operand");
+  }
+  char c = str[0];
+  if (c == '(') {
+    tag_ = Tag::OpenParen;
+    return;
+  } else if (c == ')') {
+    tag_ = Tag::CloseParen;
+    return;
+  }
+  new (&data_.oper) odintsov::Operator(c);
+  tag_ = Tag::Operator;
+}
+
+bool odintsov::MathNode::isDataType(Tag tag) const
+{
+  return tag_ == tag;
+}
+
+long long odintsov::MathNode::getOperand() const
+{
+  if (!isDataType(Tag::Operand)) {
+    throw std::logic_error("Invalid attempt to interpret node as operand");
+  }
+  return data_.operand;
+}
+
+odintsov::Operator& odintsov::MathNode::getOperator()
+{
+  return const_cast< odintsov::Operator& >(const_cast< const odintsov::MathNode* >(this)->getOperator());
+}
+
+const odintsov::Operator& odintsov::MathNode::getOperator() const
+{
+  if (!isDataType(Tag::Operator)) {
+    throw std::logic_error("Invalid attempt to interpret node as operator");
+  }
+  return data_.oper;
+}
---
+++ odintsov.kirill/S1/MathNode.hpp
@@ -0,0 +1,39 @@
+#ifndef MATHNODE_HPP
+#define MATHNODE_HPP
+
+#include <string>
+
+#include "Operator.hpp"
+
+namespace odintsov {
+  class MathNode {
+   public:
+    enum class Tag {
+      Operand,
+      Operator,
+      OpenParen,
+      CloseParen
+    };
+
+    MathNode(long long operand);
+    MathNode(const odintsov::Operator& oper);
+    MathNode(char paren);
+    MathNode(const std::string& str);
+
+    bool isDataType(Tag tag) const;
+
+    long long getOperand() const;
+    odintsov::Operator& getOperator();
+    const odintsov::Operator& getOperator() const;
+
+   private:
+    Tag tag_;
+
+    union Data {
+      long long operand;
+      odintsov::Operator oper;
+    } data_;
+  };
+}
+
+#endif
