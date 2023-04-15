4ce03794d2c6c5820b87e13156a66f6f8ff51c0f
ArsDi <abstractlevl@gmail.com>
2023-04-15 19:45:55 +0300

    functions tralization for constr

--- dmitriev.arseny/S1/operator.cpp
+++ dmitriev.arseny/S1/operator.cpp
@@ -3,6 +3,12 @@
 #include <iomanip>
 #include <limits>
 
+long long plus(long long p1, long long p2);
+long long minus(long long p1, long long p2);
+long long multiple(long long p1, long long p2);
+long long division(long long p1, long long p2);
+long long mod(long long p1, long long p2);
+
 auto defineOperator(char op)
 {
 	if (op == '+')
@@ -37,4 +43,69 @@ Operator::Operator(char op) :
 	priority(definePriority(op))
 {
 
+}
+
+long long plus(long long p1, long long p2)
+{
+	if (p1 > max - p2 && p2 > 0)
+	{
+		throw std::overflow_error("overflow_error");
+	}
+	if (p1 < min - p2 && p2 < 0)
+	{
+		throw std::underflow_error("underflow_error");
+	}
+
+	return p2 + p1;
+}
+
+long long minus(long long p1, long long p2)
+{
+	if (p2 > max + p1 && p1 < 0)
+	{
+		throw std::overflow_error("overflow_error");
+	}
+	if (p2 < min + p1 && p1 > 0)
+	{
+		throw std::underflow_error("underflow_error");
+	}
+
+	return p2 - p1;
+}
+
+long long multiple(long long p1, long long p2)
+{
+	if ((p1 > max / p2 && p2 > 0) || (p1 > max / p2 && p2 < 0))
+	{
+		throw std::overflow_error("overflow_error");
+	}
+	if ((p1 > min / p2 && p2 < 0) || (p1 > max / p2 && p2 > 0))
+	{
+		throw std::underflow_error("underflow_error");
+	}
+
+	return p2 * p1;
+}
+
+long long division(long long p1, long long p2)
+{
+	if (p1 == 0)
+	{
+		throw std::logic_error("div on zero");
+	}
+
+	return p2 / p1;
+}
+
+long long mod(long long p1, long long p2)
+{
+	if (p1 == 0)
+	{
+		throw std::logic_error("mod on zero");
+	}
+	if ((p2 % p1) < 0)
+	{
+		return (p2 % p1) + p1;
+	}
+	return p2 % p1;
 }
