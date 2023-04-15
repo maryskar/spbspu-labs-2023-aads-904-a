b33510df2358ff4e8d893c7c6fb1b1b30d2a12aa
ArsDi <abstractlevl@gmail.com>
2023-04-15 19:36:57 +0300

    probably first header realiz

---
+++ dmitriev.arseny/S1/operator.h
@@ -0,0 +1,20 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+#include "mathExpression.h"
+
+class Operator : public Expression
+{
+public:
+	explicit Operator(char op);
+	char getCondition() override;
+	char getPriority() const;
+	long long calculate(const long long& p1, const long long& p2);
+
+private:
+	long long (*mathOperator)(long long, long long);
+	char priority;
+
+};
+
+#endif
