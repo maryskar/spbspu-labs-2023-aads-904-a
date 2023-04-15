ea37d4392fb9dc73ea87b575ce0598ba67b04a68
Машкин Андрей <beedzibood@gmail.com>
2023-04-15 22:53:04 +0300

    added nodeList.h and improved iterator.h

--- mashkin.andrew/S2/forwardList.h
+++ mashkin.andrew/S2/forwardList.h
@@ -1,14 +1,41 @@
 #ifndef FORWARDLIST_H
 #define FORWARDLIST_H
+#include "iterator.h"
+#include "nodeList.h"
+
+namespace mashkin
+{
+  template< class T >
+  class ForwardList
+  {
+  public:
+    ForwardList();
+    Iterator< T > begin() noexcept;
+    Iterator< T > end() noexcept;
+    void push();
+    T pop();
+
+    bool isEmpty() const;
+
+  private:
+    nodeList< T >* head;
+    nodeList< T >* tail;
+  };
+}
 
 template< class T >
-class ForwardList
+mashkin::ForwardList< T >::ForwardList()
 {
-public:
-  T push();
-  T pop();
-private:
-  T data;
-  ForwardList< T >* next;
-};
+}
+
+template< class T >
+void mashkin::ForwardList< T >::push()
+{
+
+}
+
+template< class T >
+bool mashkin::ForwardList< T >::isEmpty() const
+{
+}
 #endif
---
+++ mashkin.andrew/S2/iterator.h
@@ -0,0 +1,91 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+#include <cassert>
+#include <memory>
+#include "forwardList.h"
+
+namespace mashkin
+{
+  template< class T >
+  struct Iterator
+  {
+    ForwardList< T >* node;
+
+    Iterator();
+    ~Iterator() = default;
+    Iterator(const Iterator< T >&) = default;
+
+    Iterator< T >& operator=(const ForwardList< T >&) = default;
+    Iterator< T >& operator++();
+    Iterator< T > operator++(int);
+
+    T& operator*();
+    T& operator*() const;
+    T* operator->();
+    T* operator->() const;
+
+    bool operator!=(const ForwardList< T >& rhs) const;
+    bool operator==(const ForwardList< T >& rhs) const;
+  };
+}
+
+template< class T >
+mashkin::Iterator< T >::Iterator():
+  node(nullptr)
+{
+}
+
+template< class T >
+mashkin::Iterator< T >& mashkin::Iterator< T >::operator++()
+{
+  assert(node != nullptr);
+  node = node->next;
+}
+
+template< class T >
+mashkin::Iterator< T > mashkin::Iterator< T >::operator++(int)
+{
+  assert(node != nullptr);
+  Iterator< T > res(*this);
+  ++(*this);
+  return res;
+}
+
+template< class T >
+bool mashkin::Iterator< T >::operator==(const ForwardList< T >& rhs) const
+{
+  return node == rhs.node;
+}
+
+template< class T >
+bool mashkin::Iterator< T >::operator!=(const ForwardList< T >& rhs) const
+{
+  return !(rhs == *this);
+}
+
+template< class T >
+T& mashkin::Iterator< T >::operator*()
+{
+  assert(node != nullptr);
+  return node->data;
+}
+
+template< class T >
+T* mashkin::Iterator< T >::operator->()
+{
+  assert(node != nullptr);
+  return std::addressof(node->data);
+}
+
+template< class T >
+T& mashkin::Iterator< T >::operator*() const
+{
+  return this->operator*();
+}
+
+template< class T >
+T* mashkin::Iterator< T >::operator->() const
+{
+  return this->operator->();
+}
+#endif
