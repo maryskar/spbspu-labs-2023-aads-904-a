b311bb31a142b21291d125f557642cad4c045e46
xeniaTa <kseniatarasenko1@gmail.com>
2023-04-16 11:46:58 +0300

    add struct Pair

--- tarasenko.ksenia/S2/dictionary.h
+++ tarasenko.ksenia/S2/dictionary.h
@@ -1,8 +1,49 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
-
+#include "forward_list.h"
 namespace tarasenko
 {
+  template< typename Key, typename Value >
+  struct Pair
+  {
+    Key key_;
+    Value value_;
+
+    Pair()
+    {};
+
+    Pair(const Key& key):
+      key_(key)
+    {};
+
+    Pair(const Key& key, const Value& val):
+      key_(key),
+      value_(val)
+    {};
+
+    Pair(const Pair& p):
+      key_(p.key_),
+      value_(p.value_)
+    {}
+
+    Pair< Key, Value >& operator=(const Pair& p)
+    {
+      key_ = p.key_;
+      value_ = p.value_;
+      return *this;
+    }
+
+    bool operator<(const Pair< Key, Value >& p) const
+    {
+      return key_ < p.key_;
+    }
+
+    bool operator==(const Pair< Key, Value >& p) const
+    {
+      return key_ == p.key_;
+    }
+  };
+
   template< typename Key, typename Value, typename Compare >
   class Dictionary
   {
@@ -14,7 +55,7 @@ namespace tarasenko
    //Value find(Key& k);
    //void remove(Key& k);
   private:
-   ForwardList <Pair> list;
+   ForwardList< Pair< Key, Value > > list;
   };
 
   template< typename Key, typename Value, typename Compare >
@@ -22,11 +63,13 @@ namespace tarasenko
   {
 
   };
+
   template< typename Key, typename Value, typename Compare >
   Value Dictionary< Key, Value, Compare >::get(Key& k)
   {
 
   };
+
   template< typename Key, typename Value, typename Compare >
   Value Dictionary< Key, Value, Compare >::drop(Key& k)
   {
