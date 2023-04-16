b1f5096139c1eda8d588c646ac2df50ea0f0e698
xeniaTa <kseniatarasenko1@gmail.com>
2023-04-16 11:39:14 +0300

    fix dictionary

--- tarasenko.ksenia/S2/dictionary.h
+++ tarasenko.ksenia/S2/dictionary.h
@@ -1,14 +1,37 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
-template< typename Key, typename Value, typename Compare >
-class Dictionary
+namespace tarasenko
 {
-public:
- void push(Key k, Value v);
- Value get(Key k);
- Value drop(Key k);
-//...
-};
+  template< typename Key, typename Value, typename Compare >
+  class Dictionary
+  {
+  public:
+   void push(Key& k, Value& v);
+   Value get(Key& k);
+   Value drop(Key& k); // !!!
+   //...
+   //Value find(Key& k);
+   //void remove(Key& k);
+  private:
+   ForwardList <Pair> list;
+  };
+
+  template< typename Key, typename Value, typename Compare >
+  void Dictionary< Key, Value, Compare >::push(Key& k, Value& v)
+  {
+
+  };
+  template< typename Key, typename Value, typename Compare >
+  Value Dictionary< Key, Value, Compare >::get(Key& k)
+  {
+
+  };
+  template< typename Key, typename Value, typename Compare >
+  Value Dictionary< Key, Value, Compare >::drop(Key& k)
+  {
+
+  };
+}
 
 #endif
