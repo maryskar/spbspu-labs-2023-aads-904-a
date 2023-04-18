7338886e3d111acfb017f49ac5ab5e85d21fc428
Chemodurovart <chemodurovart@gmail.com>
2023-04-18 09:10:43 +0300

    some changes in list.hpp

--- chemodurov.artem/common/list.hpp
+++ chemodurov.artem/common/list.hpp
@@ -1,5 +1,6 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+#include <utility>
 
 namespace chemodurov
 {
@@ -19,6 +20,36 @@ namespace chemodurov
       delete head;
       head = temp;
     }
+  }
+
+  template< typename T >
+  std::pair< List< T > *, List< T > * > copyList(const List< T > * head)
+  {
+    if (!head)
+    {
+      return {nullptr, nullptr};
+    }
+
+    List< T > * source = head;
+    List< T > * dest = new List< T >{source->data, nullptr};
+    List< T > * temp = dest;
+    List< T > * dest_last = temp;
+    try
+    {
+      while (source->next)
+      {
+        source = source->next;
+        dest_last = temp;
+        temp->next = new List< T >{source->data, nullptr};
+        temp = temp->next;
+      }
+    }
+    catch (...)
+    {
+      deleteList(dest);
+      throw;
+    }
+    return {dest, dest_last};
   }
 }
 
