e8085e0ccb26ba67aedac2d1eb5720d5182d0806
Chemodurovart <chemodurovart@gmail.com>
2023-04-18 09:06:34 +0300

    add some logic in main

--- chemodurov.artem/S2/main.cpp
+++ chemodurov.artem/S2/main.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char ** argv)
 {
+  if (argc != 2)
+  {
+    std::cerr << "There is no file in parameters\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  if (!input.is_open())
+  {
+    std::cerr << "The file doesn't opened\n";
+    return 1;
+  }
+
   return 0;
 }
