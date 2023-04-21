6d6f34e8a942864e78443b86ca19c037b80755aa
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-04-21 19:56:54 +0300

    update git ignore

---
+++ shelkovnikov.dmitrii/S2/cmake-build-debug-wsl/CMakeFiles/S2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/S2.dir/main.cpp.o"
+  "CMakeFiles/S2.dir/main.cpp.o.d"
+  "S2"
+  "S2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/S2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
