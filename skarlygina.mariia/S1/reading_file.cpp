5fe3325cc5012145688ccd34925f3d50e5ad6ba7
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-04-25 12:19:55 +0300

    changed spaces

--- skarlygina.mariia/S1/reading_file.cpp
+++ skarlygina.mariia/S1/reading_file.cpp
@@ -12,18 +12,18 @@ List< T > readElements(std::ifstream file)
   if (file.is_open())
   {
     while (getline(file, line))
-	{
-	  size_t pos = 0;
-	  std::string token;
-	  while ((pos = line.find(" ") != std::string::npos))
-	  {
-	    token = line.substr(0, pos);
-		list->data = token;
-		line.erase(0, pos + 1);
-	  }
-	  //adding the last elem later
-	}
-	file.close();
+    {
+      size_t pos = 0;
+      std::string token;
+      while ((pos = line.find(" ") != std::string::npos))
+      {
+        token = line.substr(0, pos);
+        list->data = token;
+        line.erase(0, pos + 1);
+      }
+      //adding the last elem later
+    }
+    file.close();
   }
   //change list into stack or queue
   return list;
