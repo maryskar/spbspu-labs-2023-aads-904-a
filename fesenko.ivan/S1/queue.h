9c8b9e8a794135526e816775ea6d6af6e2d177bc
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-04-25 17:23:34 +0300

    Add Queue interface

--- fesenko.ivan/S1/queue.h
+++ fesenko.ivan/S1/queue.h
@@ -1,10 +1,21 @@
 #ifndef QUEUE_H
 #define QUEUE_H
-template < typename T >
-class Queue
+#include "list.h"
+namespace fesenko
 {
- public:
-  void push(T rhs);
-  T drop();
-};
+  template < typename T >
+  class Queue
+  {
+   public:
+    Queue();
+    ~Queue();
+    void push(const T rhs);
+    T &getOutOfQueue();
+    void pop();
+    bool isEmpty();
+   private:
+    List< T > *head_;
+    List< T > *tail_;
+  };
+}
 #endif
