c8329ffd359bba4f5135e6e2aba36ee157534b7e
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-04-25 17:34:16 +0300

    Add ~Queue realization

--- fesenko.ivan/S1/queue.h
+++ fesenko.ivan/S1/queue.h
@@ -24,4 +24,16 @@ fesenko::Queue< T >::Queue():
   head_(nullptr),
   tail_(nullptr)
 {}
+
+template< typename T >
+fesenko::Queue< T >::~Queue()
+{
+  while (head_) {
+    List< T > *temp = head_->next;
+    delete head_;
+    head_ = temp;
+  }
+  head_ = nullptr;
+  tail_ = nullptr;
+}
 #endif
