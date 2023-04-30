5d415fedf07cc7ac8f2cdb3d85abd996a6cb568e
Artem <artrom170@gmail.com>
2023-04-30 18:19:34 +0300

    add files

---
+++ romanovich.artem/S1/operations.cpp
@@ -0,0 +1,5 @@
+#include "operations.h"
+char as_char(operations_t operation)
+{
+  return static_cast<char>(operation);
+}
---
+++ romanovich.artem/S1/parenthesis.cpp
@@ -0,0 +1,5 @@
+#include "parenthesis.h"
+char as_char(parenthesis_t parenthesis)
+{
+  return static_cast<char>(parenthesis);
+}
