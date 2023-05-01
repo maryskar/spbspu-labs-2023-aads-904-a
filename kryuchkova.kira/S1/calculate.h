66f548389c88c4c1674201d37ea475621988b378
KiraGelaz <kiragelaz@yandex.ru>
2023-05-02 00:55:20 +0300

    start doing calculate functions...

---
+++ kryuchkova.kira/S1/calculate.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+
+namespace kryuchkova
+{
+  bool isSameSign(long long lhs, long long rhs);
+  long long calcSum(long long lhs, long long rhs);
+  long long calcDiff(long long lhs, long long rhs);
+  long long calcMulti(long long lhs, long long rhs);
+  long long calcDiv(long long lhs, long long rhs);
+  long long calcEucDiv(long long lhs, long long rhs);
+}
+
+#endif
---
+++ kryuchkova.kira/S1/get_result.h
@@ -0,0 +1,9 @@
+#ifndef GET_RESULT_H
+#define GET_RESULT_H
+
+namespace kryuchkova
+{
+  long long getResult(Queue< ExpressionMember > &post);
+}
+
+#endif
