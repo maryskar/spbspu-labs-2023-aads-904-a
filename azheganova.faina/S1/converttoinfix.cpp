f241ddd72e254720e40464996f631b86916ebda4
Faina <azheganova.fl@edu.spbstu.ru>
2023-05-03 00:06:00 +0300

    change int to size_t

--- azheganova.faina/S1/converttoinfix.cpp
+++ azheganova.faina/S1/converttoinfix.cpp
@@ -4,8 +4,8 @@ azheganova::Queue< std::string > convertToInfix(const std::string & string)
 {
   azheganova::Queue< std::string > infix;
   size_t begin = 0;
-  int end = string.find(' ');
-  while (end >= 0)
+  size_t end = string.find(' ');
+  while (end + 1 > 0)
   {
     infix.push(string.substr(begin, end - begin));
     begin = end + 1;
