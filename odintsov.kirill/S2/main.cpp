7c4bc300c2b39fb505c6e23a2935cee2dc853a35
Kirill Odintsov <ksodintsov03@gmail.com>
2023-05-05 16:50:05 +0300

    Main union command +exception catching

--- odintsov.kirill/S2/main.cpp
+++ odintsov.kirill/S2/main.cpp
@@ -60,64 +60,94 @@ int main(int argc, char* argv[])
     }
     odintsov::StringSplitter split(commandLine);
     std::string commandName;
-    split >> commandName;
-    if (commandName == "print") {
-      std::string dataSetName;
-      split >> dataSetName;
-      std::cout << dataSetName;
-      odintsov::Dictionary< int, std::string > dataSet = dataSetDict[dataSetName];
-      if (dataSet.empty()) {
-        std::cout << " <EMPTY>\n";
-      } else {
-        for (odintsov::Dictionary< int, std::string >::ConstIter i = dataSet.cbegin(); i != dataSet.cend(); ++i) {
-          std::cout << ' ' << i->first << ' ' << i->second;
+    try {
+      split >> commandName;
+      if (commandName == "print") {
+        std::string dataSetName;
+        split >> dataSetName;
+        std::cout << dataSetName;
+        odintsov::Dictionary< int, std::string > dataSet = dataSetDict[dataSetName];
+        if (dataSet.empty()) {
+          std::cout << " <EMPTY>\n";
+        } else {
+          for (odintsov::Dictionary< int, std::string >::ConstIter i = dataSet.cbegin(); i != dataSet.cend(); ++i) {
+            std::cout << ' ' << i->first << ' ' << i->second;
+          }
+          std::cout << '\n';
         }
-        std::cout << '\n';
-      }
-    } else if (commandName == "complement") {
-      std::string outDataSetName;
-      std::string inDataSet1Name;
-      std::string inDataSet2Name;
-      split >> outDataSetName >> inDataSet1Name >> inDataSet2Name;
-      odintsov::Dictionary< int, std::string >& outDataSet = dataSetDict[outDataSetName];
-      const odintsov::Dictionary< int, std::string >& inDataSet1 = dataSetDict.at(inDataSet1Name);
-      const odintsov::Dictionary< int, std::string >& inDataSet2 = dataSetDict.at(inDataSet2Name);
-      odintsov::Dictionary< int, std::string >::ConstIter set1Iter = inDataSet1.cbegin();
-      odintsov::Dictionary< int, std::string >::ConstIter set2Iter = inDataSet2.cbegin();
-      auto comp = inDataSet1.keyComp();
-      for (; set1Iter != inDataSet1.cend(); ++set1Iter) {
-        while (set2Iter != inDataSet2.cend() && comp(set2Iter->first, set1Iter->first)) {
+      } else if (commandName == "complement") {
+        std::string outDataSetName;
+        std::string inDataSet1Name;
+        std::string inDataSet2Name;
+        split >> outDataSetName >> inDataSet1Name >> inDataSet2Name;
+        odintsov::Dictionary< int, std::string >& outDataSet = dataSetDict[outDataSetName];
+        const odintsov::Dictionary< int, std::string >& inDataSet1 = dataSetDict.at(inDataSet1Name);
+        const odintsov::Dictionary< int, std::string >& inDataSet2 = dataSetDict.at(inDataSet2Name);
+        odintsov::Dictionary< int, std::string >::ConstIter set1Iter = inDataSet1.cbegin();
+        odintsov::Dictionary< int, std::string >::ConstIter set2Iter = inDataSet2.cbegin();
+        auto comp = inDataSet1.keyComp();
+        for (; set1Iter != inDataSet1.cend(); ++set1Iter) {
+          while (set2Iter != inDataSet2.cend() && comp(set2Iter->first, set1Iter->first)) {
+            outDataSet.insert(*set2Iter);
+            ++set2Iter;
+          }
+          if (set2Iter == inDataSet2.cend() || set2Iter->first != set1Iter->first) {
+            outDataSet.insert(*set1Iter);
+          } else {
+            ++set2Iter;
+          }
+        }
+      } else if (commandName == "intersect") {
+        std::string outDataSetName;
+        std::string inDataSet1Name;
+        std::string inDataSet2Name;
+        split >> outDataSetName >> inDataSet1Name >> inDataSet2Name;
+        odintsov::Dictionary< int, std::string >& outDataSet = dataSetDict[outDataSetName];
+        const odintsov::Dictionary< int, std::string >& inDataSet1 = dataSetDict.at(inDataSet1Name);
+        const odintsov::Dictionary< int, std::string >& inDataSet2 = dataSetDict.at(inDataSet2Name);
+        odintsov::Dictionary< int, std::string >::ConstIter set1Iter = inDataSet1.cbegin();
+        odintsov::Dictionary< int, std::string >::ConstIter set2Iter = inDataSet2.cbegin();
+        auto comp = inDataSet1.keyComp();
+        for (; set1Iter != inDataSet1.cend(); ++set1Iter) {
+          while (set2Iter != inDataSet2.cend() && comp(set2Iter->first, set1Iter->first)) {
+            ++set2Iter;
+          }
+          if (set2Iter == inDataSet2.cend()) {
+            break;
+          }
+          if (set2Iter->first == set1Iter->first) {
+            outDataSet.insert(*set1Iter);
+          }
+        }
+      } else if (commandName == "union") {
+        std::string outDataSetName;
+        std::string inDataSet1Name;
+        std::string inDataSet2Name;
+        split >> outDataSetName >> inDataSet1Name >> inDataSet2Name;
+        odintsov::Dictionary< int, std::string >& outDataSet = dataSetDict[outDataSetName];
+        const odintsov::Dictionary< int, std::string >& inDataSet1 = dataSetDict.at(inDataSet1Name);
+        const odintsov::Dictionary< int, std::string >& inDataSet2 = dataSetDict.at(inDataSet2Name);
+        odintsov::Dictionary< int, std::string >::ConstIter set1Iter = inDataSet1.cbegin();
+        odintsov::Dictionary< int, std::string >::ConstIter set2Iter = inDataSet2.cbegin();
+        auto comp = inDataSet1.keyComp();
+        for (; set1Iter != inDataSet1.cend(); ++set1Iter) {
+          while (set2Iter != inDataSet2.cend() && comp(set2Iter->first, set1Iter->first)) {
+            outDataSet.insert(*set2Iter);
+            ++set2Iter;
+          }
+          outDataSet.insert(*set1Iter);
+        }
+        while (set2Iter != inDataSet2.cend()) {
           outDataSet.insert(*set2Iter);
           ++set2Iter;
         }
-        if (set2Iter == inDataSet2.cend() || set2Iter->first != set1Iter->first) {
-          outDataSet.insert(*set1Iter);
-        } else {
-          ++set2Iter;
-        }
       }
-    } else if (commandName == "intersect") {
-      std::string outDataSetName;
-      std::string inDataSet1Name;
-      std::string inDataSet2Name;
-      split >> outDataSetName >> inDataSet1Name >> inDataSet2Name;
-      odintsov::Dictionary< int, std::string >& outDataSet = dataSetDict[outDataSetName];
-      const odintsov::Dictionary< int, std::string >& inDataSet1 = dataSetDict.at(inDataSet1Name);
-      const odintsov::Dictionary< int, std::string >& inDataSet2 = dataSetDict.at(inDataSet2Name);
-      odintsov::Dictionary< int, std::string >::ConstIter set1Iter = inDataSet1.cbegin();
-      odintsov::Dictionary< int, std::string >::ConstIter set2Iter = inDataSet2.cbegin();
-      auto comp = inDataSet1.keyComp();
-      for (; set1Iter != inDataSet1.cend(); ++set1Iter) {
-        while (set2Iter != inDataSet2.cend() && comp(set2Iter->first, set1Iter->first)) {
-          ++set2Iter;
-        }
-        if (set2Iter == inDataSet2.cend()) {
-          break;
-        }
-        if (set2Iter->first == set1Iter->first) {
-          outDataSet.insert(*set1Iter);
-        }
-      }
+    } catch (const std::runtime_error& e) {
+      std::cerr << "<INVALID COMMAND>\n";
+      continue;
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
+      return 2;
     }
   }
 }
