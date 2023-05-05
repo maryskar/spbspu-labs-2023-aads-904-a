564715bd1a5ae332ef0f36bdc075666847644aa0
Машкин Андрей <beedzibood@gmail.com>
2023-05-05 21:19:29 +0300

    changed to constexpr

--- mashkin.andrew/S1/solving.cpp
+++ mashkin.andrew/S1/solving.cpp
@@ -14,8 +14,8 @@ bool same_sign(int long long a, int long long b)
 
 int long long mashkin::sum(int long long a, int long long b)
 {
-  const int long long MAX = std::numeric_limits< int long long >::max();
-  const int long long MIN = std::numeric_limits< int long long >::min();
+  constexpr int long long MAX = std::numeric_limits< int long long >::max();
+  constexpr int long long MIN = std::numeric_limits< int long long >::min();
   if (same_sign(a, b) && (a >= 0))
   {
     if (MAX - a >= b)
