9ad4653b6ecdd588c4ce28aad6175637a7269545
Машкин Андрей <beedzibood@gmail.com>
2023-05-06 22:57:31 +0300

    renamed to constexpr in multiplicate function

--- mashkin.andrew/S1/solving.cpp
+++ mashkin.andrew/S1/solving.cpp
@@ -46,8 +46,8 @@ int long long mashkin::sum(int long long a, int long long b)
 
 int long long mashkin::multiplicate(long long a, long long b)
 {
-  const int long long MAX = std::numeric_limits< int long long >::max();
-  const int long long MIN = std::numeric_limits< int long long >::min();
+  constexpr int long long MAX = std::numeric_limits< int long long >::max();
+  constexpr int long long MIN = std::numeric_limits< int long long >::min();
   if (same_sign(a, b) && (a >= 0))
   {
     if (MAX / a >= b)
