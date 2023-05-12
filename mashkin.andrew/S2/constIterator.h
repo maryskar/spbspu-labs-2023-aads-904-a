c5b80f430f9811d49aeba7bd873e3e93f39745ae
Машкин Андрей <beedzibood@gmail.com>
2023-05-12 22:12:46 +0300

    declared constIterator

---
+++ mashkin.andrew/S2/constIterator.h
@@ -0,0 +1,78 @@
+#ifndef S2_CONSTITERATOR_H
+#define S2_CONSTITERATOR_H
+#include <cassert>
+#include <memory>
+#include "forwardList.h"
+
+namespace mashkin
+{
+  template< class T >
+  struct ConstIterator: public std::iterator< std::forward_iterator_tag, T >
+  {
+    NodeList< T >* node;
+
+    ConstIterator();
+    ~ConstIterator() = default;
+    ConstIterator(const ConstIterator< T >&) = default;
+
+    ConstIterator< T >& operator=(const ConstIterator< T >&) = default;
+    ConstIterator< T >& operator++();
+    ConstIterator< T > operator++(int);
+
+    T& operator*() const;
+    T* operator->() const;
+
+    bool operator!=(const ConstIterator< T >& rhs) const;
+    bool operator==(const ConstIterator< T >& rhs) const;
+  };
+}
+
+template< class T >
+mashkin::ConstIterator< T >::ConstIterator():
+  node(nullptr)
+{
+}
+
+template< class T >
+mashkin::ConstIterator< T >& mashkin::ConstIterator< T >::operator++()
+{
+  assert(node != nullptr);
+  node = node->next;
+}
+
+template< class T >
+mashkin::ConstIterator< T > mashkin::ConstIterator< T >::operator++(int)
+{
+  assert(node != nullptr);
+  Iterator< T > res(*this);
+  ++(*this);
+  return res;
+}
+
+template< class T >
+bool mashkin::ConstIterator< T >::operator==(const ConstIterator< T >& rhs) const
+{
+  return node == rhs.node;
+}
+
+template< class T >
+bool mashkin::ConstIterator< T >::operator!=(const ConstIterator< T >& rhs) const
+{
+  return !(rhs == *this);
+}
+
+template< class T >
+T& mashkin::ConstIterator< T >::operator*() const
+{
+  assert(node != nullptr);
+  return node->data;
+}
+
+template< class T >
+T* mashkin::ConstIterator< T >::operator->() const
+{
+  assert(node != nullptr);
+  return std::addressof(node->data);
+}
+
+#endif
