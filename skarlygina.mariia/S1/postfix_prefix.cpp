53e94410d1edd53d9bdb7abfc4e28801903db78f
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-05-15 19:49:51 +0300

    deleted

--- skarlygina.mariia/S1/postfix_prefix.cpp
+++
@@ -1,4 +0,0 @@
-#include "postfix_prefix.h"
-
-Queue< std::string > operation::makePostfix(Queue< std::string >& infix_queue);
-Queue< std::string > operation::makePrefix(Queue< std::string >& infix_queue);
