18975b56fd7d45a5eca9dc933574e0c921f11e3f
Chemodurovart <chemodurovart@gmail.com>
2023-05-16 10:46:11 +0300

    add bidirectional-iterator.hpp

---
+++ chemodurov.artem/S4/bidirectional-iterator.hpp
@@ -0,0 +1,69 @@
+#ifndef S4_BIDIRECTIONAL_ITERATOR_HPP
+#define S4_BIDIRECTIONAL_ITERATOR_HPP
+#include <cassert>
+#include "tree.hpp"
+
+namespace chemodurov
+{
+  template< typename T >
+  class BidirectionalIterator
+  {
+   public:
+    using this_t = BidirectionalIterator< T >;
+    BidirectionalIterator();
+    BidirectionalIterator(const this_t &) = default;
+    ~BidirectionalIterator() = default;
+    this_t & operator=(const this_t &) = default;
+    this_t & operator++();
+    this_t operator++(int);
+    this_t & operator--();
+    this_t operator--(int);
+    T & operator*();
+    const T & operator*() const;
+    T * operator->();
+    T * operator->() const;
+   private:
+    Tree< T > * node_;
+    Tree< T > * fake_;
+  };
+
+  template< typename T >
+  BidirectionalIterator< T >::BidirectionalIterator():
+   node_(nullptr),
+   fake_(nullptr)
+  {}
+
+  template< typename T >
+  BidirectionalIterator< T > & BidirectionalIterator< T >::operator++()
+  {
+    assert(node_ != nullptr);
+    Tree< T > * previous = node_;
+    if (node_->right != fake_)
+    {
+      node_= node_->right;
+      return *this;
+    }
+    node_ = node_->parent;
+    while (node_ != fake_ && node_->right == previous)
+    {
+      previous = node_;
+      node_ = node_->parent;
+    }
+    return *this;
+  }
+  template< typename T >
+  BidirectionalIterator< T > BidirectionalIterator< T >::operator++(int)
+  {
+    assert(node_ != nullptr);
+    this_t temp(*this);
+    ++(*this);
+    return temp;
+  }
+  template< typename T >
+  BidirectionalIterator< T > & BidirectionalIterator< T >::operator--()
+  {
+    assert(node_ != nullptr);
+    //
+  }
+}
+#endif
--- chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
+++ chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
@@ -1,18 +1,20 @@
 #ifndef S4_UNBALANCED_BINARY_SEARCH_TREE_HPP
 #define S4_UNBALANCED_BINARY_SEARCH_TREE_HPP
 #include <functional>
-#include "tree.hpp"
+#include "bidirectional-iterator.hpp"
 
 namespace chemodurov
 {
   template< typename T, typename Compare = std::less<> >
   class UnbalancedBinarySearchTree
   {
+    friend class BidirectionalIterator< T >;
    public:
 
    private:
     Tree< T, Compare > * fake_;
     Compare comp_;
+    std::size_t size_;
   };
 }
 
