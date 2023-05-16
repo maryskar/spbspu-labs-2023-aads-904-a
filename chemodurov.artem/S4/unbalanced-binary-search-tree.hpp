f1cf8b33729f933651956daee0f3edab868abb0f
Chemodurovart <chemodurovart@gmail.com>
2023-05-16 23:14:06 +0300

    add some interface for UBST

--- chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
+++ chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
@@ -10,7 +10,26 @@ namespace chemodurov
   {
     friend class BidirectionalIterator< T >;
    public:
-
+    using value_type = T;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using key_compare = Compare;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator = BidirectionalIterator< value_type >;
+    using const_iterator = ConstBidirectionalIterator< value_type >;
+    using this_t = UnbalancedBinarySearchTree< value_type, key_compare >;
+    UnbalancedBinarySearchTree();
+    UnbalancedBinarySearchTree(const this_t & other);
+    UnbalancedBinarySearchTree(this_t && other);
+    UnbalancedBinarySearchTree(const key_compare & comp);
+    template< typename InputIt >
+    UnbalancedBinarySearchTree(InputIt first, InputIt last, const key_compare & comp = key_compare());
+    UnbalancedBinarySearchTree(std::initializer_list< value_type > init, const key_compare & comp = key_compare());
+    ~UnbalancedBinarySearchTree();
+    this_t & operator=(const this_t & other);
+    this_t & operator=(this_t && other);
+    this_t & operator=(std::initializer_list< value_type > init);
    private:
     Tree< T, Compare > * fake_;
     Compare comp_;
