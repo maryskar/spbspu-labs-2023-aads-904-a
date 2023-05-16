9a7b06e948d54275d42cf9b7a037237cc91f06ab
Chemodurovart <chemodurovart@gmail.com>
2023-05-16 09:38:31 +0300

    add tree

---
+++ chemodurov.artem/S4/tree.hpp
@@ -0,0 +1,14 @@
+#ifndef S4_TREE_HPP
+#define S4_TREE_HPP
+#include <functional>
+
+template< typename T, typename Compare = std::less< > >
+struct Tree
+{
+  T data;
+  Tree< T, Compare > * left;
+  Tree< T, Compare > * right;
+  Tree< T, Compare > * parent;
+};
+
+#endif
---
+++ chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
@@ -0,0 +1,13 @@
+#ifndef S4_UNBALANCED_BINARY_SEARCH_TREE_HPP
+#define S4_UNBALANCED_BINARY_SEARCH_TREE_HPP
+#include <functional>
+
+template< typename Key, typename Value, typename Compare = std::less< > >
+class UnbalancedBinarySearchTree
+{
+ public:
+ private:
+
+};
+
+#endif
