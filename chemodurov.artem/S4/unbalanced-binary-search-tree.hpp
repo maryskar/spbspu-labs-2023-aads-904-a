c0a21b6d0d6269031c7f95da84bf99a86c138089
Chemodurovart <chemodurovart@gmail.com>
2023-05-17 20:15:08 +0300

    add all interface for UBST

--- chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
+++ chemodurov.artem/S4/unbalanced-binary-search-tree.hpp
@@ -13,23 +13,64 @@ namespace chemodurov
     using value_type = T;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
-    using key_compare = Compare;
+    using value_compare = Compare;
     using reference = value_type &;
     using const_reference = const value_type &;
     using iterator = BidirectionalIterator< value_type >;
     using const_iterator = ConstBidirectionalIterator< value_type >;
-    using this_t = UnbalancedBinarySearchTree< value_type, key_compare >;
+    using this_t = UnbalancedBinarySearchTree< value_type, value_compare >;
     UnbalancedBinarySearchTree();
     UnbalancedBinarySearchTree(const this_t & other);
     UnbalancedBinarySearchTree(this_t && other);
-    UnbalancedBinarySearchTree(const key_compare & comp);
+    UnbalancedBinarySearchTree(const value_compare & comp);
     template< typename InputIt >
-    UnbalancedBinarySearchTree(InputIt first, InputIt last, const key_compare & comp = key_compare());
-    UnbalancedBinarySearchTree(std::initializer_list< value_type > init, const key_compare & comp = key_compare());
+    UnbalancedBinarySearchTree(InputIt first, InputIt last, const value_compare & comp = value_compare());
+    UnbalancedBinarySearchTree(std::initializer_list< value_type > init, const value_compare & comp = value_compare());
     ~UnbalancedBinarySearchTree();
     this_t & operator=(const this_t & other);
     this_t & operator=(this_t && other);
     this_t & operator=(std::initializer_list< value_type > init);
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    const_iterator cbegin() const noexcept;
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
+    const_iterator cend() const noexcept;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    void clear() noexcept;
+    std::pair< iterator, bool > insert(const_reference value);
+    template< typename P >
+    std::pair< iterator, bool > insert(P && value);
+    iterator insert(const_iterator pos, const_reference value);
+    template< typename P >
+    iterator insert(const_iterator pos, P && value);
+    template< typename InputIt >
+    void insert(InputIt first, InputIt last);
+    void insert(std::initializer_list< value_type > init);
+    template< typename... Args >
+    std::pair< iterator, bool > emplace(Args && ... args);
+    template < typename... Args >
+    iterator emplace_hint(const_iterator hint, Args && ... args);
+    T& at( const_reference value );
+    const T& at( const_reference value ) const;
+    T& operator[]( const_reference value );
+    T& operator[]( value_type && value );
+    iterator erase( iterator pos );
+    iterator erase( const_iterator pos );
+    iterator erase( const_iterator first, const_iterator last );
+    size_type erase( const_reference value );
+    void swap(this_t & other);
+    size_type count( const_reference value ) const;
+    iterator find( const_reference value );
+    const_iterator find( const_reference value ) const;
+    std::pair<iterator,iterator> equal_range( const_reference value );
+    std::pair<const_iterator,const_iterator> equal_range( const_reference value ) const;
+    iterator lower_bound( const_reference value);
+    const_iterator lower_bound( const_reference value ) const;
+    iterator upper_bound( const_reference value );
+    const_iterator upper_bound( const_reference value ) const;
+    value_compare value_comp() const;
    private:
     Tree< T, Compare > * fake_;
     Compare comp_;
