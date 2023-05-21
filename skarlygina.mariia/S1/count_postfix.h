49478d2faaa228f474ae647c48e6849e1b1d5b14
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-05-22 00:45:55 +0300

    added parameters

--- skarlygina.mariia/S1/count_postfix.h
+++ skarlygina.mariia/S1/count_postfix.h
@@ -1,1 +1,10 @@
-#pragma once
+#ifndef COUNT_POSTFIX_H
+#define COUNT_POSTFIX_H
+#include "queue.h"
+#include "stack.h"
+template< typename T >
+long long countPostfix(Queue< T >& postfix_queue)
+{
+
+}
+#endif
