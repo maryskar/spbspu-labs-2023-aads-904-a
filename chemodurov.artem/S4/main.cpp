a78d7607c2986a9006b02be45adc50ee1fec98c4
Chemodurovart <chemodurovart@gmail.com>
2023-05-22 21:43:44 +0300

    fixed 1.5

--- chemodurov.artem/S4/main.cpp
+++ chemodurov.artem/S4/main.cpp
@@ -3,19 +3,5 @@
 
 int main()
 {
-  chemodurov::RBTree< int > tree;
-  for (int i = 0; i < 50; ++i)
-  {
-    int a = std::rand() % 100;
-    tree.insert(a);
-    std::cout << a << ' ';
-  }
-  std::cout << '\n';
-  tree.erase(37);
-  for (auto i = tree.cbegin(); i != tree.cend(); ++i)
-  {
-    std::cout << *i << ' ';
-  }
-  std::cout << '\n';
   return 0;
 }
