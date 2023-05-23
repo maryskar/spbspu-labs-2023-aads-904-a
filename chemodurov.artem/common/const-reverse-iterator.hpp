b544b20d7dad99dcaa3e26fce8acbfe29456f50e
Chemodurovart <chemodurovart@gmail.com>
2023-05-23 18:25:43 +0300

    add reverse iterator

--- chemodurov.artem/common/const-reverse-iterator.hpp
+++ chemodurov.artem/common/const-reverse-iterator.hpp
@@ -1,19 +1,19 @@
 #ifndef S4_CONST_REVERSE_ITERATOR_HPP
 #define S4_CONST_REVERSE_ITERATOR_HPP
 #include "const-bidirectional-iterator.hpp"
+#include "reverse_iterator.hpp"
 
 namespace chemodurov
 {
   template< typename T, typename Compare = std::less< > >
   class ConstReverseIterator: public std::iterator< std::bidirectional_iterator_tag, T >
   {
-    friend class UnbalancedBinarySearchTree< T, Compare >;
-    friend class BidirectionalIterator< T, Compare >;
-    friend class RBTree< T, Compare >;
+    template< typename, typename, typename >
+    friend class Map;
    public:
     using this_t = ConstReverseIterator< T, Compare >;
     ConstReverseIterator();
-    //ConstReverseIterator(const ReverseIterator< T, Compare > & rhs);
+    ConstReverseIterator(const ReverseIterator< T, Compare > & rhs);
     ConstReverseIterator(const this_t &) = default;
     ~ConstReverseIterator() = default;
     this_t & operator=(const this_t &) = default;
@@ -31,6 +31,11 @@ namespace chemodurov
   template< typename T, typename Compare >
   ConstReverseIterator< T, Compare >::ConstReverseIterator():
     iter_()
+  {}
+
+  template< typename T, typename Compare >
+  ConstReverseIterator< T, Compare >::ConstReverseIterator(const ReverseIterator< T, Compare > & rhs):
+    iter_(rhs.iter_)
   {}
 
   template< typename T, typename Compare >
