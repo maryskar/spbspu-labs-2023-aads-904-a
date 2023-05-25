3a9b54859c5a2eb6d3d04344d8cb9f965129433c
Chemodurovart <chemodurovart@gmail.com>
2023-05-25 23:00:34 +0300

    add read-map-of-int-string

---
+++ chemodurov.artem/S5/read-map-of-int-string.cpp
@@ -0,0 +1,6 @@
+#include "read-map-of-int-string.hpp"
+
+chemodurov::Map< int, std::string > chemodurov::readMapOfIntString(std::istream & in)
+{
+
+}
---
+++ chemodurov.artem/S5/read-map-of-int-string.hpp
@@ -0,0 +1,10 @@
+#ifndef S5_READ_MAP_OF_INT_STRING_HPP
+#define S5_READ_MAP_OF_INT_STRING_HPP
+#include <map.hpp>
+
+namespace chemodurov
+{
+  Map< int, std::string > readMapOfIntString(std::istream & in);
+}
+
+#endif
