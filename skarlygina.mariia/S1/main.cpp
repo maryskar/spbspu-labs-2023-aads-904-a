194a3e3fce270f0c6e69948a5c1919b19c217d38
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-05-26 00:01:20 +0300

    main modification 1

--- skarlygina.mariia/S1/main.cpp
+++ skarlygina.mariia/S1/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include "queue.h"
+#include "stack.h"
+#include "count.h"
+#include "postfix_prefix.h"
+#include "count_postfix.h"
 
 int main(int argc, char* argv[])
 {
+  if (argc > 2)
+  {
+    std::cerr << "Error. The number of argc";
+  }
   if (argc == 2)
   {
     std::ifstream fin(argv[1]);
