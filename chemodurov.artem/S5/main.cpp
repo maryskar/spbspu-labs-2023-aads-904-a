72532d0a6322ce72ee66a1e5e0bbae46afde242e
Chemodurovart <chemodurovart@gmail.com>
2023-05-25 23:20:50 +0300

    some reading in main

--- chemodurov.artem/S5/main.cpp
+++ chemodurov.artem/S5/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include "read-map-of-int-string.hpp"
+
+bool checkArg(const char * arg)
+{
+  return !std::strcmp(arg, "ascending") || !std::strcmp(arg, "descending") || !std::strcmp(arg, "breadth");
+}
 
 int main(int argc, char ** argv)
 {
@@ -9,16 +15,19 @@ int main(int argc, char ** argv)
     std::cerr << "Incorrect amount of CML args\n";
     return 1;
   }
-  if (std::strcmp(argv[1], "ascending") != 0 && std::strcmp(argv[1], "decending") != 0 && std::strcmp(argv[1], "breadth") != 0)
+  if (!checkArg(argv[1]))
   {
     std::cerr << "Incorrect first CML arg\n";
     return 1;
   }
+
   std::ifstream input(argv[2]);
   if (!input.is_open())
   {
     std::cerr << "The file doesn't opened\n";
     return 1;
   }
+  chemodurov::Map< int, std::string > data = chemodurov::readMapOfIntString(input);
+
   return 0;
 }
