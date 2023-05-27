9f784abc2f7e2871f4d76e5a0e14f29b0dcc5d67
Shuzamada <shuzamada@inbox.ru>
2023-05-27 19:38:55 +0300

    delete std::stringstream

--- hrushchev.nikita/S1/convertstringtoinfix.cpp
+++ hrushchev.nikita/S1/convertstringtoinfix.cpp
@@ -1,19 +1,25 @@
 #include "convertstringtoinfix.hpp"
 #include <string>
-#include <cstring>
-#include <sstream>
 #include "queue.hpp"
 
-hrushchev::Queue< std::string > hrushchev::convertStringToInfix(std::string str)
+hrushchev::Queue<std::string> hrushchev::convertStringToInfix(std::string str)
 {
   namespace hrn = hrushchev;
-  hrn::Queue< std::string > infix_queue;
-  std::stringstream string_stream(str);
+  hrn::Queue<std::string> infix_queue;
+  
+  std::string delimiter = " ";
+  size_t pos = 0;
   std::string token;
-  while (string_stream >> token)
+  
+  while ((pos = str.find(delimiter)) != std::string::npos)
   {
+    token = str.substr(0, pos);
     infix_queue.push(token);
+    str.erase(0, pos + 1);
   }
+  
+  infix_queue.push(str);
+  
   return infix_queue;
 }
 
