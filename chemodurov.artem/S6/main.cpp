2546e25e4f10e79bbf6bf4cb00541b8e4fdcd634
Chemodurovart <chemodurovart@gmail.com>
2023-05-28 14:27:06 +0300

    moved list to common and test mergeSort

--- chemodurov.artem/S6/main.cpp
+++ chemodurov.artem/S6/main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include <forward-list.hpp>
+#include "merge-sort.hpp"
+
 int main()
 {
+  chemodurov::ForwardList< int > list;
+  for (size_t i = 0; i < 20; ++i)
+  {
+    list.push_front(std::rand() % 100);
+  }
+  chemodurov::mergeSort(list.begin(), 20);
+  for (auto i = list.cbegin(); i != list.cend(); ++i)
+  {
+    std::cout << *i << ' ';
+  }
+  std::cout << '\n';
   return 0;
 }
