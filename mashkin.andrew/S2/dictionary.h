3350f372c8aaacb32a46ebeaf84123b35f673a9b
Машкин Андрей <beedzibood@gmail.com>
2023-05-28 22:22:56 +0300

    defined 'push' method

--- mashkin.andrew/S2/dictionary.h
+++ mashkin.andrew/S2/dictionary.h
@@ -1,5 +1,9 @@
 #ifndef S2_DICTIONARY_H
 #define S2_DICTIONARY_H
+#include <utility>
+#include "forwardConstIterator.h"
+#include "forwardIterator.h"
+#include "forwardList.h"
 
 namespace mashkin
 {
@@ -7,9 +11,22 @@ namespace mashkin
   class Dictionary
   {
   public:
+    using key_type = Key;
+    using value_type = std::pair< const Key, Value >;
+    using iter = Iterator< Value >;
+    using const_iter = ConstIterator< Value >;
     void push(Key k, Value v);
     Value get(Key k);
-    Value drop(Key k);
+    Value& drop(Key k);
+
+  private:
+    ForwardList< value_type > pair_;
   };
+  template< class Key, class Value, class Compare >
+  void Dictionary< Key, Value, Compare >::push(Key k, Value v)
+  {
+    std::pair< Key, Value > pair{k, v};
+    pair_.push_front(pair);
+  }
 }
 #endif
