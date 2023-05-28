12eb43e9e9a72581e7739f040c12be4d4e7747c1
Artem <artrom170@gmail.com>
2023-05-28 13:50:03 +0300

    created input parser

--- romanovich.artem/S2/main.cpp
+++ romanovich.artem/S2/main.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <limits>
+#include <vector>
 #include "dict.h"
-int main(int argc, char **argv)
+std::vector< std::string > splitString(const std::string &line, char del)
 {
-  if (argc % 2 == 1)
+  std::vector< std::string > elems;
+  std::string word;
+  size_t startPos = 0;
+  size_t endPos = line.find(del);
+  while (endPos != std::string::npos)
   {
-    std::cerr << "Not appropriate parameters number.\n";
+    word = line.substr(startPos, endPos - startPos);
+    elems.push_back(word);
+    startPos = endPos + 1;
+    endPos = line.find(del, startPos);
+  }
+  word = line.substr(startPos, endPos);
+  elems.push_back(word);
+  return elems;
+}
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "No file provided.\n";
     return 1;
   }
-  std::string dict = argv[1];
-  Dictionary< int, std::string > dictionary{};
-  for (size_t i = 2; i < argc; i += 2)
+  std::ifstream input(argv[1]);
+  if (!input)
   {
-    int key = std::stoi(argv[i]);
-    std::string value = argv[i + 1];
-    //dictionary.insert(key, value);
-    ///std::cout  <<  key << " " <<   value << "\n";
+    std::cerr << "Cannot open file.\n";
+    return 1;
+  }
+  std::string line;
+  while (std::getline(input, line))
+  {
+    std::vector< std::string > lineWords = splitString(line, ' ');
+    std::string dictName = lineWords[0];
+    for (size_t i = 1; i < lineWords.size(); i += 2)
+    {
+      int key = std::stoi(lineWords[i]);
+      std::string val = lineWords[i + 1];
+      std::cout << key << " " << val << "\n";
+    }
+    /*Dictionary< int, std::string > dictionary{};
+    for (size_t i = 2; i < argc; i += 2)
+    {
+      int key = std::stoi(argv[i]);
+      std::string value = argv[i + 1];
+      //dictionary.insert(key, value);
+      std::cout << key << " " << value << "\n";
+    }*/
   }
 }
