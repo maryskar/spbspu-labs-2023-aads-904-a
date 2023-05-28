cd5851a013280fe2bb5173ae9f93ea6316b1a1ef
Chemodurovart <chemodurovart@gmail.com>
2023-05-28 11:13:22 +0300

    declare merge sort

---
+++ chemodurov.artem/S6/merge-sort.hpp
@@ -0,0 +1,14 @@
+#ifndef S6_MERGE_SORT_HPP
+#define S6_MERGE_SORT_HPP
+#include <functional>
+
+namespace chemodurov
+{
+  template< typename ForwardIt, typename Compare = std::less< > >
+  void mergeSort(ForwardIt begin, ForwardIt end, Compare comp = Compare{})
+  {
+    //
+  }
+}
+
+#endif
