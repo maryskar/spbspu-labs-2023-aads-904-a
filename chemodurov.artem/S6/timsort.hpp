1eb8975fe5722f0dcedb4ff0323a83f4446f442d
Chemodurovart <chemodurovart@gmail.com>
2023-05-29 15:21:30 +0300

    add sortInsertion for timsort

--- chemodurov.artem/S6/timsort.hpp
+++ chemodurov.artem/S6/timsort.hpp
@@ -1,6 +1,6 @@
 #ifndef S6_TIMSORT_HPP
 #define S6_TIMSORT_HPP
-#include <cstddef>
+#include <functional>
 
 namespace chemodurov
 {
@@ -15,7 +15,27 @@ namespace chemodurov
       }
       return size + r;
     }
+    template< typename RandomIt, typename Compare = std::less< > >
+    void sortInsertion(RandomIt begin, size_t size, Compare comp)
+    {
+      using value_type = std::remove_reference_t< decltype(*begin) >;
+      for (size_t i = 1; i < size; ++i)
+      {
+        value_type temp = begin[i];
+        size_t j = i;
+        while (j > 1 && comp(temp, begin[j - 1]))
+        {
+          begin[j] = begin[j - 1];
+          --j;
+        }
+        begin[j] = temp;
+      }
+    }
   }
+  template< typename RandomIt, typename Compare = std::less< > >
+  void timsort(RandomIt begin, RandomIt end, Compare comp = Compare{})
+  {
 
+  }
 }
 #endif
