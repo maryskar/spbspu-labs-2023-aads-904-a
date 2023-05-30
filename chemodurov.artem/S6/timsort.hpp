f9fb9fac38cd6f1d8b3247431b91c922d716fe95
Chemodurovart <chemodurovart@gmail.com>
2023-05-30 12:05:01 +0300

    add splitIntoSortedArrays

--- chemodurov.artem/S6/timsort.hpp
+++ chemodurov.artem/S6/timsort.hpp
@@ -32,6 +32,66 @@ namespace chemodurov
         begin[j] = temp;
       }
     }
+
+    template< typename RandomIt, typename Compare = std::less< > >
+    size_t * splitIntoSortedArrays(RandomIt begin, size_t size, Compare comp, size_t & num_of_run)
+    {
+      size_t min_run = detail::getMinRun(size);
+      num_of_run = size / min_run + 1;
+      auto runs_sizes = new size_t[num_of_run];
+      try
+      {
+        for (size_t i = 0, j = 0; i < size - 1; ++i, ++j)
+        {
+          runs_sizes[j] = 2;
+          if (comp(begin[i + 1], begin[i++]))
+          {
+            while (i < size - 1 && comp(begin[i + 1], begin[i]))
+            {
+              ++i;
+              ++(runs_sizes[j]);
+            }
+            if (runs_sizes[j] >= min_run)
+            {
+              for (size_t k = i - runs_sizes[j] + 1, l = i; k < l; ++k, --l)//??
+              {
+                std::swap(begin[k], begin[l]);
+              }
+            }
+          }
+          else
+          {
+            while (i < size - 1 && !comp(begin[i + 1], begin[i]))
+            {
+              ++i;
+              ++(runs_sizes[j]);
+            }
+          }
+          if (runs_sizes[j] < min_run)
+          {
+            size_t delta = min_run - runs_sizes[j];
+            if (i + delta < size)
+            {
+              runs_sizes[j] += delta;
+              i += delta;
+            }
+            else
+            {
+              runs_sizes[j] += size - i;
+              i += size - i;
+            }
+            detail::insertionSort(begin + i - runs_sizes[j], runs_sizes[j], comp);
+          }
+          num_of_run = j;
+        }
+      }
+      catch (...)
+      {
+        delete [] runs_sizes;
+        throw;
+      }
+      return runs_sizes;
+    }
   }
 
   template< typename RandomIt, typename Compare = std::less< > >
@@ -41,59 +101,9 @@ namespace chemodurov
     {
       return;
     }
-    size_t min_run = detail::getMinRun(size);
-    size_t num_of_runs = size / min_run + 1;
-    auto runs_sizes = new size_t[num_of_runs];
-    try
-    {
-      for (size_t i = 0, j = 0; i < size - 1; ++i)
-      {
-        runs_sizes[j] = 2;
-        if (comp(begin[i + 1], begin[i++]))
-        {
-          while (i < size - 1 && comp(begin[i + 1], begin[i]))
-          {
-            ++i;
-            ++(runs_sizes[j]);
-          }
-          if (runs_sizes[j] >= min_run)
-          {
-            for (size_t k = i - runs_sizes[j] + 1; k < (i + k) / 2; ++k)//??
-            {
-              std::swap(begin[k], begin[i - k]);
-            }
-          }
-        }
-        else
-        {
-          while (i < size - 1 && !comp(begin[i + 1], begin[i]))
-          {
-            ++i;
-            ++(runs_sizes[j]);
-          }
-        }
-        if (runs_sizes[j] < min_run)
-        {
-          size_t delta = min_run - runs_sizes[j];
-          if (i + delta < size)
-          {
-            runs_sizes[j] += delta;
-            i += delta;
-          }
-          else
-          {
-            runs_sizes[j] += size - i;
-            i += size - i;
-          }
-          detail::insertionSort(begin + i - runs_sizes[j], runs_sizes[j], comp);
-        }
-      }
-    }
-    catch (...)
-    {
-      delete [] runs_sizes;
-      throw;
-    }
+    size_t num_of_runs = 0;
+    auto runs_sizes = detail::splitIntoSortedArrays(begin, size, comp, num_of_runs);
+    //
     delete [] runs_sizes;
   }
 }
