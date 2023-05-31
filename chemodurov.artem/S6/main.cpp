4939011e4c4514d073328efc1d970df3eacb2bea
Chemodurovart <chemodurovart@gmail.com>
2023-05-31 21:34:55 +0300

    use sortAndPrintRandomValue in main

--- chemodurov.artem/S6/main.cpp
+++ chemodurov.artem/S6/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include "sort-and-print-random-value.hpp"
 
 int main(int argc, char ** argv)
 {
@@ -23,6 +24,42 @@ int main(int argc, char ** argv)
     std::cerr << "Incorrect third argument\n";
     return 1;
   }
-
+  using gen_t = std::default_random_engine;
+  gen_t gen;
+  if (!std::strcmp(argv[2], "ints"))
+  {
+    using distr_t = std::uniform_int_distribution< >;
+    distr_t distr;
+    if (!std::strcmp(argv[1], "ascending"))
+    {
+      using comp = std::less< >;
+      chemodurov::sortAndPrintRandomValue< int, comp, distr_t, gen_t >(comp{}, distr, gen, std::cout, num_of_elem);
+    }
+    else
+    {
+      using comp = std::greater< >;
+      chemodurov::sortAndPrintRandomValue< int, comp, distr_t, gen_t >(comp{}, distr, gen, std::cout, num_of_elem);
+    }
+  }
+  else if (!std::strcmp(argv[2], "floats"))
+  {
+    using distr_t = std::uniform_real_distribution< >;
+    distr_t distr;
+    if (!std::strcmp(argv[1], "ascending"))
+    {
+      using comp = std::less< >;
+      chemodurov::sortAndPrintRandomValue< double, comp, distr_t, gen_t >(comp{}, distr, gen, std::cout, num_of_elem);
+    }
+    else
+    {
+      using comp = std::greater< >;
+      chemodurov::sortAndPrintRandomValue< double, comp, distr_t, gen_t >(comp{}, distr, gen, std::cout, num_of_elem);
+    }
+  }
+  else
+  {
+    std::cerr << "Incorrect second argument\n";
+    return 1;
+  }
   return 0;
 }
--- chemodurov.artem/S6/sort-and-print-random-value.hpp
+++ chemodurov.artem/S6/sort-and-print-random-value.hpp
@@ -38,17 +38,12 @@ namespace chemodurov
     std::deque< Value > d_for_shell(f_list.begin(), f_list.end());
     std::deque< Value > d_for_tim(f_list.begin(), f_list.end());
 
-    using namespace std::placeholders;
-    auto merge = std::bind(mergeSort, _1, num_of_elem, comp);
-    auto shell = std::bind(shellSort, _1, num_of_elem, comp);
-    auto tim = std::bind(timSort, _1, num_of_elem, comp);
-
-    merge(f_list.begin());
-    merge(l_for_merge.begin());
-    shell(l_for_shell.begin());
-    merge(d_for_merge.begin());
-    shell(d_for_shell.begin());
-    tim(d_for_tim.begin());
+    mergeSort(f_list.begin(), num_of_elem, comp);
+    mergeSort(l_for_merge.begin(), num_of_elem, comp);
+    shellSort(l_for_shell.begin(), num_of_elem, comp);
+    mergeSort(d_for_merge.begin(), num_of_elem, comp);
+    shellSort(d_for_shell.begin(), num_of_elem, comp);
+    timSort(d_for_tim.begin(), num_of_elem, comp);
 
     print(f_list.begin(), f_list.end(), out);
     out << '\n';
