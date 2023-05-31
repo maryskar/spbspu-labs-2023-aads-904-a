6ce73c0b38a8aaafbc508b2d043212045229d798
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-05-31 12:38:54 +0300

    add preparations for input

--- fesenko.ivan/S1/main.cpp
+++ fesenko.ivan/S1/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include "stack.h"
+#include "queue.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-
+  if (argc != 1 && argc != 2) {
+    std::cerr << "Invalid number of arguments\n";
+    return 1;
+  }
+  std::ifstream input;
+  if (argc == 2) {
+    input.open(argv[1]);
+  }
+  std::istream &in = (argc == 2) ? input : std::cin;
 }
