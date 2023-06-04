0b3366bb85422aa317d789d58d065940426e3163
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-06-04 15:04:58 +0300

    Add function stringToInfixQueue

---
+++ fesenko.ivan/S1/stringToInfixQueue.cpp
@@ -0,0 +1,18 @@
+#include "stringToInfixQueue.h"
+
+fesenko::Queue< std::string > fesenko::stringToInfixQueue(std::string s)
+{
+  Queue< std::string > queue;
+  std::string element = "";
+  size_t i = 0;
+  while (i != s.size()) {
+    while (i != s.size() && !std::isspace(s[i])) {
+      element += s[i];
+      i++;
+    }
+    queue.push(element);
+    element = "";
+    i++;
+  }
+  return queue;
+}
---
+++ fesenko.ivan/S1/stringToInfixQueue.h
@@ -0,0 +1,9 @@
+#ifndef STRINGTOINFIXQUEUE_H
+#define STRINGTOINFIXQUEUE_H
+#include <string>
+#include "queue.h"
+namespace fesenko
+{
+  Queue < std::string > stringToInfixQueue(std::string s);
+}
+#endif
