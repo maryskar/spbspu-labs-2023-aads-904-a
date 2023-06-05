ef1c62fd248036d2441dfd24c2c1ae1c8810a272
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-06-05 16:26:51 +0300

    Add function to find priority of operations

--- fesenko.ivan/S1/symbols.cpp
+++ fesenko.ivan/S1/symbols.cpp
@@ -19,3 +19,12 @@ bool isNumber(std::string s)
 {
   return !isOpenedBracket(s) && !isClosedBracket(s) && !isOperation(s);
 }
+
+size_t priority(std::string s)
+{
+  if (s == "+" || s == "-") {
+    return 0;
+  } else {
+    return 1;
+  }
+}
--- fesenko.ivan/S1/symbols.h
+++ fesenko.ivan/S1/symbols.h
@@ -5,4 +5,5 @@ bool isOpenedBracket(std::string s);
 bool isClosedBracket(std::string s);
 bool isOperation(std::string s);
 bool isNumber(std::string s);
+size_t priority(std::string s);
 #endif
