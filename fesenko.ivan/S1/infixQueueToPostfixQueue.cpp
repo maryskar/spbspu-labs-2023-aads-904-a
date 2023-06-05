0f0878e4cbee9ab096d3113f5f19e347454a3a11
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-06-05 16:59:57 +0300

    Complete algorithm to transform infix queue to postfix queue

---
+++ fesenko.ivan/S1/infixQueueToPostfixQueue.cpp
@@ -0,0 +1,48 @@
+#include "infixQueueToPostfixQueue.h"
+#include <string>
+#include <stdexcept>
+#include "stack.h"
+#include "queue.h"
+#include "symbols.h"
+
+fesenko::Queue< std::string > infixQueueToPostfixQueue(fesenko::Queue < std::string > &infixQueue)
+{
+  fesenko::Stack< std::string > stack;
+  fesenko::Queue< std::string > postfixQueue;
+  while (!infixQueue.isEmpty()) {
+    std::string element = infixQueue.front();
+    if (isNumber(element)) {
+      postfixQueue.push(element);
+    }
+    if (isOperation(element)) {
+      while (!stack.isEmpty() && priority(element) >= priority(stack.top()) && !isOpenedBracket(stack.top())) {
+        postfixQueue.push(stack.top());
+        stack.pop();
+      }
+      stack.push(infixQueue.front());
+      infixQueue.pop();
+    }
+    if (isOpenedBracket(element)) {
+      stack.push(element);
+      infixQueue.pop();
+    }
+    if (isClosedBracket(element)) {
+      while (!isOpenedBracket(stack.top())) {
+        postfixQueue.push(stack.top());
+        stack.pop();
+        if (stack.isEmpty()) {
+          throw std::logic_error("Incorrect expression");
+        }
+      }
+      stack.pop();
+    }
+  }
+  while (!stack.isEmpty()) {
+    if (isOpenedBracket(stack.top())) {
+      throw std::logic_error("Incorrect expression");
+    }
+    postfixQueue.push(stack.top());
+    stack.pop();
+  }
+  return postfixQueue;
+}
