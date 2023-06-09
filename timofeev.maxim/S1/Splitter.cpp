0c147df3147bb78e1e246378ff478ef22ed58ba1
Maxim <maks.timofeev9834@gmail.com>
2023-06-10 01:26:27 +0300

    splitter updated

--- timofeev.maxim/S1/Splitter.cpp
+++ timofeev.maxim/S1/Splitter.cpp
@@ -10,25 +10,23 @@ bool timofeev::Splitter::empty() const
 {
   return str_.empty();
 }
+
 const std::string& timofeev::Splitter::getSplit()
 {
   if (str_.empty())
   {
-    throw std::invalid_argument("Empty string");
+    throw std::runtime_error("Read error");
   }
   size_t space = str_.find_first_of(' ');
   if (space == str_.npos)
   {
-    throw std::invalid_argument("invalid_argument dsf");
-  }
-  std::string String = str_.substr(0, space);
-  if (space == str_.size() - 1)
-  {
+    split_ = str_;
     str_.clear();
   }
   else
   {
+    split_ = str_.substr(0, space);
     str_ = str_.substr(space + 1);
   }
-  return String;
+  return split_;
 }
