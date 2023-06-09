43ba2e6c06de3602577ac275d9db888f7a635a0b
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2023-06-09 22:50:27 +0300

    Revert "Initial commit"
    This reverts commit 82a5bfc5

--- romanovich.artem/S1/main.cpp
+++ romanovich.artem/S1/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include "stack.h"
+#include "queue.h"
+#include "notations.h"
+#include "expressionpart.h"
 int main(int argc, char **argv)
 {
   if ((argc != 1) && (argc != 2))
@@ -7,24 +11,63 @@ int main(int argc, char **argv)
     std::cerr << "Not appropriate parameters number.\n";
     return 1;
   }
-  else if (argc == 2)
+  romanovich::Stack< romanovich::ExpPart > stack;
+  std::fstream fileInput;
+  if (argc == 2)
   {
-    std::string fileIn = argv[1];
-    std::fstream fileInput(fileIn);
+    fileInput.open(argv[1]);
     if (!fileInput.is_open())
     {
-      std::cerr << "Error while opening file " << fileIn << ".\n";
+      std::cerr << "Error while opening file.\n";
       return 1;
     }
-    for (std::string line; std::getline(fileInput, line);)
+    if (fileInput.eof())
     {
-      std::cout << line << "\n";
-    }
-  } else {
-    for (std::string line; std::getline(std::cin, line);)
-    {
-      std::cout << line << "\n";
+      std::cout << "\n";
+      return 0;
     }
   }
+  std::istream &in = (argc == 2) ? fileInput : std::cin;
+  romanovich::Stack< romanovich::ExpPart > answer;
+  for (std::string line; std::getline(in, line);)
+  {
+    if (line.empty())
+    {
+      continue;
+    }
+    romanovich::Queue< romanovich::ExpPart > postfixQueue;
+    romanovich::Stack< romanovich::ExpPart > stack1;
+    try
+    {
+      romanovich::Queue< romanovich::ExpPart > infixNotation = romanovich::splitLine(line);
+      romanovich::getPostfixFromInfix(infixNotation, stack1, postfixQueue);
+    }
+    catch (...)
+    {
+      std::cerr << "Error while calc.\n";
+      return 2;
+    }
+    romanovich::Stack< romanovich::ExpPart > calcStack;
+    try
+    {
+      romanovich::calcPostfixExpression(postfixQueue, answer, calcStack);
+    }
+    catch (...)
+    {
+      std::cerr << "Error while calc.\n";
+      return 2;
+    }
+  }
+  if (!answer.isEmpty())
+  {
+    std::cout << answer.get().getOperand();
+    answer.pop();
+    while (!answer.isEmpty())
+    {
+      std::cout << " " << answer.get().getOperand();
+      answer.pop();
+    }
+  }
+  std::cout << "\n";
   return 0;
 }
