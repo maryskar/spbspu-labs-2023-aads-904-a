232a8c1611852c2a5bd32f0dd4cda1f69352d8df
Maxim <maks.timofeev9834@gmail.com>
2023-06-09 23:31:58 +0300

    update main

--- timofeev.maxim/S1/main.cpp
+++ timofeev.maxim/S1/main.cpp
@@ -1,4 +1,34 @@
-int main()
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "queue.h"
+#include "stack.h"
+int main(int argc, char* argv[])
 {
-  //test
+  timofeev::Stack <long long> result;
+  std::string str = "";
+  if (argc == 1)
+  {
+    try
+    {
+
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << e.what() << "\n";
+      return 1;
+    }
+  }
+  if (argc == 2)
+  {
+    try
+    {
+
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << e.what() << "\n";
+      return 1;
+    }
+  }
 }
