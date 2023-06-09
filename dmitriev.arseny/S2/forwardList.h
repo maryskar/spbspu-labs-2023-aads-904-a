d67c5d8e4a3b55184d123ef9ac6a5d95f2f02fbf
ArsDi <abstractlevl@gmail.com>
2023-06-09 23:00:34 +0300

    adding basic functional

--- dmitriev.arseny/S2/forwardList.h
+++ dmitriev.arseny/S2/forwardList.h
@@ -1,13 +1,71 @@
 #ifndef FORWARDLSIT_H
 #define FORWARDLSIT_H
 
+#include "node.h"
+
 template< typename T >
 class ForwardList
 {
 public:
+  ForwardList();
+  ~ForwardList();
+
+  void push_front(const T& data);
+  void push_front(T&& data);
+
+  const T& front() const;
+  T& front();
+
+  void pop_front();
 
 private:
+  Node< T >* head;
 
 };
+
+template< typename T >
+ForwardList< T >::ForwardList():
+  head(nullptr)
+{}
+
+template< typename T >
+ForwardList< T >::~ForwardList()
+{
+  clear(head);
+}
+
+template< typename T >
+void ForwardList< T >::push_front(const T& data)
+{
+  Node< T >* newHead = new Node< T >{data, head};
+  head = newHead;
+}
+
+template< typename T >
+void ForwardList< T >::push_front(T&& data)
+{
+  Node< T >* newHead = new Node< T >{std::move(data), head};
+  head = newHead;
+}
+
+template< typename T >
+const T& ForwardList< T >::front() const
+{
+  return head->data;
+}
+
+template< typename T >
+T& ForwardList< T >::front()
+{
+  return head->data;
+}
+
+template< typename T >
+void ForwardList< T >::pop_front()
+{
+  Node< T >* newHead = head->next;
+  delete head;
+  head = newHead;
+}
 
 #endif
--- dmitriev.arseny/S2/node.h
+++ dmitriev.arseny/S2/node.h
@@ -1,11 +1,24 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <stdexcept>
+
 template< typename T >
 struct Node
 {
   T data;
   Node< T >* next;
 };
+
+template< typename T >
+void clear(Node< T >* head)
+{
+  while (head)
+  {
+    Node< T >* newHead = head->next;
+    delete head;
+    head = newHead;
+  }
+}
 
 #endif
