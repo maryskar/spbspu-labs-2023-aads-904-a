178f061599b8af3c492fb21ef825163fc01bc57d
Maxim <maks.timofeev9834@gmail.com>
2023-06-10 01:05:38 +0300

    function for operators header

---
+++ timofeev.maxim/S1/getResultOfOperators.h
@@ -0,0 +1,12 @@
+#ifndef GETRESULTOFOPERATORS_H
+#define GETRESULTOFOPERATORS_H
+using ll = long long;
+namespace timofeev
+{
+    ll getSum(ll one, ll two);
+    ll getMinus(ll one, ll two);
+    ll getDiv(ll one, ll two);
+    ll getMultip(ll one, ll two);
+    ll getRemander(ll one, ll two);
+}
+#endif
