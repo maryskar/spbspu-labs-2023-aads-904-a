31fcaec0a9a7d760c150ef2a0d44a0fbb8f142a6
Maxim <maks.timofeev9834@gmail.com>
2023-06-10 01:24:01 +0300

    getMultip  getRemander

--- timofeev.maxim/S1/getResultOfOperators.cpp
+++ timofeev.maxim/S1/getResultOfOperators.cpp
@@ -50,5 +50,38 @@ namespace timofeev
       return one / two;
     }
   }
-
+  ll getMultip(ll one, ll two)
+  {
+    if (one > 0 && two > 0 && one > Max / two)
+    {
+      throw std::overflow_error("overflow mul");
+    }
+    if (one < 0 && two < 0 && one < Max / two)
+    {
+      throw std::overflow_error("overflow mul");
+    }
+    if (one < 0 && two > 0 && one < Min / two)
+    {
+      throw std::underflow_error("underflow mul");
+    }
+    if (one > 0 && two < 0 && one > Min / two)
+    {
+      throw std::underflow_error("underflow mul");
+    }
+    else
+    {
+      return one * two;
+    }
+  }
+  ll getRemander(ll one, ll two)
+  {
+    if (two == 0)
+    {
+      throw std::invalid_argument("div by ZERO");
+    }
+    long long int ret = one % two;
+    if (ret < 0)
+      ret += two;
+    return ret;
+  }
 }
