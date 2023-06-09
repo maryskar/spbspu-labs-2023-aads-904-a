afb807ca777f868da142a5ff61b63ef73fc237a1
Maxim <maks.timofeev9834@gmail.com>
2023-06-10 00:30:42 +0300

    getSplit

--- timofeev.maxim/S1/Splitter.cpp
+++ timofeev.maxim/S1/Splitter.cpp
@@ -1,4 +1,6 @@
 #include "Splitter.h"
+#include <cstddef>
+#include <stdexcept>
 
 timofeev::Splitter::Splitter(const std::string& str):
         str_(str),
@@ -7,4 +9,26 @@ timofeev::Splitter::Splitter(const std::string& str):
 bool timofeev::Splitter::empty() const
 {
   return str_.empty();
+}
+const std::string& timofeev::Splitter::getSplit()
+{
+  if (str_.empty())
+  {
+    throw std::invalid_argument("Empty string");
+  }
+  size_t space = str_.find_first_of(' ');
+  if (space == str_.npos)
+  {
+    throw std::invalid_argument("invalid_argument dsf");
+  }
+  std::string String = str_.substr(0, space);
+  if (space == str_.size() - 1)
+  {
+    str_.clear();
+  }
+  else
+  {
+    str_ = str_.substr(space + 1);
+  }
+  return String;
 }
