3c702ea7e54a11f36b2231ad93393583e8e4b04d
Maxim <maks.timofeev9834@gmail.com>
2023-06-10 01:08:22 +0300

    getsum done

---
+++ timofeev.maxim/S1/getResultOfOperators.cpp
@@ -0,0 +1,24 @@
+#include "getResultOfOperators.h"
+#include <stdexcept>
+#include <limits>
+ll Max = std::numeric_limits<ll>::max();
+ll Min = std::numeric_limits<ll>::min();
+namespace timofeev
+{
+  ll getSum(ll one, ll two)
+  {
+    if (one > 0 && two > 0 && one > Max - two)
+    {
+      throw std::overflow_error("Overflow sum");
+    }
+    else if (one < 0 && two < 0 & one < Min - two)
+    {
+      throw std::underflow_error("underflow sum");
+    }
+    else
+    {
+      return one + two;
+    }
+  }
+
+}
