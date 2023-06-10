b217a4af29d2d88dd95cead7ecbf16522375f9ca
aksenovEA <aksenov200477@gmail.com>
2023-06-10 19:02:37 +0300

    trying to convert infix to postfix

---
+++ aksenov.egor/S1/convertToPostfix.h
@@ -0,0 +1,7 @@
+#ifndef CONVERTTOPOSTFIX_H
+#define CONVERTTOPOSTFIX_H
+#include "queue.h"
+namespace aksenov {
+  Queue< std::string > getPostfixQueue(Queue< std::string > &infQueue);
+}
+#endif CONVERTTOPOSTFIX_H
