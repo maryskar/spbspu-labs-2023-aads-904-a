6430e2cafd03ed1848acfe7fae625a45a9c08710
aksenovEA <aksenov200477@gmail.com>
2023-06-12 02:25:12 +0300

    fix main for file input

--- aksenov.egor/S1/main.cpp
+++ aksenov.egor/S1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "queue.h"
 #include "stringToInfix.h"
@@ -41,7 +42,31 @@ int main(int argc, char *argv[])
   }
   if (argc == 2)
   {
-
+    try
+    {
+      std::ifstream f;
+      f.open(argv[1]);
+      if (!f.is_open())
+      {
+        std::cerr << "cant open file" << "\n";
+        return 2;
+      }
+      while (std::getline(f, str))
+      {
+        if (str.find_first_not_of(" \n") == std::string::npos)
+        {
+          continue;
+        }
+        aksenov::Queue< std::string > infixQueue = aksenov::convertToInfix(str);
+        aksenov::Queue< std::string > postfixQueue = aksenov::getPostfixQueue(infixQueue);
+        result = aksenov::solvePostfixExpr(postfixQueue);
+      }
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << e.what() << "\n";
+      return 1;
+    }
   }
-  std::cout << result;
+  std::cout << result << "\n";
 }
