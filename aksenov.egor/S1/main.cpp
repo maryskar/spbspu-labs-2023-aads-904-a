f5aa8daafa2d75516816c9992771574570637e88
aksenovEA <aksenov200477@gmail.com>
2023-06-12 02:06:52 +0300

    main.cpp first try

--- aksenov.egor/S1/main.cpp
+++ aksenov.egor/S1/main.cpp
@@ -4,30 +4,20 @@
 #include "stringToInfix.h"
 #include "convertToPostfix.h"
 #include "solvePostfixExpr.h"
-void printQueueElements(aksenov::Queue< std::string > &queue)
+int main(int argc, char *argv[])
 {
-  while (!queue.isEmpty())
+  if (argc > 2)
   {
-    std::string element;
-    if (!queue.isEmpty())
-    {
-      element = queue.drop();
-      queue.pop();
-    }
-    std::cout << element << std::endl;
+    std::cerr << "err" << "\n";
+    return 1;
   }
-}
+  std::string str = "";
+  if (argc == 1)
+  {
 
-int main()
-{
-  std::string str = "( 7 + 8 ) * 10 - 3";
-  try {
-    aksenov::Queue< std::string > queue = aksenov::convertToInfix(str);
-    aksenov::Queue< std::string > postfixQ = aksenov::getPostfixQueue(queue);
-    //printQueueElements(postfixQ);
-    std::cout << aksenov::solvePostfixExpr(postfixQ);
-  } catch (const std::invalid_argument& e) {
-    std::cout << "Invalid argument: " << e.what() << std::endl;
   }
-  return 0;
+  if (argc == 2)
+  {
+
+  }
 }
