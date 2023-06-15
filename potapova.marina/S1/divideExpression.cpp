f79ef867fb987e744dcb6be915cc93dc28edcd02
Потапова <marina-potapova04@mail.ru>
2023-06-16 00:32:44 +0300

    add almost empty function for postfix expression TODO: continue realisation of this function

--- potapova.marina/S1/divideExpression.cpp
+++ potapova.marina/S1/divideExpression.cpp
@@ -1,10 +1,22 @@
 #include "divideExpression.h"
+#include "inputInfixQueue.h"
+#include "stack.h"
 
-size_t checkPriority(char& operation)
+size_t potapova::checkPriority(char& operation)
 {
   if (operation == '*' || operation == '/')
   {
     return 1;
   }
   return 0;
+}
+
+potapova::Queue< potapova::ArithmExpMember >& potapova::composePostfixQueue(potapova::Queue< potapova::ArithmExpMember >& infix_expr)
+{
+  potapova::Queue< potapova::ArithmExpMember > postfix_expr;
+  potapova::Stack< potapova::ArithmExpMember > operators_stack;
+  for (size_t expr_ptr = 0; expr_ptr < infix_expr.size(); ++expr_ptr)
+  {
+
+  }
 }
--- potapova.marina/S1/divideExpression.h
+++ potapova.marina/S1/divideExpression.h
@@ -3,10 +3,13 @@
 
 #include <cstddef>
 #include <string>
+#include "queue.h"
+#include "valueType.h"
 
 namespace potapova
 {
   size_t checkPriority(char& operation);
+  potapova::Queue< potapova::ArithmExpMember >& potapova::composePostfixQueue(potapova::Queue< potapova::ArithmExpMember >& infix_expr);
 }
 
 #endif
