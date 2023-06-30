c3014d2ea7ccc0fc7f559b9a2e5d4c544d8a1434
Потапова <marina-potapova04@mail.ru>
2023-06-30 19:11:47 +0300

    add postfix queue in main

--- potapova.marina/S1/main.cpp
+++ potapova.marina/S1/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include "valueType.h"
 #include "queue.h"
-#include "getInfixQueue.h"
+#include "inputInfixQueue.h"
+#include "divideExpression.h"
 
 int main()
 {
-  potapova::Queue< potapova::ArithmExpMember > dest;
-  std::istream& in = std::cin;
-  if (!inputInfixQueue(dest, in))
+  potapova::expr_queue infix_expr;
+  if (!inputInfixQueue(infix_expr))
   {
     std::cerr << "Input error\n";
     return 1;
+  }
+  auto postfix_queue(potapova::composePostfixQueue(infix_expr));
+  while (!postfix_queue.empty())
+  {
+    if (postfix_queue.front().type == potapova::ArithmExpMember::Type::Num)
+    {
+      std::cout << postfix_queue.front().num << ' ';
+    }
+    else
+    {
+      std::cout << postfix_queue.front().operation << ' ';
+    }
+    postfix_queue.pop();
   }
 }
