2953fbe42e457cc2d3571528b25f86b534363c38
beesaiboo <beedzibood@gmail.com>
2023-07-03 09:41:17 +0500

    declared constructors for dictionary

--- mashkin.andrew/S2/dictionary.h
+++ mashkin.andrew/S2/dictionary.h
@@ -15,13 +15,41 @@ namespace mashkin
     using value_type = std::pair< const Key, Value >;
     using iter = Iterator< Value >;
     using const_iter = ConstIterator< Value >;
+
+    Dictionary();
+    Dictionary(const Compare& comp);
+    template< class InputIt >
+    Dictionary(InputIt first, InputIt last, const Compare& comp);
+    Dictionary(const Dictionary& other);
+    Dictionary(Dictionary&& other);
+
     void push(Key k, Value v);
     Value get(Key k);
-    Value& drop(Key k);
+    void drop(Key k);
 
   private:
     ForwardList< value_type > pair_;
+    Compare comp_;
   };
+
+  template< class Key, class Value, class Compare >
+  Dictionary< Key, Value, Compare >::Dictionary():
+    pair_()
+  {
+  }
+
+  template< class Key, class Value, class Compare >
+  Dictionary< Key, Value, Compare >::Dictionary(const Compare& comp):
+    pair_()
+  {
+  }
+
+  template< class Key, class Value, class Compare >
+  template< class InputIt >
+  Dictionary< Key, Value, Compare >::Dictionary(InputIt first, InputIt last, const Compare& comp)
+  {
+  }
+
   template< class Key, class Value, class Compare >
   void Dictionary< Key, Value, Compare >::push(Key k, Value v)
   {
