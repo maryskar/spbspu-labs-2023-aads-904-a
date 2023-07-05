368a5eca5834bb309234693db15c3cd78dfa9e58
xeniaTa <kseniatarasenko1@gmail.com>
2023-07-05 15:46:47 +0300

    add left and right rotation

--- tarasenko.ksenia/S4/BinarySearchTree.h
+++ tarasenko.ksenia/S4/BinarySearchTree.h
@@ -24,11 +24,13 @@ namespace tarasenko
    }
 
    void insert(const data_t& data);
-   void leftRotation(root_t* ptree);
-   void rightRotation(root_t* ptree);
+   void leftRotation();
+   void rightRotation();
 
   private:
    root_t* root_;
+   void leftRotation(root_t* ptree);
+   void rightRotation(root_t* ptree);
   };
 
   template< typename Key, typename Value, typename Compare >
@@ -43,6 +45,70 @@ namespace tarasenko
     {
       details::insert(data, root_);
     }
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void BinarySearchTree< Key, Value, Compare >::leftRotation(root_t* ptree)
+  {
+    auto rightChild = ptree->right_;
+    ptree->right_ = rightChild->left_;
+    if (rightChild->left_)
+    {
+      rightChild->left_->parent_ = ptree;
+    }
+    rightChild->parent_ = ptree->parent_;
+    if (!ptree->parent_)
+    {
+      root_ = rightChild;
+    }
+    else if (ptree == ptree->parent_->left_)
+    {
+      ptree->parent_->left_ = rightChild;
+    }
+    else
+    {
+      ptree->parent_->right_ = rightChild;
+    }
+    rightChild->left_ = ptree;
+    ptree->parent_ = rightChild;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void BinarySearchTree< Key, Value, Compare >::rightRotation(root_t* ptree)
+  {
+    auto leftChild = ptree->left_;
+    ptree->left_ = leftChild->right_;
+    if (leftChild->right_)
+    {
+      leftChild->right_->parent_ = ptree;
+    }
+    leftChild->parent_ = ptree->parent_;
+    if (!ptree->parent_)
+    {
+      root_ = leftChild;
+    }
+    else if (ptree == ptree->parent_->left_)
+    {
+      ptree->parent_->left_ = leftChild;
+    }
+    else
+    {
+      ptree->parent_->right_ = leftChild;
+    }
+    leftChild->right_ = ptree;
+    ptree->parent_ = leftChild;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void BinarySearchTree< Key, Value, Compare >::leftRotation()
+  {
+    leftRotation(root_);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void BinarySearchTree< Key, Value, Compare >::rightRotation()
+  {
+    rightRotation(root_);
   }
 }
 
