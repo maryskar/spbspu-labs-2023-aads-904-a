c15eb67dff3d29602f225ed5d7eb12bdfc452d0c
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-07-06 23:04:47 +0500

    Complete summarize function

---
+++ fesenko.ivan/S1/mathOperations.cpp
@@ -0,0 +1,13 @@
+#include "mathOperations.h"
+#include <limits>
+long long fesenko::summarize(long long a, long long b)
+{
+  if (a > 0 && b > 0 && a > std::numeric_limits< long long >::max() - b) {
+    throw std::overflow_error("Overflow");
+  }
+  if (a < 0 && b < 0 && a < std::numeric_limits< long long >::min() - b) {
+    throw std::overflow_error("Overflow");
+  }
+  return a + b;
+}
+
