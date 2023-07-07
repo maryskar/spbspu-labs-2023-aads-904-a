1605fbdde9e271da294a4d7fd92ab21082bc3b4f
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-07-08 00:01:23 +0500

    Completed main.cpp

--- fesenko.ivan/S1/main.cpp
+++ fesenko.ivan/S1/main.cpp
@@ -2,10 +2,13 @@
 #include <fstream>
 #include "stack.h"
 #include "queue.h"
+#include "stringToInfixQueue.h"
+#include "infixQueueToPostfixQueue.h"
+#include "calculateInPostfixQueue.h"
 
-int main(int argc, char *argv[])
+int main(/*int argc, char *argv[]*/)
 {
-  if (argc != 1 && argc != 2) {
+  /*if (argc != 1 && argc != 2) {
     std::cerr << "Invalid number of arguments\n";
     return 1;
   }
@@ -13,5 +16,32 @@ int main(int argc, char *argv[])
   if (argc == 2) {
     input.open(argv[1]);
   }
-  std::istream &in = (argc == 2) ? input : std::cin;
+  std::istream &in = (argc == 2) ? input : std::cin;*/
+  std::string expression = "";
+  long long result = 0;
+  fesenko::Stack< long long > results;
+  while (std::cin) {
+    std::getline(std::cin, expression);
+    if (expression.empty()) {
+      continue;
+    }
+    try {
+      fesenko::Queue< std::string > infixQueue = fesenko::stringToInfixQueue(expression);
+      fesenko::Queue< std::string > postfixQueue = fesenko::infixQueueToPostfixQueue(infixQueue);
+      result = fesenko::calculateInPostfixQueue(postfixQueue);
+      results.push(result);
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << "\n";
+      return 2;
+    }
+  }
+  while (!results.isEmpty()) {
+    result = results.top();
+    results.pop();
+    if (results.isEmpty()) {
+      std::cout << result << "\n";
+    } else {
+      std::cout << result << " ";
+    }
+  }
 }
