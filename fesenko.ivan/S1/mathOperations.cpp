f2b795790fdf34ffbe34de56e6ce927500331441
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-07-08 00:00:35 +0500

    Improve algorithm

--- fesenko.ivan/S1/mathOperations.cpp
+++ fesenko.ivan/S1/mathOperations.cpp
@@ -22,13 +22,23 @@ long long fesenko::multiply(long long a, long long b)
   if (a == 0 || b == 0) {
     return 0;
   }
-  if (((a > 0 && b > 0) || (a < 0 && b < 0)) && a > std::numeric_limits< long long >::max() / b) {
-    throw std::overflow_error("Overflow");
+  if ((a > 0 && b > 0) || (a < 0 && b < 0)) {
+    if (a > std::numeric_limits< long long >::max() / b && b < 0) {
+      return a * b;
+    }
+    if (a < std::numeric_limits< long long >::max() / b && b > 0) {
+      return a * b;
+    }
+  } else {
+    if (b == -1 || (a < std::numeric_limits< long long >::min() / b && b < 0)) {
+      return a * b;
+    }
+    if (a > std::numeric_limits< long long >::min() / b && b > 0) {
+      return a * b;
+    }
   }
-  if (((a > 0 && b < 0) || (a < 0 && b > 0)) && a > std::numeric_limits< long long >::min() / b) {
-    throw std::overflow_error("Overflow");
-  }
-  return a * b;
+  throw std::overflow_error("Overflow");
+
 }
 
 long long fesenko::divide(long long a, long long b)
