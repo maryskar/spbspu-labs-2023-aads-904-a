1c3a79e9689824fa2dfee5d23cad551ac8a67547
Shuzamada <shuzamada@inbox.ru>
2023-07-07 14:09:48 +0300

    start node

--- hrushchev.nikita/S2/forwardlist.hpp
+++ hrushchev.nikita/S2/forwardlist.hpp
@@ -1,9 +1,16 @@
+#ifndef FORWARDLIST_HPP
+#define FORWARDLIST_HPP
+
+#include <cstddef>
+
 template <typename T>
 class ForwardList
 {
   using value_type = T;
-  using size_type = std::size_t;
-  using deffirence_type = std::ptrdiff_t;
+  using size_type = size_t;
+  using deffirence_type = ptrdiff_t;
   using reference = value_type&;
   using const_reference = const value_type&;
 };
+
+#endif
