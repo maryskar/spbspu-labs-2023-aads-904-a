e1d490d6f326dff88000265a143bd5b7755f8778
beesaiboo <beedzibood@gmail.com>
2023-07-09 23:35:14 +0500

    added cheking to opening file

--- mashkin.andrew/S2/main.cpp
+++ mashkin.andrew/S2/main.cpp
@@ -1,8 +1,7 @@
 #include <cstddef>
-#include <forward_list>
 #include <fstream>
 #include <iostream>
-#include <iterator>
+#include "dictionary.h"
 #include "forwardConstIterator.h"
 #include "forwardIterator.h"
 #include "forwardList.h"
@@ -11,57 +10,21 @@ int main(int argc, char** argv)
 {
   if (argc != 2)
   {
+    std::cerr << "Wrong number of arguments\n";
     return 1;
   }
   else
   {
-    std::cout << argv[1];
-  }
-  std::forward_list< size_t > realf;
-  mashkin::ForwardList< size_t > test;
-  mashkin::ForwardList< size_t > test2;
-  for (size_t i = 0; i < 4; ++i)
-  {
-    test.push_front(i);
-    realf.push_front(i);
-  }
-  for (size_t i = 4; i < 8; ++i)
-  {
-    test2.push_front(i);
-  }
-  // mashkin::forwardConstIterator< size_t > citer = test.cbegin();
-  // mashkin::forwardIterator< size_t > iter = test.begin();
-  // mashkin::forwardIterator< size_t > last = test.begin();
-  // last++;
-  // last++;
-  // last++;
-  // last++;
-  // mashkin::forwardIterator< size_t> before_iter = test.before_begin();
-  // mashkin::forwardConstIterator< size_t > cend = test.cend();
-  // mashkin::forwardIterator< size_t > end = test.end();
-  // mashkin::forwardIterator< size_t > Iter = test.erase_after(before_iter);
-  // std::forward_list< size_t >::iterator iterr = realf.erase_after(realf.cbegin());
-  // realf.reverse();
-  // test.reverse();
-  // test.insert_after(test.cbegin(), 8);
-  // test.splice_after(test.before_begin(), test2);
-  // test.splice_after(test.before_begin(), test2, test2.cbegin(), ++test2.cbegin());
-  // realf.push_front(1);
-  // realf.remove(1);
-  // test.push_front(1);
-  // test.remove(1);
-  for (auto it = realf.begin(); it != realf.end();)
-  {
-    std::cout << realf.front() << " ";
-    ++it;
-    realf.pop_front();
-  }
-  std::cout << "\n";
-  for (auto it = test.begin(); it != test.end();)
-  {
-    std::cout << test.front() << " ";
-    ++it;
-    test.pop_front();
+    std::ifstream input;
+    input.open(argv[1]);
+    if (!input.is_open())
+    {
+      std::cerr << "Can't open file\n";
+      return 1;
+    }
+    using dict = mashkin::Dictionary< int, std::string >;
+    using dicts = mashkin::Dictionary< std::string, dict >;
+
   }
   return 0;
 }
