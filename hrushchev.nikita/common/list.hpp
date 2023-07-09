2480f69be7265349087324f02814141c13fc6830
Shuzamada <shuzamada@inbox.ru>
2023-07-09 23:15:22 +0300

    upgrade list

--- hrushchev.nikita/common/list.hpp
+++ hrushchev.nikita/common/list.hpp
@@ -1,11 +1,39 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
-template < typename T >
+#include <utility>
+
+template< typename T >
 struct List
 {
   T data_;
   List< T >* next_;
+  List();
+  List(const T& value);
+  List(T&& value);
+  template <typename... Args>
+  List(Args&&... args);
 };
+
+template< typename T >
+List< T >::List():
+  data_(),
+  next_(nullptr)
+{
+}
+
+template< typename T >
+List< T >::List(const T& value):
+  data_(value),
+  next_(nullptr)
+{
+}
+
+template< typename T >
+List< T >::List(T&& value):
+  data_(std::move(value)),
+  next_(nullptr)
+{
+}
 
 #endif
