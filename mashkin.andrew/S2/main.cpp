11e443c4f571c299d2c032cffe0b1ae8a23ee752
beesaiboo <beedzibood@gmail.com>
2023-07-10 23:52:25 +0500

    added input dictionaries dictionaries

--- mashkin.andrew/S2/main.cpp
+++ mashkin.andrew/S2/main.cpp
@@ -1,9 +1,11 @@
 #include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include "dictionary.h"
 #include "forwardConstIterator.h"
 #include "forwardIterator.h"
+#include "IOrealization.h"
 #include "forwardList.h"
 
 int main(int argc, char** argv)
@@ -22,9 +24,20 @@ int main(int argc, char** argv)
       std::cerr << "Can't open file\n";
       return 1;
     }
-    /*using dict = mashkin::Dictionary< int, std::string >;
-    using dicts = mashkin::Dictionary< std::string, dict >;*/
-
+    using dict = mashkin::Dictionary< int, std::string >;
+    using dictionaries = mashkin::Dictionary< std::string, dict >;
+    constexpr auto maxSize = std::numeric_limits< std::streamsize >::max();
+    dictionaries dicts;
+    while (!input.eof())
+    {
+      input >> dicts;
+      if (input.fail())
+      {
+        input.clear();
+        input.ignore(maxSize, '\n');
+        std::cout << "<INVALID COMMAND>\n";
+      }
+    }
   }
   return 0;
 }
