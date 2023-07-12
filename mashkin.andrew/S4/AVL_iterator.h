ef2e5e2d6330a9c412fb0335d991f0c85ebc61cf
beesaiboo <beedzibood@gmail.com>
2023-07-12 17:59:08 +0500

    improved AVL iterator: added comporator and defined constructors

--- mashkin.andrew/S4/AVL_iterator.h
+++ mashkin.andrew/S4/AVL_iterator.h
@@ -1,18 +1,19 @@
 #ifndef S4_AVL_ITERATOR_H
 #define S4_AVL_ITERATOR_H
+#include <functional>
 #include <iterator>
 #include "tree.h"
 
 namespace mashkin
 {
-  template< class T >
+  template< class T, class Comporator = std::less< T > >
   class AVL_iterator: public std::iterator< std::bidirectional_iterator_tag, T >
   {
   public:
     AVL_iterator();
     ~AVL_iterator() = default;
-    AVL_iterator(const AVL_iterator< T >&) = default;
-    AVL_iterator(Tree< T >* rhs);
+    explicit AVL_iterator(const AVL_iterator< T >&) = default;
+    explicit AVL_iterator(Tree< T >* rhs);
 
     AVL_iterator< T >& operator=(const AVL_iterator< T >&) = default;
     AVL_iterator< T >& operator++();
@@ -25,8 +26,24 @@ namespace mashkin
 
     bool operator!=(const AVL_iterator< T >& rhs) const;
     bool operator==(const AVL_iterator< T >& rhs) const;
+
   private:
-    Tree< T >* node;
+    Tree< T >* node_;
+    Comporator comp_;
   };
+
+  template< class T, class Comp >
+  AVL_iterator< T, Comp >::AVL_iterator():
+    node_(nullptr),
+    comp_()
+  {
+  }
+
+  template< class T, class Comp >
+  AVL_iterator< T, Comp >::AVL_iterator(Tree< T >* rhs):
+    node_(rhs),
+    comp_()
+  {
+  }
 }
 #endif
