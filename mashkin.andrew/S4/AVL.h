6ef691a934c824b9a93eefd41e4afc8c9e40398d
beesaiboo <beedzibood@gmail.com>
2023-07-12 17:00:41 +0500

    defined constructor AVL()

--- mashkin.andrew/S4/AVL.h
+++ mashkin.andrew/S4/AVL.h
@@ -17,5 +17,14 @@ namespace mashkin
     Tree< T, Comporator > root_;
     Comporator comp_;
   };
+
+  template< class T, class Comp >
+  AVL< T, Comp >::AVL():
+    root_(static_cast< Tree< T, Comp >* >(::operator new(sizeof(Tree< T, Comp >)))),
+    comp_()
+  {
+    root_.left = nullptr;
+    root_.right = nullptr;
+  }
 }
 #endif
