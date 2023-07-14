cf80e62ff2ae8e784084c4b1cc19f720567259c9
beesaiboo <beedzibood@gmail.com>
2023-07-14 13:05:42 +0500

    defined increment and decrement operator++ for iterator

--- mashkin.andrew/S4/AVL_iterator.h
+++ mashkin.andrew/S4/AVL_iterator.h
@@ -2,6 +2,7 @@
 #define S4_AVL_ITERATOR_H
 #include <functional>
 #include <iterator>
+#include <cassert>
 #include "tree.h"
 
 namespace mashkin
@@ -12,10 +13,10 @@ namespace mashkin
   public:
     AVL_iterator();
     ~AVL_iterator() = default;
-    explicit AVL_iterator(const AVL_iterator< T >&) = default;
+    explicit AVL_iterator(const AVL_iterator< T >&);
     explicit AVL_iterator(Tree< T >* rhs);
 
-    AVL_iterator< T >& operator=(const AVL_iterator< T >&) = default;
+    AVL_iterator< T >& operator=(const AVL_iterator< T >&);
     AVL_iterator< T >& operator++();
     AVL_iterator< T > operator++(int);
     AVL_iterator< T >& operator--();
@@ -30,7 +31,86 @@ namespace mashkin
   private:
     Tree< T >* node_;
     Comporator comp_;
+    void doWhileParent();
+    void doWhileLeft();
   };
+
+  template< class T, class Comp >
+  AVL_iterator< T >& AVL_iterator< T, Comp >::operator--()
+  {
+    return ;
+  }
+
+  template< class T, class Comp >
+  AVL_iterator< T > AVL_iterator< T, Comp >::operator++(int)
+  {
+    assert(node_ != nullptr);
+    auto res(*this);
+    ++(*this);
+    return res;
+  }
+
+  template< class T, class Comp >
+  void AVL_iterator< T, Comp >::doWhileLeft()
+  {
+    auto newNode = node_->right;
+    while (newNode->left)
+    {
+      newNode = newNode->left;
+    }
+    node_ = newNode;
+  }
+
+  template< class T, class Comp >
+  void AVL_iterator< T, Comp >::doWhileParent()
+  {
+    auto newNode = node_->parent_;
+    while (newNode && !comp_(node_->data, newNode->data))
+    {
+      newNode = newNode->parent_;
+    }
+    node_ = newNode;
+  }
+
+  template< class T, class Comp >
+  AVL_iterator< T >& AVL_iterator< T, Comp >::operator++()
+  {
+    assert(node_ != nullptr);
+    if (!node_->left && !node_->right && node_->parent_)
+    {
+      if (node_ == node_->parent_->left)
+      {
+        node_ = node_->parent_;
+        return *this;
+      }
+      else
+      {
+        doWhileParent();
+        return *this;
+      }
+    }
+    else if (node_->left && node_->right && node_->parent_)
+    {
+      doWhileLeft();
+      return *this;
+    }
+    else if (!node_->right && node_->parent_)
+    {
+      doWhileParent();
+      return *this;
+    }
+    else if (!node_->parent_)
+    {
+      doWhileLeft();
+      return *this;
+    }
+    else if (!node_->left && node_->right && node_->parent_)
+    {
+      doWhileLeft();
+      return *this;
+    }
+    return *this;
+  }
 
   template< class T, class Comp >
   AVL_iterator< T, Comp >::AVL_iterator():
