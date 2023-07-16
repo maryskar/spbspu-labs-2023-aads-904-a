2a6dbe80f43679c80b9137364ff5f6ed4d2dc1cb
beesaiboo <beedzibood@gmail.com>
2023-07-17 01:27:27 +0500

    defined insert() method

--- mashkin.andrew/S4/AVL.h
+++ mashkin.andrew/S4/AVL.h
@@ -32,15 +32,110 @@ namespace mashkin
     bool empty();
 
   private:
+    void rotate_left(Tree< T, Comporator >* node);
+    void rotate_right(Tree< T, Comporator >* node);
+    void rotate_RightLeft(Tree< T, Comporator >* node);
+    void rotate_LeftRight(Tree< T, Comporator >* node);
+    size_t checkHeight(Tree< T, Comporator >* head);
+    size_t checkHeightImpl(Tree< T, Comporator >* head, size_t height);
+    Tree< T, Comporator >* insert_impl(const T& data, Tree< T, Comporator >* root, Tree< T, Comporator >* before);
     void clear_impl(Tree< T, Comporator >* toDel);
     Tree< T, Comporator >* fake_;
     Comporator comp_;
   };
 
   template< class T, class C >
+  size_t AVL< T, C >::checkHeightImpl(Tree< T, C >* head, size_t height)
+  {
+    if (!head)
+    {
+      return height;
+    }
+    height++;
+    size_t left = checkHeightImpl(head->left, height);
+    size_t right = checkHeightImpl(head->right, height);
+    height = left < right ? right : left;
+    return height;
+  }
+
+  template< class T, class C >
+  size_t AVL< T, C >::checkHeight(Tree< T, C >* head)
+  {
+    size_t height = 0;
+    return checkHeightImpl(head, height);
+  }
+
+  template< class T, class C >
+  Tree< T, C >* AVL< T, C >::insert_impl(const T& data, Tree< T, C >* root, Tree< T, C >* before)
+  {
+    if (!root)
+    {
+      return before;
+    }
+    before = root;
+    root = insert_impl(data, comp_(data, root->data) ? root->left : root->right, before);
+    return root;
+  }
+
+  template< class T, class C >
   bool AVL< T, C >::empty()
   {
     return fake_ == fake_->parent_;
+  }
+
+  template< class T, class C >
+  AVL< T, C >::iter AVL< T, C >::insert(const T& val)
+  {
+    if (empty())
+    {
+      fake_->parent_ = new Tree< T, C >{val, fake_, nullptr, nullptr};
+    }
+    else
+    {
+      auto newNode = insert_impl(val, fake_->parent, fake_->parent);
+      if (comp_(val, newNode->data))
+      {
+        newNode->left = new Tree< T, C >{val, newNode, nullptr, nullptr};
+      }
+      else
+      {
+        newNode->right = new Tree< T, C >{val, newNode, nullptr, nullptr};
+      }
+      while (newNode != fake_)
+      {
+        size_t left = checkHeight(newNode->left);
+        size_t right = checkHeight(newNode->right);
+        if (right - left == 2)
+        {
+          auto subTree = newNode->right;
+          auto subLeft = checkHeight(subTree->left);
+          auto subRight = checkHeight(subTree->right);
+          if (subLeft <= subRight)
+          {
+            rotate_left(subTree);
+          }
+          else
+          {
+            rotate_RightLeft(subTree);
+          }
+        }
+        else if (left - right == 2)
+        {
+          auto subTree = newNode->left;
+          auto subLeft = checkHeight(subTree->left);
+          auto subRight = checkHeight(subTree->right);
+          if (subLeft <= subRight)
+          {
+            rotate_right(subTree);
+          }
+          else
+          {
+            rotate_LeftRight(subTree);
+          }
+        }
+        newNode = newNode->parent_;
+      }
+    }
   }
 
   template< class T, class Comp >
