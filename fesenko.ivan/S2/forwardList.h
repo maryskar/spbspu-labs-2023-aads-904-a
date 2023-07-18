b253f083df18541da4d61988310891f8f910a24c
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-07-18 16:30:43 +0500

    Add basic interface

---
+++ fesenko.ivan/S2/forwardList.h
@@ -0,0 +1,17 @@
+#ifndef FORWARDLIST_H
+#define FORWARDLIST_H
+namespace fesenko
+{
+  template< typename T >
+  class ForwadrList
+  {
+   public:
+    ForwardList();
+    ForwardList(const ForwardList< T > &);
+    ForwardList(ForwardList< T > &&);
+    ForwardList< T > &operator=(const ForwardList< T > &);
+    ForwardList< T > &operator=(ForwardList< T > &&);
+    ~ForwardList();
+  }
+}
+#endif
