9dc44436b969fd4c0eb3abcb27579e1252cf7147
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-07-18 13:25:21 +0500

    Add a file check

--- fesenko.ivan/S2/main.cpp
+++ fesenko.ivan/S2/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-
+  if (argc != 2) {
+    std::cerr << "Invalid number of arguments\n";
+    return 2;
+  }
+  std::ifstream in(argv[1]);
+  if (!in.is_open()) {
+    std::cerr << "Can`t open the file\n";
+    return 2;
+  }
 }
