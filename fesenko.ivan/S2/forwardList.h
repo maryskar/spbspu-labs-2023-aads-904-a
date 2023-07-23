ce161cb383d7c89b319d25822345fca9a40269ad
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-07-23 11:52:55 +0500

    Add member types

--- fesenko.ivan/S2/forwardList.h
+++ fesenko.ivan/S2/forwardList.h
@@ -1,11 +1,20 @@
 #ifndef FORWARDLIST_H
 #define FORWARDLIST_H
+#include "forwardIterator.h"
+#include "constForwardIterator.h"
 namespace fesenko
 {
   template< typename T >
   class ForwardList
   {
    public:
+    using value_type = T;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator = ForwardIterator< T >;
+    using const_iterator = ConstForwardIterator< T >;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
     ForwardList();
     ForwardList(const ForwardList< T > &);
     ForwardList(ForwardList< T > &&);
