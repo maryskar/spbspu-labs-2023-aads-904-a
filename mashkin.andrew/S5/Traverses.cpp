2c479ed13a40382508a762aa10c9497b7696d686
beesaiboo <beedzibood@gmail.com>
2023-07-23 12:42:46 +0500

    changed realization of Traverse function: changed output

--- mashkin.andrew/S5/Traverses.cpp
+++ mashkin.andrew/S5/Traverses.cpp
@@ -8,20 +8,20 @@ namespace mashkin
   {
     QueueForTraverse res;
     tree.traverse_lnr(res);
-    out << res.res_;
+    out << res;
   }
 
   void descend(std::ostream& out, avl& tree)
   {
     QueueForTraverse res;
     tree.traverse_rnl(res);
-    out << res.res_;
+    out << res;
   }
 
   void doBreadthTr(std::ostream& out, avl& tree)
   {
     StackForTraverse res;
     tree.traverse_breadth(res);
-    out << res.res_;
+    out << res;
   }
 }
