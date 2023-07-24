7f2c1163167ddd557f23a6b418dc7d0acef0e594
beesaiboo <beedzibood@gmail.com>
2023-07-24 19:22:19 +0500

    added a check of arguments and completing of function

--- mashkin.andrew/S6/main.cpp
+++ mashkin.andrew/S6/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cstddef>
+#include <cstdlib>
+#include <AVL.h>
+#include <string>
 
 int main(int argc, char** argv)
 {
@@ -6,6 +10,26 @@ int main(int argc, char** argv)
   {
     std::cerr << "Wrong number of arguments\n";
     return 1;
+  }
+  char* end{};
+  size_t nimbOfNumbers = std::strtol(argv[3], &end, 10);
+  mashkin::AVL< std::string, mashkin::AVL< std::string, void (*)(std::ostream&, size_t) > > dict;
+  if (dict.contains(argv[1]))
+  {
+    if (dict[argv[1]].contains(argv[2]))
+    {
+      dict[argv[1]][argv[2]](std::cout, nimbOfNumbers);
+    }
+    else
+    {
+      std::cerr << "Incorrect argument\n";
+      return 0;
+    }
+  }
+  else
+  {
+    std::cerr << "Incorrect argument\n";
+    return 0;
   }
   return 0;
 }
