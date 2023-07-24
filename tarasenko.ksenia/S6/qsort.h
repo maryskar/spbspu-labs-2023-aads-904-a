9594dcc65dca2129dacc43f04ed5f3566b4acd19
xeniaTa <kseniatarasenko1@gmail.com>
2023-07-24 13:14:58 +0300

    add qsort

---
+++ tarasenko.ksenia/S6/qsort.h
@@ -0,0 +1,51 @@
+#ifndef S6_QSORT_H
+#define S6_QSORT_H
+
+#include <iterator>
+
+namespace tarasenko
+{
+  namespace details
+  {
+    template< typename ForwardIt, typename UnaryPred >
+    ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPred p)
+    {
+      while (first != last && p(*first))
+      {
+        ++first;
+      }
+      if (first == last)
+      {
+        return first;
+      }
+      for (auto i = std::next(first); i != last; ++i)
+        if (p(*i))
+        {
+          std::swap(*i, *first);
+          ++first;
+        }
+      return first;
+    }
+  }
+
+  template< typename ForwardIt, typename Compare >
+  void quickSort(ForwardIt begin, ForwardIt last, Compare comp)
+  {
+    if (std::distance(begin, last) < 2)
+    {
+      return;
+    }
+    auto pivot = *std::next(begin, std::distance(begin, last) / 2);
+    auto l = details::partition(begin, last, [pivot, comp](const auto& i)
+    {
+      return comp(i, pivot);
+    });
+    auto b = details::partition(begin, last, [pivot, comp](const auto& i)
+    {
+      return !comp(pivot, i);
+    });
+    quickSort(begin, l, comp);
+    quickSort(b, last, comp);
+  }
+}
+#endif
