4bf8f8e1b19a9f0be68393d247d598218f909ed2
beesaiboo <beedzibood@gmail.com>
2023-07-24 18:48:32 +0500

    defined and declared merge function and added comporator to parameters of merge and mergeSort functions

--- mashkin.andrew/S6/mergeSort.h
+++ mashkin.andrew/S6/mergeSort.h
@@ -4,22 +4,58 @@
 
 namespace mashkin
 {
-  template< class InputIter, class Comporator >
-  void merge(InputIter begin, size_t left, size_t mid, size_t right)
+  template< class InputStruct, class Comporator >
+  void merge(InputStruct dataStruct, size_t left, size_t mid, size_t right, Comporator comp)
   {
+    size_t subStructOne = mid - left + 1;
+    size_t subStructTwo = right - mid;
+    auto begin = dataStruct.begin();
+    decltype(dataStruct) leftStruct(begin, begin + subStructOne);
+    decltype(dataStruct) rightStruct(begin + subStructOne + 1, begin + right);
+    size_t indOfSubStructOne = 0;
+    size_t indOfSubStructTwo = 0;
+    size_t indOfMergedStruct = left;
+    auto leftBegin = leftStruct.begin();
+    auto rightBegin = rightStruct.begin();
+    while (indOfSubStructOne < subStructOne && indOfSubStructTwo < subStructTwo)
+    {
+      if (comp(*(leftBegin + indOfSubStructOne), *(rightBegin + indOfSubStructTwo)))
+      {
+        *(begin + indOfMergedStruct) = *(leftBegin + indOfSubStructOne);
+        indOfSubStructOne++;
+      }
+      else
+      {
+        *(begin + indOfMergedStruct) = *(rightBegin + indOfSubStructTwo);
+        indOfSubStructTwo++;
+      }
+      indOfMergedStruct++;
+    }
+    while (leftBegin + indOfSubStructOne != leftStruct.end())
+    {
+      *(begin + indOfMergedStruct) = *(leftBegin + indOfSubStructOne);
+      indOfSubStructOne++;
+      indOfMergedStruct++;
+    }
+    while (rightBegin + indOfSubStructTwo != rightStruct.end())
+    {
+      *(begin + indOfMergedStruct) = *(rightBegin + indOfSubStructTwo);
+      indOfMergedStruct++;
+      indOfSubStructTwo++;
+    }
   }
 
   template< class InputIter, class Comporator >
-  void mergeSort(InputIter data_struct, size_t begin, size_t end)
+  void mergeSort(InputIter dataStructIt, size_t begin, size_t end, Comporator comp)
   {
     if (begin <= end)
     {
       return;
     }
     size_t mid = begin + (end - begin) / 2;
-    mergeSort< InputIter, Comporator >(data_struct, begin, mid);
-    mergeSort< InputIter, Comporator >(data_struct, mid + 1, end);
-    merge< InputIter, Comporator >(data_struct, begin, mid, end);
+    mergeSort< InputIter, Comporator >(dataStructIt, begin, mid, comp);
+    mergeSort< InputIter, Comporator >(dataStructIt, mid + 1, end, comp);
+    merge< InputIter, Comporator >(dataStructIt, begin, mid, end, comp);
   }
 }
 #endif
