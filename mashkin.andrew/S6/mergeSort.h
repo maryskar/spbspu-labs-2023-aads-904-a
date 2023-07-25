0a941534ff97c3cc234753e3295e06d9a0a77b1f
beesaiboo <beedzibood@gmail.com>
2023-07-25 14:37:18 +0500

    improved mergeSort

--- mashkin.andrew/S6/mergeSort.h
+++ mashkin.andrew/S6/mergeSort.h
@@ -1,61 +1,70 @@
 #ifndef MASHKIN_ANDREW_MERGESORT_H
 #define MASHKIN_ANDREW_MERGESORT_H
 #include <cstddef>
+#include <iterator>
 
 namespace mashkin
 {
   template< class InputStruct, class Comporator >
-  void merge(InputStruct dataStruct, size_t left, size_t mid, size_t right, Comporator comp)
+  void merge(InputStruct& dataStruct, size_t left, size_t mid, size_t right, Comporator comp)
   {
     size_t subStructOne = mid - left + 1;
     size_t subStructTwo = right - mid;
     auto begin = dataStruct.begin();
-    decltype(dataStruct) leftStruct(begin, begin + subStructOne);
-    decltype(dataStruct) rightStruct(begin + subStructOne + 1, begin + right);
-    size_t indOfSubStructOne = 0;
-    size_t indOfSubStructTwo = 0;
+    auto varBegin = begin;
+    auto varEnd = begin;
+    std::advance(varBegin, left);
+    std::advance(varEnd, left + subStructOne);
+    std::remove_reference_t< decltype(dataStruct) > leftStruct(varBegin, varEnd);
+    varEnd = begin;
+    varBegin = begin;
+    std::advance(varBegin, left + subStructOne);
+    std::advance(varEnd, mid + 1 + subStructTwo);
+    std::remove_reference_t< decltype(dataStruct) > rightStruct(varBegin, varEnd);
     size_t indOfMergedStruct = left;
-    auto leftBegin = leftStruct.begin();
-    auto rightBegin = rightStruct.begin();
-    while (indOfSubStructOne < subStructOne && indOfSubStructTwo < subStructTwo)
+    auto iter = begin;
+    std::advance(iter, indOfMergedStruct);
+    auto leftIter = leftStruct.begin();
+    auto rightIter = rightStruct.begin();
+    while (leftIter != leftStruct.end() && rightIter != rightStruct.end())
     {
-      if (comp(*(leftBegin + indOfSubStructOne), *(rightBegin + indOfSubStructTwo)))
+      if (comp(*leftIter, *rightIter))
       {
-        *(begin + indOfMergedStruct) = *(leftBegin + indOfSubStructOne);
-        indOfSubStructOne++;
+        *iter = *leftIter;
+        leftIter++;
       }
       else
       {
-        *(begin + indOfMergedStruct) = *(rightBegin + indOfSubStructTwo);
-        indOfSubStructTwo++;
+        *iter = *rightIter;
+        rightIter++;
       }
-      indOfMergedStruct++;
+      iter++;
     }
-    while (leftBegin + indOfSubStructOne != leftStruct.end())
+    while (leftIter != leftStruct.end())
     {
-      *(begin + indOfMergedStruct) = *(leftBegin + indOfSubStructOne);
-      indOfSubStructOne++;
-      indOfMergedStruct++;
+      *iter = *leftIter;
+      leftIter++;
+      iter++;
     }
-    while (rightBegin + indOfSubStructTwo != rightStruct.end())
+    while (rightIter != rightStruct.end())
     {
-      *(begin + indOfMergedStruct) = *(rightBegin + indOfSubStructTwo);
-      indOfMergedStruct++;
-      indOfSubStructTwo++;
+      *iter = *rightIter;
+      rightIter++;
+      iter++;
     }
   }
 
-  template< class InputIter, class Comporator >
-  void mergeSort(InputIter dataStructIt, size_t begin, size_t end, Comporator comp)
+  template< class InputStruct, class Comporator >
+  void mergeSort(InputStruct& dataStructIt, size_t begin, size_t end, Comporator comp)
   {
-    if (begin <= end)
+    if (begin >= end)
     {
       return;
     }
     size_t mid = begin + (end - begin) / 2;
-    mergeSort< InputIter, Comporator >(dataStructIt, begin, mid, comp);
-    mergeSort< InputIter, Comporator >(dataStructIt, mid + 1, end, comp);
-    merge< InputIter, Comporator >(dataStructIt, begin, mid, end, comp);
+    mergeSort< InputStruct, Comporator >(dataStructIt, begin, mid, comp);
+    mergeSort< InputStruct, Comporator >(dataStructIt, mid + 1, end, comp);
+    merge< InputStruct, Comporator >(dataStructIt, begin, mid, end, comp);
   }
 }
 #endif
