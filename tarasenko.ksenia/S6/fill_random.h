72119f2d8db4951378b12e8cc8d5c19532c15c3e
xeniaTa <kseniatarasenko1@gmail.com>
2023-07-25 16:10:00 +0300

    add func fillRandom

---
+++ tarasenko.ksenia/S6/fill_random.h
@@ -0,0 +1,35 @@
+#ifndef S6_FILL_RANDOM_H
+#define S6_FILL_RANDOM_H
+#include <random>
+#include <stdexcept>
+namespace tarasenko
+{
+  template< typename T >
+  void fillRandom(T& container, size_t size)
+  {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    if (std::is_same< typename T::value_type, int >::value)
+    {
+      std::uniform_int_distribution< int > dis(0, 100);
+      for (size_t i = 0; i < size; ++i)
+      {
+        container.push_back(dis(gen));
+      }
+    }
+    else if (std::is_same< typename T::value_type, float >::value)
+    {
+      std::uniform_real_distribution< float > dis(0.0, 100.0);
+      for (size_t i = 0; i < size; ++i)
+      {
+        container.push_back(dis(gen));
+      }
+    }
+    else
+    {
+      throw std::invalid_argument("Unsupported type");
+    }
+  }
+
+}
+#endif
