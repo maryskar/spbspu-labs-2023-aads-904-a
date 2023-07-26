4c71e792d66bf535892d252648a1ea2b6959e22a
ArsDi <abstractlevl@gmail.com>
2023-07-27 01:51:39 +0300

    basic methods rework

--- dmitriev.arseny/S2/forwardList.h
+++ dmitriev.arseny/S2/forwardList.h
@@ -6,12 +6,21 @@
 namespace
 {
   template< typename T >
-  dmitriev::List< T >* copyForwardList(dmitriev::List< T >* otherFakeNode)
+  dmitriev::List< T >* headInit(dmitriev::List< T >* otherHead)
+  {
+    dmitriev::List< T >* newBeforeHead = static_cast< dmitriev::List< T >* >(::operator new (sizeof(dmitriev::List< T >)));
+    newBeforeHead->otherList = otherHead;
+
+    return newBeforeHead;
+  }
+
+  template< typename T >
+  dmitriev::List< T >* copyForwardList(dmitriev::List< T >* otherHead)
   {
     std::pair< dmitriev::List< T >*, dmitriev::List< T >* > headTail;
-    headTail = dmitriev::copy(otherFakeNode);
+    headTail = dmitriev::copy(otherHead);
 
-    return headTail.first;
+    return headInit(headTail.first);
   }
 }
 
@@ -21,189 +30,92 @@ namespace dmitriev
   class ForwardList
   {
   public:
-    ForwardList();
-    ForwardList(ForwardList< T >&& other);
-    ForwardList(const ForwardList< T >& other);
+    using list = dmitriev::List< T >;
 
-    ForwardList< T >& operator=(ForwardList< T >&& other);
-    ForwardList< T >& operator=(const ForwardList< T >& other);
+    ForwardList():
+      m_beforeHead(static_cast< list* >(::operator new (sizeof(list))))
+    {
+      m_beforeHead->otherList = nullptr;
+    }
+    ForwardList(const ForwardList< T >& other):
+      m_beforeHead(copyForwardList(other.m_beforeHead->otherList))
+    {}
+    ForwardList(ForwardList< T >&& other):
+      m_beforeHead(headInit(other.m_beforeHead->otherList))
+    {
+      other.m_beforeHead->otherList = nullptr;
+    }
 
-    ~ForwardList();
+    ForwardList< T >& operator=(const ForwardList< T >& other)
+    {
+      if (this == std::addressof(other))
+      {
+        return *this;
+      }
+      ForwardList < T > newForwardList = other;
+      *this = std::move(newForwardList);
 
-    void pushFront(const T& data);
-    void pushFront(T&& data);
+      return *this;
+    }
+    ForwardList< T >& operator=(ForwardList< T >&& other)
+    {
+      if (this == std::addressof(other))
+      {
+        return *this;
+      }
+      clear(m_beforeHead->otherList);
+      m_beforeHead->otherList = other.m_beforeHead->otherList;
+      other.m_beforeHead->otherList = nullptr;
 
-    const T& front() const;
-    T& front();
+      return *this;
+    }
 
-    void popFront();
+    ~ForwardList()
+    {
+      dmitriev::clear(m_beforeHead);
+    }
 
-    ForwardIterator< T > beforBegin();
-    ForwardIterator< T > begin();
-    ForwardIterator< T > end();
+    void pushFront(const T& data)
+    {
+      list* newHead = new list{data, m_beforeHead->otherList};
+      m_beforeHead->otherList = newHead;
+    }
+    void pushFront(T&& data)
+    {
+      list* newHead = new list{std::move(data), m_beforeHead->otherList};
+      m_beforeHead->otherList = newHead;
+    }
 
-    ConstForwardIterator< T > beforBeginConst();
-    ConstForwardIterator< T > beginConst();
-    ConstForwardIterator< T > endConst();
+    const T& front() const
+    {
+      return m_beforeHead->otherList->data;
+    }
+    T& front()
+    {
+      return m_beforeHead->otherList->data;
+    }
 
-    bool isEmtpy();
+    void popFront()
+    {
+      list* newHead = m_beforeHead->otherList->otherList;
+      delete m_beforeHead->otherList;
 
-    void clear();
-    //insertAfter();
-    //emplaceAfter();
-    //eraceAfter();
-    //emplaceFront();
-    //resize();
-    //swap();
+      m_beforeHead->otherList = newHead;
+    }
 
-    //spliceAfter();
-    //remove();
-    //removeIf();
-    //revrese();
-
-    //operator==();
-    //operator!=();
-
-    //std::swap(ForwardList)//?
+    bool isEmtpy()
+    {
+      return m_beforeHead->otherList == nullptr;
+    }
+    void clear()
+    {
+      dmitriev::clear(m_beforeHead->otherList);
+    }
 
   private:
-    dmitriev::List< T >* m_FakeNode;
-    //  size_t size;
+    list* m_beforeHead;
 
   };
-}
-
-template< typename T >
-dmitriev::ForwardList< T >::ForwardList():
-  m_FakeNode(new dmitriev::List< T >{T(), nullptr})//может не быть деф констр
-{}
-
-template< typename T >
-dmitriev::ForwardList< T >::ForwardList(ForwardList< T >&& other):
-  m_FakeNode(other.m_FakeNode)
-{
-  other.m_FakeNode = nullptr;
-}
-
-template< typename T >
-dmitriev::ForwardList< T >::ForwardList(const ForwardList< T >& other):
-  m_FakeNode(copyForwardList(other.m_FakeNode))
-{}
-
-template< typename T >
-dmitriev::ForwardList< T >& dmitriev::ForwardList< T >::operator=(ForwardList< T >&& other)
-{
-  if (this == std::addressof(other))
-  {
-    return *this;
-  }
-  clear(m_FakeNode->otherList);
-  m_FakeNode->otherList = other.m_FakeNode->otherList;
-  other.m_FakeNode->otherList = nullptr;
-
-  return *this;
-}
-
-template< typename T >
-dmitriev::ForwardList< T >& dmitriev::ForwardList< T >::operator=(const ForwardList< T >& other)
-{
-  if (this == std::addressof(other))
-  {
-    return *this;
-  }
-  ForwardList < T > newForwardList = other;
-  *this = std::move(newForwardList);
-
-  return *this;
-}
-
-template< typename T >
-dmitriev::ForwardList< T >::~ForwardList()
-{
-  dmitriev::clear(m_FakeNode);
-}
-
-template< typename T >
-void dmitriev::ForwardList< T >::pushFront(const T& data)
-{
-  dmitriev::List< T >* newHead = new dmitriev::List< T >{data, m_FakeNode->otherList};
-  m_FakeNode->otherList = newHead;
-}
-
-template< typename T >
-void dmitriev::ForwardList< T >::pushFront(T&& data)
-{
-  dmitriev::List< T >* newHead = new dmitriev::List< T >{std::move(data), m_FakeNode->otherList};
-  m_FakeNode->otherList = newHead;
-}
-
-template< typename T >
-const T& dmitriev::ForwardList< T >::front() const
-{
-  return m_FakeNode->otherList->data;
-}
-
-template< typename T >
-T& dmitriev::ForwardList< T >::front()
-{
-  return m_FakeNode->otherList->data;
-}
-
-template< typename T >
-void dmitriev::ForwardList< T >::popFront()
-{
-  dmitriev::List< T >* newHead = m_FakeNode->otherList->otherList;
-  delete m_FakeNode->otherList;
-
-  m_FakeNode->otherList = newHead;
-}
-
-template< typename T >
-dmitriev::ForwardIterator< T > dmitriev::ForwardList< T >::beforBegin()
-{
-  return ForwardIterator< T >(m_FakeNode);
-}
-
-template< typename T >
-dmitriev::ForwardIterator< T > dmitriev::ForwardList< T >::begin()
-{
-  return ForwardIterator< T >(m_FakeNode->otherList);
-}
-
-template< typename T >
-dmitriev::ForwardIterator< T > dmitriev::ForwardList< T >::end()
-{
-  return ForwardIterator< T >(nullptr);
-}
-
-template< typename T >
-dmitriev::ConstForwardIterator< T > dmitriev::ForwardList< T >::beforBeginConst()
-{
-  return ConstForwardIterator< T >(m_FakeNode);
-}
-
-template< typename T >
-dmitriev::ConstForwardIterator< T > dmitriev::ForwardList< T >::beginConst()
-{
-  return ConstForwardIterator< T >(m_FakeNode->otherList);
-}
-
-template< typename T >
-dmitriev::ConstForwardIterator< T > dmitriev::ForwardList< T >::endConst()
-{
-  return ConstForwardIterator< T >(nullptr);
-}
-
-template< typename T >
-bool dmitriev::ForwardList< T >::isEmtpy()
-{
-  return m_FakeNode->otherList == nullptr;
-}
-
-template< typename T >
-void dmitriev::ForwardList< T >::clear()
-{
-  dmitriev::clear(m_FakeNode->otherList);
 }
 
 
