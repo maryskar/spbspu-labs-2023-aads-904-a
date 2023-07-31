68ee30ffc2c481c594051a12cad1b1eb98e2c462
Потапова <marina-potapova04@mail.ru>
2023-07-31 15:55:32 +0300

    add overflow checker

--- potapova.marina/S1/countPostfixExpression.cpp
+++ potapova.marina/S1/countPostfixExpression.cpp
@@ -1,33 +1,43 @@
 #include "countPostfixExpression.h"
 #include <iostream>
 #include <istream>
+#include <limits>
+#include <stdexcept>
 #include "queue.h"
 #include "stack.h"
 
-int potapova::countPostfixExpression(expr_queue& postfix_queue)
+bool checkOverflow(const std::int64_t& value, std::int64_t& added)
 {
-  Stack< int > operands_stack;
+  return (std::numeric_limits< std::int64_t >::max() - added >= value);
+}
+
+std::int64_t potapova::countPostfixExpression(expr_queue& postfix_queue)
+{
+  Stack< std::int64_t > operands_stack;
+  std::int64_t added = 0;
   while (!postfix_queue.empty())
   {
     if (postfix_queue.front().type == ArithmExpMember::Type::Num)
     {
-      operands_stack.push(static_cast< int >(postfix_queue.front()));
+      operands_stack.push(postfix_queue.front().num);
       postfix_queue.pop();
     }
     else
     {
-      int operand2 = operands_stack.back();
+      std::int64_t operand2 = operands_stack.back();
       operands_stack.pop();
-      int operand1 = operands_stack.back();
+      std::int64_t operand1 = operands_stack.back();
       operands_stack.pop();
       switch (postfix_queue.front().operation)
       {
         case '+':
           operands_stack.push(operand1 + operand2);
+          added += operand1 + operand2;
           postfix_queue.pop();
           break;
         case '-':
           operands_stack.push(operand1 - operand2);
+          added += operand1 - operand2;
           postfix_queue.pop();
           break;
         case '*':
@@ -45,5 +55,12 @@ int potapova::countPostfixExpression(expr_queue& postfix_queue)
       }
     }
   }
-  return operands_stack.back();
+  if (!checkOverflow(operands_stack.back(), added))
+  {
+    throw std::overflow_error("Overflow");
+  }
+  else
+  {
+    return operands_stack.back();
+  }
 }
--- potapova.marina/S1/countPostfixExpression.h
+++ potapova.marina/S1/countPostfixExpression.h
@@ -1,11 +1,12 @@
 #ifndef COUNTPOSTFIXEXPRESSION_H
 #define COUNTPOSTFIXEXPRESSION_H
 
+#include <cstdint>
 #include "valueType.h"
 
 namespace potapova
 {
-  int countPostfixExpression(expr_queue& postfix_queue);
+  std::int64_t countPostfixExpression(expr_queue& postfix_queue);
 }
 
 #endif
