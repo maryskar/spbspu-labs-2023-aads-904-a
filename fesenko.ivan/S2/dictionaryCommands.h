82e208927e5edd0839dd140b1391d69d849744f9
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-01 18:46:39 +0500

    Add complement command

--- fesenko.ivan/S2/dictionaryCommands.h
+++ fesenko.ivan/S2/dictionaryCommands.h
@@ -23,5 +23,34 @@ namespace fesenko
     }
     out << "\n";
   }
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare > complement(const Dictionary< Key, Value, Compare > &one, const Dictionary< Key, Value, Compare > &two)
+  {
+    Dictionary< Key, Value, Compare > result;
+    if (std::addressof(one) == std::addressof(two)) {
+      return result;
+    }
+    auto it_one = one.cbegin();
+    auto it_two = two.cbegin();
+    Compare comp = Compare{};
+    while (it_one != one.cend() && it_two != two.cend()) {
+      while (it_two != two.cend() && comp(it_two->first, it_one->first)) {
+        it_two++;
+      }
+      if (it_two == two.cend()) {
+        break;
+      }
+      if (it_one->first != it_two->first) {
+        result.insert(*it_one);
+      }
+      it_one++;
+    }
+    while (it_one != one.cend()) {
+      result.insert(*it_one);
+      it_one++;
+    }
+    return result;
+  }
 }
 #endif
