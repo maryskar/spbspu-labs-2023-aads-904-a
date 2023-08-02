01b5b8a44682032979c1493830b1cf473db372c3
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-02 14:20:26 +0500

    Complete main.cpp

--- fesenko.ivan/S2/main.cpp
+++ fesenko.ivan/S2/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include "dictionary.h"
+#include "dictionaryCommands.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,5 +13,69 @@ int main(int argc, char *argv[])
   if (!in.is_open()) {
     std::cerr << "Can`t open the file\n";
     return 2;
+  }
+  using dictionary = fesenko::Dictionary< int, std::string, std::less< > >;
+  using dictionary_elem = std::pair< int, std::string >;
+  using dict_of_dict = fesenko::Dictionary< std::string, dictionary, std::less< std::string > >;
+  using dict_of_dict_elem = std::pair < std::string, dictionary >;
+  dict_of_dict container;
+  try {
+    while (in) {
+      std::string name;
+      in >> name;
+      if (!in) {
+        break;
+      }
+      dictionary dict;
+      int key = 0;
+      std::string value;
+      while (in) {
+        in >> key >> value;
+        if (!in) {
+          break;
+        }
+        dict.insert(dictionary_elem(key, value));
+      }
+      in.clear();
+      container.insert(dict_of_dict_elem(name, dict));
+    }
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  try {
+    while (std::cin) {
+      std::string command;
+      std::cin >> command;
+      if (!std::cin) {
+        break;
+      }
+      if (command == "print") {
+        std::string name;
+        std::cin >> name;
+        fesenko::print(dict_of_dict_elem(name, container.at(name)), std::cout);
+      } else {
+        std::string newDictName;
+        std::string dictName1;
+        std::string dictName2;
+        std::cin >> newDictName >> dictName1 >> dictName2;
+        dictionary dict1 = container.at(dictName1);
+        dictionary dict2 = container.at(dictName2);
+        dictionary newDict;
+        if (command == "complement") {
+          newDict = make_complementation(dict1, dict2);
+        } else if (command == "intersect") {
+          newDict = make_intersection(dict1, dict2);
+        } else if (command == "union") {
+          newDict = make_union(dict1, dict2);
+        } else {
+          fesenko::outInvalidCommandMessage(std::cout);
+          continue;
+        }
+        container.insert(dict_of_dict_elem(newDictName, newDict));
+      }
+    }
+  } catch (...) {
+    fesenko::outInvalidCommandMessage(std::cout);
   }
 }
