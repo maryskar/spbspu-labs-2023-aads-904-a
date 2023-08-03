d8103bd2cc4f24db4961629f941830484cb5ab10
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-03 13:53:13 +0500

    add function to copy list

--- fesenko.ivan/S1/list.h
+++ fesenko.ivan/S1/list.h
@@ -19,5 +19,28 @@ namespace fesenko
     }
     head = nullptr;
   }
+
+  template< typename T >
+  std::pair< List< T > *, List< T > * > copyList(const List< T > *head)
+  {
+    if (head == nullptr) {
+      return {nullptr, nullptr};
+    }
+    List< T > *newHead = nullptr;
+    try {
+      newHead = new List< T > {head->data, nullptr};
+      head = head->next;
+      auto *cur = newHead;
+      while (head) {
+        cur->next = new List< T > {head->data, nullptr};
+        cur = cur->next;
+        head = head->next;
+      }
+      return {newHead, cur};
+    } catch (...) {
+      deleteList(newHead);
+      throw;
+    }
+  }
 }
 #endif
