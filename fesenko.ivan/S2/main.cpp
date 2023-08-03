7bcf070004c108c9df742a60be2d27be591570a6
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-03 13:05:34 +0500

    now program can change existing dictionaries

--- fesenko.ivan/S2/main.cpp
+++ fesenko.ivan/S2/main.cpp
@@ -72,12 +72,9 @@ int main(int argc, char *argv[])
           fesenko::outInvalidCommandMessage(std::cout);
           continue;
         }
-        if (newDictName.compare(dictName1) == 0) {
-          container.at(dictName1) = newDict;
-        } else if (newDictName.compare(dictName2) == 0) {
-          container.at(dictName2) = newDict;
-        } else {
-          container.insert(dict_of_dict_elem(newDictName, newDict));
+        if (container.insert(dict_of_dict_elem(newDictName, newDict)).second == false) {
+          auto it = container.find(newDictName);
+          container.at(it->first) = newDict;
         }
       }
     }
