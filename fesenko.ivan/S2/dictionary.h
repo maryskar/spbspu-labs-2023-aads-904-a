419401981961e6e094dbe5d04d039bb310626995
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-08-05 11:34:24 +0500

    Add S2 to start S4

---
+++ fesenko.ivan/S2/dictionary.h
@@ -0,0 +1,370 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+#include <stdexcept>
+#include "forwardList.h"
+
+namespace fesenko
+{
+  template< typename Key, typename Value, typename Compare >
+  class Dictionary
+  {
+   public:
+    using key_type = Key;
+    using mapped_type = Value;
+    using value_type = std::pair< const key_type, mapped_type >;
+    using key_compare = Compare;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using pointer = value_type *;
+    using const_pointer = const value_type *;
+    using iterator = typename ForwardList< value_type >::iterator;
+    using const_iterator = typename ForwardList< value_type >::const_iterator;
+    using difference_type = std::ptrdiff_t;
+    using size_type = std::size_t;
+    using this_t = Dictionary< Key, Value, Compare >;
+    class value_compare;
+    Dictionary();
+    explicit Dictionary(const key_compare &);
+    Dictionary(const this_t &);
+    Dictionary(this_t &&);
+    ~Dictionary() = default;
+    this_t &operator=(const this_t &);
+    this_t &operator=(this_t &&);
+    iterator before_begin() noexcept;
+    const_iterator before_begin() const noexcept;
+    const_iterator cbefore_begin() const noexcept;
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    const_iterator cbegin() const noexcept;
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
+    const_iterator cend() const noexcept;
+    bool empty() const noexcept;
+    size_type size() const noexcept;
+    mapped_type &operator[](const key_type &);
+    mapped_type &operator[](key_type &&);
+    mapped_type &at(const key_type &);
+    const mapped_type &at(const key_type &) const;
+    std::pair< iterator, bool > insert(const value_type &);
+    template< typename P >
+    std::pair< iterator, bool > insert(P &&);
+    iterator insert(const_iterator, const value_type &);
+    template< typename P >
+    iterator insert(const_iterator, P &&);
+    template< typename InputIterator >
+    void insert(InputIterator, InputIterator);
+    iterator erase_after(const_iterator);
+    iterator erase_after(const_iterator, const_iterator);
+    void swap(this_t &);
+    void clear() noexcept;
+    key_compare key_comp() const;
+    value_compare value_comp() const;
+    iterator find(const key_type &);
+    const_iterator find(const key_type &) const;
+   private:
+    ForwardList< value_type > list_;
+    Compare comp_;
+    size_type size_;
+  };
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary():
+    list_(),
+    comp_(),
+    size_(0)
+  {}
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary(const key_compare &comp):
+    list_(),
+    comp_(comp),
+    size_(0)
+  {}
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary(const this_t &other):
+    list_(other.list_),
+    comp_(other.comp_),
+    size_(other.size_)
+  {}
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary(this_t &&other):
+    list_(std::move(other.list_)),
+    comp_(std::move(other.comp_)),
+    size_(other.size_)
+  {
+    other.size_ = 0;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare > &Dictionary< Key, Value, Compare >::operator=(const this_t &other)
+  {
+    if (this != std::addressof(other)) {
+      Compare temp = other.comp_;
+      list_ = other.list_;
+      comp_ = std::move(temp);
+      size_ = other.size_;
+    }
+    return *this;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare > &Dictionary< Key, Value, Compare >::operator=(this_t &&other)
+  {
+    if (this != std::addressof(other)) {
+      list_ = std::move(other.list_);
+      comp_ = std::move(other.comp_);
+      size_ = other.size_;
+      other.size_ = 0;
+    }
+    return *this;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator Dictionary< Key, Value, Compare >::before_begin() noexcept
+  {
+    return list_.before_begin();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::before_begin() const noexcept
+  {
+    return cbefore_begin();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::cbefore_begin() const noexcept
+  {
+    return list_.cbefore_begin();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator Dictionary< Key, Value, Compare >::begin() noexcept
+  {
+    return list_.begin();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::begin() const noexcept
+  {
+    return cbegin();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::cbegin() const noexcept
+  {
+    return list_.cbegin();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator Dictionary< Key, Value, Compare >::end() noexcept
+  {
+    return list_.end();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::end() const noexcept
+  {
+    return cend();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::cend() const noexcept
+  {
+    return list_.cend();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  bool Dictionary< Key, Value, Compare >::empty() const noexcept
+  {
+    return list_.empty();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::size_type Dictionary< Key, Value, Compare >::size() const noexcept
+  {
+    return size_;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  const Value &Dictionary< Key, Value, Compare >::at(const key_type &key) const
+  {
+    const_iterator cit = find(key);
+    if (cit == cend()) {
+      throw std::out_of_range("There is no such key");
+    }
+    return cit->second;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  Value &Dictionary< Key, Value, Compare >::at(const key_type &key)
+  {
+    return const_cast< Value & >((static_cast< const this_t & >(*this)).at(key));
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  Value &Dictionary< Key, Value, Compare >::operator[](const key_type &key)
+  {
+    try {
+      return at(key);
+    } catch (const std::out_of_range &e) {
+    }
+    return (*((this->insert(std::make_pair(key, mapped_type()))).first));
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  Value &Dictionary< Key, Value, Compare >::operator[](key_type &&key)
+  {
+    return (*this)[key];
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::const_iterator Dictionary< Key, Value, Compare >::find(const key_type &key) const
+  {
+    Compare comp = key_comp();
+    const_iterator cur = cbegin();
+    while (cur != cend()) {
+      if (!comp(cur->first, key) && !comp(key, cur->first)) {
+        break;
+      }
+      cur++;
+    }
+    return cur;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator Dictionary< Key, Value, Compare >::find(const key_type &key)
+  {
+    const_iterator cit = (static_cast< const this_t & >(*this)).find(key);
+    return list_.erase_after(cit, cit);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  std::pair<
+      typename Dictionary< Key, Value, Compare >::iterator,
+      bool
+  > Dictionary< Key, Value, Compare >::insert(const value_type &value)
+  {
+    Compare comp = key_comp();
+    if (empty() || comp(value.first, cbegin()->first)) {
+      return {list_.insert_after(cbefore_begin(), value), true};
+    }
+    iterator cur = before_begin();
+    iterator sup = begin();
+    while (sup != end()) {
+      if (comp(value.first, sup->first)) {
+        if (comp(cur->first, value.first)) {
+          return {list_.insert_after(cur, value), true};
+        } else {
+          return {cur, false};
+        }
+      }
+      cur++;
+      sup++;
+    }
+    if (comp(cur->first, value.first)) {
+      return {list_.insert_after(cur, value), true};
+    }
+    return {end(), false};
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  template< typename P >
+  std::pair<
+      typename Dictionary< Key, Value, Compare >::iterator,
+      bool
+  > Dictionary< Key, Value, Compare >::insert(P &&value)
+  {
+    static_assert(std::is_constructible< value_type, P && >::value, "Can`t construct value type");
+    const value_type temp(std::forward< P >(value));
+    return insert(temp);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator
+      Dictionary< Key, Value, Compare >::insert(const_iterator pos, const value_type &value)
+  {
+    Compare comp = key_comp();
+    const_iterator cur = pos;
+    cur++;
+    if (comp(pos->first, value.first) && comp(value.first, cur->first)) {
+      return list_.insert_after(pos, value);
+    }
+    return (insert(value)).first;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  template< typename P >
+  typename Dictionary< Key, Value, Compare >::iterator
+      Dictionary< Key, Value, Compare >::insert(const_iterator pos, P &&value)
+  {
+    static_assert(std::is_constructible< value_type, P&& >::value, "Can`t construct value type");
+    const value_type temp(std::forward< P >(value));
+    return insert(pos, temp);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  template< typename InputIterator >
+  void Dictionary< Key, Value, Compare >::insert(InputIterator first, InputIterator last)
+  {
+    for (; first != last; first++) {
+      insert(*first);
+    }
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator Dictionary< Key, Value, Compare >::erase_after(const_iterator pos)
+  {
+    return list_.erase_after(pos);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iterator
+      Dictionary< Key, Value, Compare >::erase_after(const_iterator first, const_iterator last)
+  {
+    return list_.erase_after(first, last);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void Dictionary< Key, Value, Compare >::swap(this_t &other)
+  {
+    list_.swap(other.data_);
+    std::swap(comp_, other.comp_);
+    std::swap(size_, other.size_);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void Dictionary< Key, Value, Compare >::clear() noexcept
+  {
+    list_.clear();
+    size_ = 0;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::key_compare Dictionary< Key, Value, Compare >::key_comp() const
+  {
+    return comp_;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::value_compare Dictionary< Key, Value, Compare >::value_comp() const
+  {
+    return value_compare(comp_);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  class Dictionary< Key, Value, Compare >::value_compare
+  {
+   public:
+    bool operator()(const value_type &lhs, const value_type &rhs) const
+    {
+      return comp(lhs.first, rhs.first);
+    };
+   protected:
+    Compare comp;
+    explicit value_compare(Compare c): comp(c) {};
+  };
+
+}
+#endif
---
+++ fesenko.ivan/S2/forwardList.h
@@ -0,0 +1,290 @@
+#ifndef FORWARDLIST_H
+#define FORWARDLIST_H
+#include <list.h>
+#include "forwardIterator.h"
+#include "constForwardIterator.h"
+namespace fesenko
+{
+  template< typename T >
+  class ForwardList
+  {
+   public:
+    using value_type = T;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator = ForwardIterator< T >;
+    using const_iterator = ConstForwardIterator< T >;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using this_t = ForwardList< T >;
+    ForwardList();
+    ForwardList(const this_t &);
+    ForwardList(this_t &&);
+    this_t &operator=(const this_t &);
+    this_t &operator=(this_t &&);
+    ~ForwardList();
+    iterator before_begin() noexcept;
+    const_iterator before_begin() const noexcept;
+    const_iterator cbefore_begin() const noexcept;
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    const_iterator cbegin() const noexcept;
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
+    const_iterator cend() const noexcept;
+    bool empty() const noexcept;
+    void clear() noexcept;
+    void swap(this_t &);
+    reference front();
+    const_reference front() const;
+    iterator insert_after(const_iterator position, const value_type &);
+    iterator insert_after(const_iterator position, value_type &&);
+    iterator insert_after(const_iterator, size_type, const value_type &);
+    void push_front(const value_type &);
+    void push_front(value_type &&);
+    iterator erase_after(const_iterator);
+    iterator erase_after(const_iterator, const_iterator);
+    void pop_front();
+   private:
+    List< T > *fakeNode_;
+    List< T > *begin_;
+    List< T > *end_;
+    size_t size_;
+    void copy(const this_t &);
+  };
+
+  template< typename T >
+  ForwardList< T >::ForwardList():
+    fakeNode_(static_cast< List< T > * >(::operator new (sizeof(List< T >)))),
+    begin_(nullptr),
+    end_(nullptr),
+    size_(0)
+  {
+    fakeNode_->next = nullptr;
+  }
+
+  template< typename T >
+  ForwardList< T >::ForwardList(const this_t &rhs):
+    ForwardList()
+  {
+    copy(rhs);
+  }
+
+  template< typename T >
+  ForwardList< T >::ForwardList(this_t &&rhs):
+    ForwardList()
+  {
+    swap(rhs);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::before_begin() noexcept
+  {
+    return iterator(fakeNode_);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_iterator ForwardList< T >::before_begin() const noexcept
+  {
+    return cbefore_begin();
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_iterator ForwardList< T >::cbefore_begin() const noexcept
+  {
+    return const_iterator(fakeNode_);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::begin() noexcept
+  {
+    return iterator(begin_);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_iterator ForwardList< T >::begin() const noexcept
+  {
+    return cbegin();
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_iterator ForwardList< T >::cbegin() const noexcept
+  {
+    return const_iterator(begin_);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::end() noexcept
+  {
+    return iterator(nullptr);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_iterator ForwardList< T >::end() const noexcept
+  {
+    return cend();
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_iterator ForwardList< T >::cend() const noexcept
+  {
+    return const_iterator(nullptr);
+  }
+
+  template< typename T >
+  bool ForwardList< T >::empty() const noexcept
+  {
+    return begin_ == nullptr;
+  }
+
+  template< typename T >
+  void ForwardList< T >::clear() noexcept
+  {
+    deleteList(begin_);
+    end_ = nullptr;
+    size_ = 0;
+  }
+
+  template< typename T >
+  void ForwardList< T >::swap(this_t &rhs)
+  {
+    std::swap(fakeNode_, rhs.fakeNode_);
+    std::swap(begin_, rhs.begin_);
+    std::swap(end_, rhs.end_);
+    std::swap(size_, rhs.size_);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::reference ForwardList< T >::front()
+  {
+    return begin_->data;
+  }
+
+  template< typename T >
+  typename ForwardList< T >::const_reference ForwardList< T >::front() const
+  {
+    return begin_->data;
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::insert_after(const_iterator pos, const value_type &val)
+  {
+    auto *newNode = new List< T >{val, pos.node_->next};
+    pos.node_->next = newNode;
+    if (pos.node_ == fakeNode_) {
+      begin_ = newNode;
+      if (!end_) {
+        end_ = begin_;
+      }
+      begin_ = newNode;
+    } else if (!newNode->next) {
+      end_ = newNode;
+    }
+    size_++;
+    return iterator(pos.node_->next);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::insert_after(const_iterator pos, value_type &&val)
+  {
+    return insert_after(pos, val);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::insert_after(const_iterator pos, size_type n, const value_type &val)
+  {
+    for (size_type i = 0; i < n; ++i)
+    {
+      pos = insert_after(pos, val);
+    }
+    return iterator(pos.node_->next);
+  }
+
+  template< typename T >
+  void ForwardList< T >::push_front(const value_type &val)
+  {
+     insert_after(cbefore_begin(), val);
+  }
+
+  template< typename T >
+  void ForwardList< T >::push_front(value_type &&val)
+  {
+    insert_after(cbefore_begin(), std::move(val));
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::erase_after(const_iterator pos)
+  {
+    auto next = pos.node_->next;
+    if (next) {
+      pos.node_->next = next->next;
+    }
+    if (pos.node_ == fakeNode_) {
+      begin_ = pos.node_->next;
+    }
+    if (next) {
+      delete next;
+    }
+    size_--;
+    if (size_ == 1) {
+      end_ = nullptr;
+    } else if (size_ == 0) {
+      begin_ = nullptr;
+      end_ = nullptr;
+      fakeNode_->next = nullptr;
+    }
+    return iterator(pos.node_->next);
+  }
+
+  template< typename T >
+  typename ForwardList< T >::iterator ForwardList< T >::erase_after(const_iterator pos, const_iterator last)
+  {
+    while (pos != last) {
+      pos = erase_after(pos);
+    }
+    return iterator(last.node_);
+  }
+
+  template< typename T >
+  void ForwardList< T >::pop_front()
+  {
+    erase_after(cbefore_begin());
+  }
+
+  template< typename T >
+  ForwardList< T > &ForwardList< T >::operator=(const this_t &rhs)
+  {
+    if (std::addressof(rhs) != this) {
+      clear();
+      copy(rhs);
+    }
+    return *this;
+  }
+
+  template< typename T >
+  ForwardList< T > &ForwardList< T >::operator=(this_t &&rhs)
+  {
+    if (std::addressof(rhs) != this) {
+      clear();
+      swap(rhs);
+    }
+    return *this;
+  }
+
+  template< typename T >
+  ForwardList< T >::~ForwardList()
+  {
+    clear();
+    ::operator delete(fakeNode_);
+  }
+
+  template< typename T >
+  void ForwardList< T >::copy(const this_t &rhs)
+  {
+    auto res = copyList(rhs.begin_);
+    begin_ = res.first;
+    end_ = res.second;
+    fakeNode_->next = begin_;
+  }
+}
+#endif
