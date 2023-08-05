f2ab35d2c48565a6693962c9e74fac8a8461b4ba
Artem Romanovich <art.rom.one@gmail.com>
2023-08-05 14:48:43 +0300

    color_tree_node.h added

---
+++ romanovich.artem/S4/color_tree_node.h
@@ -0,0 +1,42 @@
+#ifndef COLOR_TREE_NODE_H
+#define COLOR_TREE_NODE_H
+#include "tree_node.h"
+enum Color
+{
+  RED,
+  BLACK
+};
+template< typename T >
+class ColorTreeNode
+{
+public:
+  explicit ColorTreeNode(const T &data, Color color = RED);
+  TreeNode< T > *getNode() const;
+  void setColor(Color color);
+  Color getColor() const;
+private:
+  TreeNode< T > *node_;
+  Color color_;
+};
+template< typename T >
+Color ColorTreeNode< T >::getColor() const
+{
+  return color_;
+}
+template< typename T >
+void ColorTreeNode< T >::setColor(Color color)
+{
+  color_ = color;
+}
+template< typename T >
+TreeNode< T > *ColorTreeNode< T >::getNode() const
+{
+  return node_;
+}
+template< typename T >
+ColorTreeNode< T >::ColorTreeNode(const T &data, Color color):
+  node_(new TreeNode< T >(data)),
+  color_(color)
+{
+}
+#endif
