af0c655f331effe85a3de7e6dc6a4c0740365181
ArsDi <abstractlevl@gmail.com>
2023-08-07 17:44:52 +0300

    iterators for dic not full realiz

--- dmitriev.arseny/S2/dictionary.h
+++ dmitriev.arseny/S2/dictionary.h
@@ -6,17 +6,118 @@
 namespace dmitriev
 {
 	template< typename Key, typename Value, typename Compare >
+	class Dictionary;
+
+	template< typename Key, typename Value, typename Compare >
+	class ConstDictionaryIterator;
+
+	template< typename Key, typename Value, typename Compare >
+	class DictionaryIterator
+	{
+		friend ConstDictionaryIterator;
+		friend Dictionary;
+
+		DictionaryIterator():
+			fIterator()
+		{}
+		DictionaryIterator(const ForwardList< std::pair< Key, Value > > it):
+			fIterator(it)
+		{}
+
+		DictionaryIterator& operator++()
+		{
+			fIterator++;
+			return *this;
+		}
+		DictionaryIterator& operator++(int)
+		{
+			return DictionaryIterator(++fIterator);
+		}
+
+		DictionaryIterator& operator*() const
+		{
+			return fIterator.operator*();
+		}
+		DictionaryIterator& operator->() const
+		{
+			return ForwardIterator.operator->();
+		}
+
+		bool operator==(const DictionatyIterator& other)
+		{
+			return (fIterator == other.fIterator);
+		}
+		bool operator!=(const DictionatyIterator& other)
+		{
+			return !(*this == other);
+		}
+
+	private:
+		ForwardList< std::pair< Key, Value > >::iterator fIterator;
+
+	};
+
+	template< typename Key, typename Value, typename Compare >
+	class ConstDictionaryIterator
+	{
+		friend Dictionary;
+
+		ConstDictionaryIterator():
+			cFIterator()
+		{}
+		//DictionaryIterator(const ForwardList< std::pair< Key, Value > > it):
+		//	fIterator(it)
+		//{}
+
+		//DictionaryIterator& operator++()
+		//{
+		//	fIterator++;
+		//	return *this;
+		//}
+		//DictionaryIterator& operator++(int)
+		//{
+		//	return DictionaryIterator(++fIterator);
+		//}
+
+		//DictionaryIterator& operator*() const
+		//{
+		//	return fIterator.operator*();
+		//}
+		//DictionaryIterator& operator->() const
+		//{
+		//	return ForwardIterator.operator->();
+		//}
+
+		//bool operator==(const DictionatyIterator& other)
+		//{
+		//	return (fIterator == other.fIterator);
+		//}
+		//bool operator!=(const DictionatyIterator& other)
+		//{
+		//	return !(*this == other);
+		//}
+
+	private:
+		ForwardList< std::pair< Key, Value > >::constIterator cFIterator;
+
+	};
+
+	template< typename Key, typename Value, typename Compare >
 	class Dictionary
 	{
 	public:
-		using fList = ForwardList< Value >;
+		using fList = ForwardList< std::pair< Key, Value > >;
 
 	  Dictionary();
-		~Dictionary();
+		Dictionary& Dictionary(const Dictionary& other);
+		Dictionary& Dictionary(Dictionary&& other);
 
-		void push(Key k, Value v);
-		Value get(Key k);
-		Value drop(Key k);
+
+
+		~Dictionary() = default;
+
+
+
 
 	private:
 		fList m_fList;
