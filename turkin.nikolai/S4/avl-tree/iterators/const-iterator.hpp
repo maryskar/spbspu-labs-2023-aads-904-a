8d7b5d7de8ebbf4cfe1fa72c4dd697c20ee05116
Turkin Nikolai <nikolai.turkin.main@gmail.com>
2023-08-08 19:32:57 +0300

    update const-iterator

--- turkin.nikolai/S4/avl-tree/iterators/const-iterator.hpp
+++ turkin.nikolai/S4/avl-tree/iterators/const-iterator.hpp
@@ -2,6 +2,7 @@
 #define DIRECT_CONST_ITERATOR_HPP
 
 #include <utility>
+#include <cassert>
 #include "../tree-node.hpp"
 
 namespace turkin
@@ -9,15 +10,20 @@ namespace turkin
   template< typename K, typename V, typename C >
   class AVLtree;
   template< typename K, typename V, typename C >
+  class Iterator;
+  template< typename K, typename V, typename C >
   class ConstIterator
   {
     friend class AVLtree< K, V, C >;
+    friend class Iterator< K, V, C >;
+    using it = Iterator< K, V, C >;
     using cit = ConstIterator< K, V, C >;
     using tree_t = std::pair< K, V >;
     using Node = TreeNode< tree_t > *;
     public:
       ConstIterator();
       explicit ConstIterator(Node rhs);
+      explicit ConstIterator(const it & rhs);
       cit & operator=(const cit & rhs) = default;
       ~ConstIterator() = default;
       cit & operator++();
@@ -31,6 +37,118 @@ namespace turkin
     private:
       Node cur_;
   };
+}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C >::ConstIterator():
+  cur_(nullptr)
+{}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C >::ConstIterator(Node rhs):
+  cur_(rhs)
+{}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C >::ConstIterator(const it & rhs):
+  cur_(rhs.cur_)
+{}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C > & turkin::ConstIterator< K, V, C >::operator++()
+{
+  assert(cur_ != nullptr);
+  if (cur_->right != nullptr)
+  {
+    cur_ = cur_->right;
+    while (cur_->left != nullptr)
+    {
+      cur_ = cur_->left;
+    }
+  }
+  else
+  {
+    Node parent = cur_->parent;
+    while (parent != nullptr && cur_ == parent->right)
+    {
+      cur_ = parent;
+      parent = parent->parent;
+    }
+    cur_ = parent;
+  }
+  return * this;
+}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C > turkin::ConstIterator< K, V, C >::operator++(int)
+{
+  assert(cur_ != nullptr);
+  auto result(*this);
+  ++(*this);
+  return result;
+}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C > & turkin::ConstIterator< K, V, C >::operator--()
+{
+  assert(cur_ != nullptr);
+  if (cur_->left != nullptr)
+  {
+    cur_ = cur_->left;
+    while (cur_->right != nullptr)
+    {
+      cur_ = cur_->right;
+    }
+  }
+  else
+  {
+    Node parent = cur_->parent;
+    if (cur_ != nullptr)
+    {
+      while (parent != nullptr && cur_ == parent->left)
+      {
+        cur_ = parent;
+        parent = parent->parent;
+      }
+    }
+    cur_ = parent;
+  }
+  return * this;
+}
+
+template< typename K, typename V, typename C >
+turkin::ConstIterator< K, V, C > turkin::ConstIterator< K, V, C >::operator--(int)
+{
+  assert(cur_ != nullptr);
+  auto result(*this);
+  --(*this);
+  return result;
+}
+
+template< typename K, typename V, typename C >
+const std::pair< K, V > & turkin::ConstIterator< K, V, C >::operator*() const
+{
+  assert(cur_ != nullptr);
+  return cur_->data;
+}
+
+template< typename K, typename V, typename C >
+const std::pair< K, V > * turkin::ConstIterator< K, V, C >::operator->() const
+{
+  assert(cur_ != nullptr);
+  return std::addressof(cur_->data);
+}
+
+template< typename K, typename V, typename C >
+bool turkin::ConstIterator< K, V, C >::operator==(const ConstIterator< K, V, C > & rhs) const
+{
+  return cur_ == rhs.cur_;
+}
+
+template< typename K, typename V, typename C >
+bool turkin::ConstIterator< K, V, C >::operator!=(const ConstIterator< K, V, C > & rhs) const
+{
+  return !(rhs == *this);
 }
 
 #endif
