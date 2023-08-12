a6c378e4c94597a9102e802a67e3e756b509dee8
Artem Romanovich <art.rom.one@gmail.com>
2023-08-12 15:10:04 +0300

    replace

--- romanovich.artem/S0/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-  std::cout << "romanovich.artem\n";
-  return 0;
-}
--- romanovich.artem/common/parsing.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <vector>
-#ifndef PARSING_H
-#define PARSING_H
-void splitString(std::vector< std::string > &elems, const std::string &line, char del);
-#endif
