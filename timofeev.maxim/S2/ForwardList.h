9da9f5e27222e2d03e68337c536cf328729af019
Caspio <maks.timofeev9834@gmail.com>
2023-08-13 19:22:17 +0400

    add forwardlist

---
+++ timofeev.maxim/S2/ForwardList.h
@@ -0,0 +1,16 @@
+//
+// Created by makst on 13.08.2023.
+//
+
+#ifndef SPBSPU_LABS_2023_AADS_904_A_FORWARDLIST_H
+#define SPBSPU_LABS_2023_AADS_904_A_FORWARDLIST_H
+namespace timofeev
+{
+    template< typename T>
+    class ForwardList
+    {
+    public:
+
+    };
+}
+#endif
