368e180fa6641565038bd98074c7ab70070beeb4
Потапова <marina-potapova04@mail.ru>
2023-08-13 21:40:57 +0300

    beginning of dictionary

---
+++ potapova.marina/S2/dictionary.h
@@ -0,0 +1,25 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+namespace potapova
+{
+  template< typename Key, typename Value, typename Compare >
+  class Dictionary 
+  {
+    public:
+      void push(const Key& key, const Value& value);
+      Value& get(const Key& key);
+      const Value& get(const Key& key) const;
+      void erase(const Key& key);
+      bool contains(const Key& key) const;
+      size_t size() const;
+      bool empty() const;
+      void clear();
+      void print(const std::string& dataset) const;
+      void complement(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2);
+      void intersect(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2);
+      void join(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2);
+  };
+}
+
+#endif
