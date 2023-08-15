5b4660cb28e04bd556351038820a0c706d227a8d
Artem Romanovich <art.rom.one@gmail.com>
2023-08-15 21:14:12 +0300

    optimazing programm

--- romanovich.artem/S6/main.cpp
+++ romanovich.artem/S6/main.cpp
@@ -3,6 +3,51 @@
 #include <random>
 #include <vector>
 #include "quicksort.h"
+std::vector< int > generateRandomData(size_t size, int min, int max)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::vector< int > data(size);
+  std::uniform_int_distribution< int > dist(min, max);
+  for (size_t i = 0; i < size; i++)
+  {
+    data[i] = dist(gen);
+  }
+  return data;
+}
+std::vector< float > generateRandomData(size_t size, float min, float max)
+{
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::vector< float > data(size);
+  std::uniform_real_distribution< float > dist(min, max);
+  for (size_t i = 0; i < size; i++)
+  {
+    data[i] = dist(gen);
+  }
+  return data;
+}
+template< typename T >
+void sortData(std::vector< T > &data, const std::string &sortOrder)
+{
+  if (sortOrder == "ascending")
+  {
+    quickSort(data.begin(), data.size(), std::less<>());
+  }
+  else
+  {
+    quickSort(data.begin(), data.size(), std::greater<>());
+  }
+}
+template< typename T >
+void printData(const std::vector< T > &data)
+{
+  for (const T &x: data)
+  {
+    std::cout << x << " ";
+  }
+  std::cout << "\n";
+}
 int main(int argc, char *argv[])
 {
   if (argc != 4)
@@ -13,50 +58,17 @@ int main(int argc, char *argv[])
   std::string sortOrder = argv[1];
   std::string dataType = argv[2];
   size_t size = std::strtoull(argv[3], nullptr, 10);
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::vector< int > dataInt(size);
-  std::vector< float > dataFloat(size);
   if (dataType == "ints")
   {
-    std::uniform_int_distribution< int > dist(1, 10);
-    for (int i = 0; i < size; i++)
-    {
-      dataInt[i] = dist(gen);
-    }
-    if (sortOrder == "ascending")
-    {
-      quickSort(dataInt.begin(), dataInt.size(), std::less<>());
-    }
-    else
-    {
-      quickSort(dataInt.begin(), dataInt.size(), std::greater<>());
-    }
-    for (int x: dataInt)
-    {
-      std::cout << x << " ";
-    }
+    std::vector< int > dataInt = generateRandomData(size, 1, 10);
+    sortData(dataInt, sortOrder);
+    printData(dataInt);
   }
   else if (dataType == "floats")
   {
-    std::uniform_real_distribution< float > dist(1.0, 10.0);
-    for (int i = 0; i < size; i++)
-    {
-      dataFloat[i] = dist(gen);
-    }
-    if (sortOrder == "ascending")
-    {
-      quickSort(dataFloat.begin(), dataFloat.size(), std::less<>());
-    }
-    else
-    {
-      quickSort(dataFloat.begin(), dataFloat.size(), std::greater<>());
-    }
-    for (float x: dataFloat)
-    {
-      std::cout << x << " ";
-    }
+    std::vector< float > dataFloat = generateRandomData(size, 1.0f, 10.0f);
+    sortData(dataFloat, sortOrder);
+    printData(dataFloat);
   }
-  std::cout << "\n";
   return 0;
 }
