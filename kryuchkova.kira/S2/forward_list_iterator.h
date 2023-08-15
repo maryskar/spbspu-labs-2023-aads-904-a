cefab26f3ce40218b0bcd535e0fde073e4f4f9df
KiraGelaz <kiragelaz@yandex.ru>
2023-08-15 15:35:31 +0300

    first step done, me too

--- kryuchkova.kira/S2/forward_list_iterator.h
+++ kryuchkova.kira/S2/forward_list_iterator.h
@@ -13,7 +13,25 @@ namespace kryuchkova
   {
     public:
       using this_t = ForwardIterator< T >;
-  }
+      friend class ForwardList< T >;
+
+      ForwardListIterator():
+        node_(nullptr)
+      {}
+      explicit ForwardIterator(Node< T > * node):
+        node_(node)
+      {}
+      ForwardIterator(const this_t&) = default;
+      ~ForwardIterator() = default;
+
+      ForwardIterator& operator++();
+      ForwardIterator operator++(int);
+      bool operator!=(const this_t&) const;
+      bool operator==(const this_t&) const;
+
+    private:
+      Node< T > * node_;
+  };
 }
 
 #endif
