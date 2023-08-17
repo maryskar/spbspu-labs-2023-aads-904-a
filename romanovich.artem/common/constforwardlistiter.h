634cdd98daadabc82bb024c041e4166618b45610
art <artrom170@gmail.com>
2023-08-18 00:34:26 +0300

    adding requirements for LegacyIterator

--- romanovich.artem/common/constforwardlistiter.h
+++ romanovich.artem/common/constforwardlistiter.h
@@ -16,8 +16,10 @@ namespace romanovich
   public:
     friend class ForwardList< T >;
     friend class ForwardListIterator< T >;
-    using iterator_category = std::forward_iterator_tag;
+    using value_type = T;
     using difference_type = std::ptrdiff_t;
+    using pointer = T*;
+    using reference = T&;
     ConstForwardListIterator();
     explicit ConstForwardListIterator(const ForwardListIterator< T > &other);
     ~ConstForwardListIterator() = default;
--- romanovich.artem/common/forwardlistiter.h
+++ romanovich.artem/common/forwardlistiter.h
@@ -16,8 +16,10 @@ namespace romanovich
   public:
     friend class ForwardList< T >;
     friend class ConstForwardListIterator< T >;
-    using iterator_category = std::forward_iterator_tag;
+    using value_type = T;
     using difference_type = std::ptrdiff_t;
+    using pointer = T*;
+    using reference = T&;
     ForwardListIterator();
     explicit ForwardListIterator(const ConstForwardListIterator< T > &other);
     ~ForwardListIterator() = default;
