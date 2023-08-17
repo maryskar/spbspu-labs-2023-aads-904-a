ac5d216e5afe7fb3f93e47e0c31e0c5947917aef
Artem Romanovich <art.rom.one@gmail.com>
2023-08-17 23:38:19 +0300

    change files

---
+++ romanovich.artem/S4/commands.cpp
@@ -0,0 +1,127 @@
+#include "commands.h"
+#include <printmessages.h>
+namespace
+{
+  using map_ref = romanovich::map_t &;
+  using const_map_ref = const romanovich::map_t &;
+  struct ComplementOperation
+  {
+    void operator()(map_ref newDict, const_map_ref firstDict, const_map_ref secondDict) const
+    {
+      for (const auto &entry: firstDict)
+      {
+        if (secondDict.count(entry.first) == 0)
+        {
+          newDict[entry.first] = entry.second;
+        }
+      }
+    }
+  };
+  struct IntersectOperation
+  {
+    void operator()(map_ref newDict, const_map_ref firstDict, const_map_ref secondDict) const
+    {
+      for (const auto &entry: firstDict)
+      {
+        int i = entry.first;
+        if (secondDict.count(i) > 0)
+        {
+          newDict[i] = entry.second;
+        }
+      }
+    }
+  };
+  struct UnionOperation
+  {
+    void operator()(map_ref newDict, const_map_ref firstDict, const_map_ref secondDict) const
+    {
+      for (auto &entry: secondDict)
+      {
+        newDict[entry.first] = entry.second;
+      }
+      for (auto &entry: firstDict)
+      {
+        newDict[entry.first] = entry.second;
+      }
+    }
+  };
+}
+namespace romanovich
+{
+  std::unordered_map< std::string, CommandHandler > createCommandDictionary(container_t &map)
+  {
+    std::string printCall = "print";
+    std::string complementCall = "complement";
+    std::string intersectCall = "intersect";
+    std::string unionCall = "union";
+    using namespace std::placeholders;
+    std::unordered_map< std::string, CommandHandler > commands;
+    commands[printCall] = std::bind(printCommand, _1, _2, std::ref(map));
+    commands[complementCall] = std::bind(performCommand, _1, _2, std::ref(map), ComplementOperation());
+    commands[intersectCall] = std::bind(performCommand, _1, _2, std::ref(map), IntersectOperation());
+    commands[unionCall] = std::bind(performCommand, _1, _2, std::ref(map), UnionOperation());
+    return commands;
+  }
+  void performCommand(std::istream &in,
+                        std::ostream &out,
+                        container_t &map, const std::function< void(map_t &, const map_t &, const map_t &) > &operation)
+  {
+    std::string newDictName, mapName1, mapName2;
+    in >> newDictName >> mapName1 >> mapName2;
+    if (!in)
+    {
+      throw std::runtime_error("Error while reading command arguments.");
+    }
+    if (map.count(mapName1) == 0 && map.count(mapName2) == 0)
+    {
+      throw std::runtime_error("Error: both mapionaries not found.");
+    }
+    if (map.count(mapName1) == 0 || map.count(mapName2) == 0)
+    {
+      std::string errDictName = map.count(mapName1) == 0 ? mapName1 : mapName2;
+      throw std::runtime_error("Error: map \"" + errDictName + "\" not found.");
+    }
+    if (map.empty())
+    {
+      printEmpty(out) << "!\n";
+    }
+    else
+    {
+      const auto &map1 = map[mapName1];
+      const auto &map2 = map[mapName2];
+      map_t newDict;
+      operation(newDict, map1, map2);
+      map[newDictName] = newDict;
+    }
+  }
+  void printCommand(std::istream &in, std::ostream &out, container_t &map)
+  {
+    std::string mapName;
+    in >> mapName;
+    if (!in)
+    {
+      throw std::runtime_error("Error while reading command arguments.");
+    }
+    if (map.count(mapName) <= 0)
+    {
+      throw std::runtime_error("Error: map not found.");
+    }
+    else
+    {
+      const auto &mapData = map[mapName];
+      if (mapData.empty())
+      {
+        printEmpty(out) << "\n";
+      }
+      else
+      {
+        out << mapName;
+        for (const auto &item: mapData)
+        {
+          out << " " << item.first << " " << item.second;
+        }
+        out << "\n";
+      }
+    }
+  }
+}
---
+++ romanovich.artem/common/const_bidirectional_iterator.h
@@ -0,0 +1,169 @@
+#ifndef CONST_BIDIRECTIONAL_ITERATOR_H
+#define CONST_BIDIRECTIONAL_ITERATOR_H
+#include <iterator>
+#include "bidirectional_iterator.h"
+namespace romanovich
+{
+  template< typename Key, typename Value, typename Compare >
+  class RedBlackTree;
+  template< typename Key, typename Value, typename Compare = std::less<> >
+  class ConstBidirectionalIterator
+  {
+  public:
+    friend class BinarySearchTree< Key, Value, Compare >;
+    friend class BidirectionalIterator< Key, Value, Compare >;
+    friend class RedBlackTree< Key, Value, Compare >;
+    using data_t = std::pair< Key, Value >;
+    using const_data_t = const std::pair< Key, Value >;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = data_t;
+    using difference_type = std::ptrdiff_t;
+    using pointer = data_t *;
+    using reference = data_t &;
+    ~ConstBidirectionalIterator() = default;
+    ConstBidirectionalIterator();
+    ConstBidirectionalIterator(const ConstBidirectionalIterator< Key, Value, Compare > &) = default;
+    explicit ConstBidirectionalIterator(const BidirectionalIterator< Key, Value, Compare > &);
+    explicit ConstBidirectionalIterator(TreeNode< data_t > *root,
+                                          TreeNode< data_t > *node, TreeNode< data_t > *fakeNode);
+    ConstBidirectionalIterator< Key, Value, Compare > &
+      operator=(const ConstBidirectionalIterator< Key, Value, Compare > &) = default;
+    const_data_t &operator*() const;
+    const_data_t *operator->() const;
+    ConstBidirectionalIterator< Key, Value, Compare > &operator++();
+    ConstBidirectionalIterator< Key, Value, Compare > operator++(int);
+    ConstBidirectionalIterator< Key, Value, Compare > &operator--();
+    ConstBidirectionalIterator< Key, Value, Compare > operator--(int);
+    bool operator!=(const ConstBidirectionalIterator< Key, Value, Compare > &other) const;
+    bool operator==(const ConstBidirectionalIterator< Key, Value, Compare > &other) const;
+  private:
+    const TreeNode< data_t > *node_;
+    const TreeNode< data_t > *fakeNode_;
+    const TreeNode< data_t > *root_;
+  };
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare >::ConstBidirectionalIterator():
+    node_(nullptr),
+    fakeNode_(nullptr),
+    root_(nullptr)
+  {
+  }
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare >::ConstBidirectionalIterator(
+    const BidirectionalIterator< Key, Value, Compare > &rhs):
+    node_(rhs.node_),
+    fakeNode_(rhs.fakeNode_),
+    root_(rhs.root_)
+  {
+  }
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare >::ConstBidirectionalIterator(TreeNode< data_t > *root,
+                                                                                  TreeNode< data_t > *node,
+                                                                                  TreeNode< data_t > *fakeNode):
+    node_(node),
+    fakeNode_(fakeNode),
+    root_(root)
+  {
+  }
+  template< typename Key, typename Value, typename Compare >
+  const std::pair< Key, Value > &ConstBidirectionalIterator< Key, Value, Compare >::operator*() const
+  {
+    return node_->data;
+  }
+  template< typename Key, typename Value, typename Compare >
+  const std::pair< Key, Value > *ConstBidirectionalIterator< Key, Value, Compare >::operator->() const
+  {
+    return std::addressof(operator*());
+  }
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare > &ConstBidirectionalIterator< Key, Value, Compare >::operator++()
+  {
+    if (node_ == fakeNode_)
+    {
+      return *this;
+    }
+    if (node_->right)
+    {
+      node_ = node_->right;
+      while (node_->left)
+      {
+        node_ = node_->left;
+      }
+    }
+    else
+    {
+      const TreeNode< data_t > *prev = nullptr;
+      do
+      {
+        prev = node_;
+        node_ = node_->parent;
+      } while (node_ && node_->right == prev);
+    }
+    if (!node_)
+    {
+      node_ = fakeNode_;
+    }
+    return *this;
+  }
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare > ConstBidirectionalIterator< Key, Value, Compare >::operator++(int)
+  {
+    ConstBidirectionalIterator< Key, Value, Compare > tmp(*this);
+    ++(*this);
+    return tmp;
+  }
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare > &ConstBidirectionalIterator< Key, Value, Compare >::operator--()
+  {
+    if (!node_)
+    {
+      throw std::runtime_error("Decrementing null iterator.");
+    }
+    if (node_ == fakeNode_)
+    {
+      return *this;
+    }
+    if (node_->left)
+    {
+      node_ = node_->left;
+      while (node_->right)
+      {
+        node_ = node_->right;
+      }
+    }
+    else
+    {
+      const TreeNode< data_t > *prev = nullptr;
+      do
+      {
+        prev = node_;
+        node_ = node_->parent;
+      } while (node_ && node_->left == prev);
+    }
+    if (!node_)
+    {
+      node_ = fakeNode_;
+    }
+    return *this;
+  }
+  template< typename Key, typename Value, typename Compare >
+  ConstBidirectionalIterator< Key, Value, Compare > ConstBidirectionalIterator< Key, Value, Compare >::operator--(int)
+  {
+    ConstBidirectionalIterator< Key, Value, Compare > tmp(*this);
+    --(*this);
+    return tmp;
+  }
+  template< typename Key, typename Value, typename Compare >
+  bool ConstBidirectionalIterator< Key, Value, Compare >::operator==(
+    const ConstBidirectionalIterator< Key, Value, Compare > &other) const
+  {
+    return node_ == other.node_;
+  }
+  template< typename Key, typename Value, typename Compare >
+  bool ConstBidirectionalIterator< Key, Value, Compare >::operator!=(
+    const ConstBidirectionalIterator< Key, Value, Compare > &other) const
+  {
+    return !(*this == other);
+  }
+}
+#endif
