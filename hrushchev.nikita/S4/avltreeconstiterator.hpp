ff1f1ce8a2e2efe96894f27a6f63484f8c236f02
Shuzamada <shuzamada@inbox.ru>
2023-08-18 15:28:47 +0300

    AVLTreeConstIterator()

--- hrushchev.nikita/S4/avltreeconstiterator.hpp
+++ hrushchev.nikita/S4/avltreeconstiterator.hpp
@@ -1,11 +1,32 @@
 #ifndef AVLTREECONSTITERATOR
 #define AVLTREECONSTITERATOR
 
+#include <utility>
+
+template< typename T >
+class Tree;
+
+template< typename Key, typename Value, typename Compare >
+class AVLTree;
+
+template< typename Key, typename Value, typename Compare >
+class AVLTreeConstIterator;
+
+template< typename Key, typename Value, typename Compare >
 class AVLTreeConstIterator
 {
   public:
+    using data_t = std::pair< Key, Value >;
     AVLTreeConstIterator();
     ~AVLTreeConstIterator() = default;
+  private:
+    Tree< data_t >* ptr_;
 };
+
+template< typename Key, typename Value, typename Compare >
+AVLTreeConstIterator< Key, Value, Compare >::AVLTreeConstIterator():
+  ptr_(nullptr)
+{
+}
 
 #endif
