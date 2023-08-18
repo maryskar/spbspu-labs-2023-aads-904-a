19801676e1e0db5fdc48cd81e7bf2fa225131cc3
Потапова <marina-potapova04@mail.ru>
2023-08-18 19:19:49 +0300

    add methods which are necessary in the task

--- potapova.marina/S2/dictionary.h
+++ potapova.marina/S2/dictionary.h
@@ -2,6 +2,8 @@
 #define DICTIONARY_H
 
 #include <stdexcept>
+#include <iostream>
+#include <unordered_map>
 #include "forwardList.h"
 
 namespace potapova
@@ -85,10 +87,88 @@ namespace potapova
         return data.clear();
       }
       
-      void print(const std::string& dataset) const;
-      void complement(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2);
-      void intersect(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2);
-      void join(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2);
+      void print(const std::string& dataset) const
+      {
+        if (data.empty())
+        {
+          std::cout << "<EMPTY>" << '\n';
+          return;
+        }
+        auto it = data.lower_bound(dataset);
+        if (it == data.end() && it->key == dataset)
+        {
+          std::cout << it->key << " " << it->value << " ";
+          ++it;
+        }
+        std::cout << '\n';
+      }
+
+      void complement(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2)
+      {
+        Dictionary< Key, Value, Compare > result;
+        Dictionary< Key, Value, Compare >* ptr_dataset1 = getDictionaryPointer(dataset1);
+        Dictionary< Key, Value, Compare >* ptr_dataset2 = getDictionaryPointer(dataset2);
+        if (ptr_dataset1 == nullptr || ptr_dataset2 == nullptr)
+        {
+          throw std::runtime_error("One or both datasets not found");
+        }
+        for (auto it = ptr_dataset1->data.begin(); it != ptr_dataset1->data.end(); ++it)
+        {
+          const Key& key = it->key;
+          const Value& value = it->value;
+          if (!ptr_dataset1->find(key))
+          {
+            result.push(key, value);
+          }
+        }
+        dictionaries[new_dataset] = result;
+      }
+
+      void intersect(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2)
+      {
+        if (!dictionaries.count(dataset1) || !dictionaries.count(dataset2))
+        {
+          throw std::runtime_error("Not found input dictionaries");
+        }
+        const std::unordered_map< Key, Value >& dictionary1 = dictionaries[dataset1];
+        const std::unordered_map< Key, Value >& dictionary2 = dictionaries[dataset2];
+        std::unordered_map< Key, Value > intersection;
+        for (const auto& pair1 : dictionary1)
+        {
+          const Key& key = pair1.first;
+          if (dictionary2.count(key))
+          {
+            if (pair1.first != pair2.first)
+            {
+              intersection[key] = pair1.second;
+            }
+          }
+        }
+        dictionaries[new_dataset] = intersection;
+      }
+      
+      void join(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2)
+      {
+        if (!dictionaries.count(dataset1) || !dictionaries.count(dataset2))
+        {
+          throw std::runtime_error("Not found input dictionaries");
+        }
+        const std::unordered_map< Key, Value >& dictionary1 = dictionaries[dataset1];
+        const std::unordered_map< Key, Value >& dictionary2 = dictionaries[dataset2];
+        std::unordered_map< Key, Value > new_dictionary;
+        for (const auto& pair1 : dictionary1)
+        {
+          new_dictionary[pair1.first] = pair1.second;
+        }
+        for (const auto& pair2 : dictionary2)
+        {
+          if (pair1.first != pair2.first)
+          {
+            new_dictionary[pair2.first] = pair2.second;
+          }
+        }
+        dictionaries[new_dataset] = new_dictionary;
+      }
     private:
       struct Node
       {
@@ -121,6 +201,7 @@ namespace potapova
 
       ForwardList< Node > data;
       CompareWrapper compare;
+      std::unordered_map< std::string, std::string > dictionaries;
   };
 }
 
