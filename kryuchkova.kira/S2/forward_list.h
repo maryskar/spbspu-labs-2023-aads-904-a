b8d5472efea26f66b5b150999b0b6c4ed7aa8fe1
KiraGelaz <kiragelaz@yandex.ru>
2023-08-18 12:05:55 +0300

    Constructors and destructors for forward list

--- kryuchkova.kira/S2/forward_list.h
+++ kryuchkova.kira/S2/forward_list.h
@@ -1,13 +1,25 @@
 #ifndef FORWARD_LIST_H
 #define FORWARD_LIST_H
-#include "../common/node.h"
+#include <iterator>
+#include <initializer_list>
 #include "forward_list_const_iterator.h"
 #include "forward_list_iterator.h"
 
-template< typename T >
-class ForwardList 
+namespace kryuchkova
 {
-
-};
+  template< typename T >
+  class ForwardList
+  {
+  public:
+    using iterator = ForwardIterator< T >;
+    using const_iterator = ConstForwardIterator< T >;
+    using this_t = ForwardList< T >;
+    ForwardList();
+    ForwardList(const this_t & rhs);
+    ForwardList(this_t && rhs);
+    ForwardList(std::initializer_list< T > init);
+    ~ForwardList();
+  };
+}
 
 #endif
