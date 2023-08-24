43409c7af5931858018ba66638208fa05268bb2e
Потапова <marina-potapova04@mail.ru>
2023-08-24 11:26:02 +0300

    add names of types + their methods

--- potapova.marina/S2/dictionary.h
+++ potapova.marina/S2/dictionary.h
@@ -2,8 +2,6 @@
 #define DICTIONARY_H
 
 #include <stdexcept>
-#include <iostream>
-#include <unordered_map>
 #include "forwardList.h"
 
 namespace potapova
@@ -12,6 +10,39 @@ namespace potapova
   class Dictionary 
   {
     public:
+      using Iterator = ForwardList::Iterator;
+      using ConstIterator = ForwardList::ConstIterator;
+
+      Iterator begin()
+      {
+        return data_.begin();
+      }
+
+      ConstIterator begin() const
+      {
+        return data_.begin();
+      }
+
+      ConstIterator cbegin() const
+      {
+        return data_.cbegin();
+      }
+
+      Iterator end()
+      {
+        return data_.end();
+      }
+
+      ConstIterator end() const
+      {
+        return data_.end();
+      }
+
+      ConstIterator cend() const
+      {
+        return data_.cend();
+      }
+      
       void push(const Key& key, const Value& value)
       {
         Node new_node(key, value);
@@ -122,6 +153,16 @@ namespace potapova
           }
         }
         dictionaries[new_dataset] = result;
+      }
+
+      Dictionary< Key, Value, Compare >* getDictionaryPointer(const std::string& dataset)
+      {
+        auto iter = dictionaries.find(dataset);
+        if (iter != dictionaries.end())
+        {
+          return &(iter->second);
+        }
+        return nullptr;
       }
 
       void intersect(const std::string& new_dataset, const std::string& dataset1, const std::string& dataset2)
