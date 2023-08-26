2f872e24efe1054f0623ba1b77812058766d5ece
Shuzamada <shuzamada@inbox.ru>
2023-08-26 16:22:04 +0300

    start random

---
+++ hrushchev.nikita/S6/filllist.hpp
@@ -0,0 +1,12 @@
+#ifndef FILLLIST_HPP
+#define FILLLIST_HPP
+
+#include <forwardlist.hpp>
+
+namespace hrushchev
+{
+  void fillints(ForwardList< int >& list, size_t size);
+  void fillfloats(ForwardList< float >& list, size_t size);
+}
+
+#endif
