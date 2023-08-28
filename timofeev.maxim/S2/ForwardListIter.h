7e3e8d70dbe2d621612a9d5cb06dce15e3db312c
Caspio <maks.timofeev9834@gmail.com>
2023-08-28 16:35:02 +0400

    forwardlistiter update

--- timofeev.maxim/S2/ForwardListIter.h
+++ timofeev.maxim/S2/ForwardListIter.h
@@ -1,24 +1,50 @@
 #ifndef FORWARDLISTITER_H
 #define FORWARDLISTITER_H
-
+#include <List.h>
 namespace timofeev
 {
-  template< typename T >
+  template<typename T>
   class ForwardListIterator
   {
-    friend class ForwardList< T >;
+    friend class ForwardList<T>;
+
   public:
     ForwardListIterator();
     ~ForwardListIterator() = default;
 
+    ForwardListIterator(timofeev::List<T> *other);
+    ForwardListIterator(const ForwardListConstIterator <T> &other) noexcept;
 
-   /* ForwardListIterator (TElement *start) noexcept
-    bool 	operator!= (ForwardListIterator other) const
-    TElement & 	operator* () const
-    ForwardListIterator & 	operator++ ()
-    ForwardListIterator 	operator++ (int)
-    TElement * 	operator-> () const
-    bool 	operator== (ForwardListIterator other) const*/
+    bool operator!=(ForwardListIterator<T> &other) const
+    bool operator==(ForwardListIterator<T> &other) const
+
+    T &operator*();
+    T *operator->();
+
+    ForwardListIterator<T> &operator++();
+    ForwardListIterator<T> operator++(int);
+
+    /* ForwardListIterator (TElement *start) noexcept
+     bool 	operator!= (ForwardListIterator other) const
+     TElement & 	operator* () const
+     ForwardListIterator & 	operator++ ()
+     ForwardListIterator 	operator++ (int)
+     TElement * 	operator-> () const
+     bool 	operator== (ForwardListIterator other) const*/
+
+  private:
+    timofeev::List<T> *node_;
   };
 }
+  template< typename T>
+  timofeev::ForwardListIterator< T >::ForwardListIterator():
+    node_(nullptr)
+  {}
+
+  template< typename T >
+  timofeev::ForwardListIterator< T >::ForwardListIterator(timofeev::List< T >* other);
+  {
+    node = other;
+  }
+
 #endif //SPBSPU_LABS_2023_AADS_904_A_FORWARDLISTITER_H
