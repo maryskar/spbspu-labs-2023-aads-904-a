0069cf1ca2a4a95cffc996bd51878feba4ddd8d3
Caspio <maks.timofeev9834@gmail.com>
2023-08-28 18:37:37 +0400

    update

--- timofeev.maxim/S2/ForwardList.h
+++ timofeev.maxim/S2/ForwardList.h
@@ -1,77 +1,80 @@
-#ifndef SPBSPU_LABS_2023_AADS_904_A_FORWARDLIST_H
-#define SPBSPU_LABS_2023_AADS_904_A_FORWARDLIST_H
+#ifndef FORWARDLIST_H
+#define FORWARDLIST_H
 #include <cstddef>
 #include "ForwardListIter.h"
-#include <List.h>
+#include "ForwardListConstIter.h"
+#include "../common/List.h"
 namespace timofeev
 {
-    template< typename T>
-    class ForwardList
-    {
-    public:
-        using iter = ForwardListIter< T >;
-        using constIter = ForwardListConstIter< T >;
+  template< typename T>
+  class ForwardList
+  {
+  public:
+    using iter = ForwardListIterator< T >;
+    using constIter = ForwardListConstIterator< T >;
 
-        ForwardList();
-        ~ForwardList();
+    ForwardList();
+    ~ForwardList();
 
-        ForwardList(const ForwardList< T > &lhs);  //
-        ForwardList(ForwardList< T > &&rhs) noexcept;
-        ForwardList< T > &operator=(const ForwardList< T > &rhs);
-        ForwardList< T > &operator=(ForwardList< T > &&rhs);
+    ForwardList(const ForwardList< T > &lhs);  //
+    ForwardList(ForwardList< T > &&rhs) noexcept;
+    ForwardList< T > &operator=(const ForwardList< T > &rhs);
+    ForwardList< T > &operator=(ForwardList< T > &&rhs);
 
-        bool empty() const;  //
-        void clear();  //
+    bool empty() const;  //
+    void clear();  //
 
-        iter before_begin() noexcept;
-        iter begin() noexcept;
-        iter end() noexcept;
+    iter before_begin() noexcept;
+    iter begin() noexcept;
+    iter end() noexcept;
 
-        constIter begin()  noexcept;//
-        constIter cbegin() const noexcept;
-        constIter end()  noexcept;//
-        constIter cend() const noexcept;
-        constIter cbefore_begin() const noexcept;
+    //constIter begin()  noexcept;//
+    constIter cbegin() const noexcept;
+    //constIter end()  noexcept;//
+    constIter cend() const noexcept;
+    constIter cbefore_begin() const noexcept;
 
-        iter insert_after(constIter pos, const T &value);  //
-        iter insert_after(constIter pos, T &&value );   //
-        iter insert_after(constIter pos, size_t count, const T &value );  //
-        iter insert_after(constIter pos, iter first, iter last );  //
-        //iter insert_after( const_iterator pos, std::initializer_list<T> ilist );
+    iter insert_after(constIter pos, const T &value);  //
+    iter insert_after(constIter pos, T &&value );   //
+    iter insert_after(constIter pos, size_t count, const T &value );  //
+    iter insert_after(constIter pos, iter first, iter last );  //
+    //iter insert_after( const_iterator pos, std::initializer_list<T> ilist );
 
-        iter erase_after(constIter pos);  //
-        iter erase_after(constIter first, constIter last);  //
+    iter erase_after(constIter pos);  //
+    iter erase_after(constIter first, constIter last);  //
 
-        template< typename ...Args >
-        iterator emplace_after(const_iterator pos, Args &&...args);
-        template< typename ...Args >
-        void emplace_front(Args &&...args);
+    template< typename ...Args >
+    iter emplace_after(constIter pos, Args &&...args);
+    template< typename ...Args >
+    void emplace_front(Args &&...args);
 
-        void push_front(const T &value); //
-        void push_front(T &&value); //
-        void pop_front(); //
+    void push_front(const T &value); //
+    void push_front(T &&value); //
+    void pop_front(); //
 
-        void resize(size_t count);
-        void resize(size_t count, const T& value);
-        void swap(ForwardList< T >& other);
-        void splice_after(constIter pos, ForwardList< T > &other);
-        void splice_after(constIter pos, ForwardList< T > &&other);
+    void resize(size_t count);
+    void resize(size_t count, const T& value);
+    void swap(ForwardList< T >& other);
+    void splice_after(constIter pos, ForwardList< T > &other);
+    void splice_after(constIter pos, ForwardList< T > &&other);
 
-        void remove(const T& value);
-        void remove_if(T p);
+    void remove(const T& value);
+    void remove_if(T p);
 
-        size_t size() const noexcept;
+    size_t size() const noexcept;
 
-    private:
-        List< T > *head_;
-        size_t size_;
-    };
+  private:
+    List< T > *head_;
+    size_t size_;
+  };
 
-    template< typename T>
-    ForwardList< T >::ForwardList():
-      head_(nullptr),
-      size_(0)
-    {}
+
+
+  template< typename T>
+  ForwardList< T >::ForwardList():
+    head_(nullptr),
+    size_(0)
+  {}
 
     template< typename T >
     void ForwardList< T >::clear()
@@ -87,7 +90,7 @@ namespace timofeev
     }
 
     template< typename T >
-    ForwardList< T >::~ForwardList():
+    ForwardList< T >::~ForwardList()
     {
         clear();
         ::operator delete(head_);
@@ -108,7 +111,7 @@ namespace timofeev
         node->data = value;
         node->next = head_;
         head_ = node;
-        size_++:
+        size_++;
     }
 
     template< typename T >
@@ -170,7 +173,7 @@ namespace timofeev
     }
 
     template< typename  T >
-    ForwardList< T >::iter ForwardList<T>::insert_after(constIter pos, iter first, iter last ):
+    ForwardList< T >::iter ForwardList<T>::insert_after(constIter pos, iter first, iter last )
     {
         while (first != last)
         {
@@ -182,11 +185,12 @@ namespace timofeev
     }
 /*        iter erase_after(constIter pos);
         iter erase_after(constIter first, constIter last);*/
+
     template< typename  T >
     ForwardList< T >::iter ForwardList<T>::erase_after(constIter pos)
     {
         //чек проверку
-        if (next = nullptr)
+        if (pos->next = nullptr)
         {
             return nullptr;
         }
@@ -210,7 +214,6 @@ namespace timofeev
         return last;
     }
 
-
     template< typename T >
     ForwardList< T >::ForwardList(const ForwardList< T > &lhs):
       ForwardList()
@@ -221,6 +224,7 @@ namespace timofeev
       }
       insert_after(before_begin(), lhs.cbegin(), lhs.cend());
     }
+
 
 }
 #endif
