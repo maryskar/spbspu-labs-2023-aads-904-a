de3c3e696d25c92e3a1f671b165653f52371a241
Caspio <maks.timofeev9834@gmail.com>
2023-08-30 17:35:52 +0400

    Create commands

---
+++ timofeev.maxim/S2/Commands.h
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include "dictionary.h"
+namespace timofeev
+{
+  void Print(std::istream& in );
+  void Complement(std::istream& in);
+  void Intersect(std::istream& in);
+  void Union(std::istream& in);
+}
+
+#endif //S2_COMMANDS_H
