3f4cc24a3c51f4a6db908b69917ce4e1bf3b11ba
Caspio <maks.timofeev9834@gmail.com>
2023-08-31 16:44:05 +0400

    Dictionary const and distr

--- timofeev.maxim/S2/Dictionary.h
+++ timofeev.maxim/S2/Dictionary.h
@@ -1,16 +1,43 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
-
+#include "ForwardList.h"
 namespace timofeev
 {
-  template< typename Key, typename Value, typename Compare >
+  template< typename Key, typename Value, typename Compare = std::less< Key > >
   class Dictionary
   {
   public:
+    using iter = ForwardListIterator< std::pair< Key, Value > >;
+    using constIter = ForwardListConstIterator< std::pair< Key, Value > >;
+
     void push(Key k, Value v);
     Value get(Key k);
     Value drop(Key k);
 
+    Dictionary();
+    ~Dictionary() = default;
+
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+
+    void clear();
+
+  private:
+    ForwardList< std::pair< Key, Value > > data_;
+    Compare compare_;
+    size_t size_;
   };
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary():
+    data_(),
+    compare_(),
+    size_()
+  {}
+
+  template< typename Key, typename Value, typename Compare >
+
+
 }
+
 #endif //S2_DICTIONARY_H
