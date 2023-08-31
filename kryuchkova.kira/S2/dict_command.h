a8106ea22087208c4cbb19d24ed96cdfc97c6cea
KiraGelaz <kiragelaz@yandex.ru>
2023-08-31 18:09:54 +0300

    add print func

--- kryuchkova.kira/S2/dict_command.h
+++ kryuchkova.kira/S2/dict_command.h
@@ -3,11 +3,27 @@
 #include <string>
 #include <fstream>
 #include "dictionary.h"
+#include "out_messages.h"
 
 namespace kryuchkova
 {
-  
+  template< typename Key, typename Value, typename Compare >
+  void print(const std::pair< std::string, Dictionary< Key, Value, Compare > > & rhs, std::ostream & out)
+  {
+    auto dict = rhs.second;
+    auto cit = dict.cbegin();
+    if (cit == dict.cend())
+    {
+      outEmpty(out);
+      return;
+    }
+    out << rhs.first;
+    while (cit != dict.cend())
+    {
+      out << " " << cit->first << " " << cit->second;
+      cit++;
+    }
+  }
 }
-
 
 #endif
