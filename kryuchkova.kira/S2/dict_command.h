75df6eedd20cca644cd32bb605a70bce431a0aa3
KiraGelaz <kiragelaz@yandex.ru>
2023-08-31 18:11:20 +0300

    add complementation func

--- kryuchkova.kira/S2/dict_command.h
+++ kryuchkova.kira/S2/dict_command.h
@@ -24,6 +24,42 @@ namespace kryuchkova
       cit++;
     }
   }
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare > make_comp(const Dictionary< Key, Value, Compare > & first_, const Dictionary< Key, Value, Compare > & second_)
+  {
+    Dictionary< Key, Value, Compare > res;
+    if (std::addressof(first_) == std::addressof(second_)) 
+    {
+      return res;
+    }
+    auto iter_first_ = first_.cbegin();
+    auto iter_second_ = second_.cbegin();
+    Compare comp = Compare{};
+    while (iter_first_ != first_.cend() && iter_second_ != second_.cend()) 
+    {
+      while (iter_second_ != second_.cend() && comp(iter_second_->first, iter_first_->first)) 
+      {
+        iter_second_++;
+      }
+      if (iter_second_ == second_.cend()) 
+      {
+        break;
+      }
+      if (iter_first_->first != iter_second_->first) 
+      {
+        res.insert(*iter_first_);
+      }
+      iter_first_++;
+    }
+    while (iter_first_ != first_.cend()) 
+    {
+      res.insert(*iter_first_);
+      iter_first_++;
+    }
+    return res;
+  }
+
 }
 
 #endif
