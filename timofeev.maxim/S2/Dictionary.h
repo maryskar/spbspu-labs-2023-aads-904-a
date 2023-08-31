7af66714f0602f554195c410c5f225166c176384
Caspio <maks.timofeev9834@gmail.com>
2023-08-31 17:19:06 +0400

    Dictionary cend end begin cbegin

--- timofeev.maxim/S2/Dictionary.h
+++ timofeev.maxim/S2/Dictionary.h
@@ -9,7 +9,6 @@ namespace timofeev
   public:
     using iter = ForwardListIterator< std::pair< Key, Value > >;
     using constIter = ForwardListConstIterator< std::pair< Key, Value > >;
-
     void push(Key k, Value v);
     Value get(Key k);
     Value drop(Key k);
@@ -17,6 +16,19 @@ namespace timofeev
     Dictionary();
     ~Dictionary() = default;
 
+    //at
+    // operator[]
+    iter begin() noexcept;
+    constIter cbegin() const noexcept;
+
+    iter end() noexcept;
+    constIter cend() const noexcept;
+    //insert
+    //erase
+    //swap
+    //count
+    //find
+    //contains
     bool empty() const noexcept;
     size_t size() const noexcept;
 
@@ -32,10 +44,52 @@ namespace timofeev
   Dictionary< Key, Value, Compare >::Dictionary():
     data_(),
     compare_(),
-    size_()
+    size_(0)
   {}
 
   template< typename Key, typename Value, typename Compare >
+  bool  Dictionary< Key, Value, Compare >::empty() const noexcept
+  {
+    return data_.empty();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  size_t Dictionary< Key, Value, Compare >::size() const noexcept
+  {
+    return size_;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  void Dictionary< Key, Value, Compare >::clear()
+  {
+    data_.clear();
+    size_ = 0;
+  }
+
+ /* iter begin() noexcept;
+  constIter cbegin() const noexcept;
+  iter end() noexcept;
+  constIter cend() const noexcept;*/
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::begin() noexcept
+  {
+    return data_.begin();
+  }
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::cbegin() const noexcept
+  {
+    return data_.cbegin();
+  }
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::cend() const noexcept
+  {
+    return data_.cend();
+  }
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::end() noexcept
+  {
+    return data_.end();
+  }
 
 
 }
