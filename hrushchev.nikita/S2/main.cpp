8af96f19affbeda6cbff382d70fb15d0cb32b4e9
Shuzamada <shuzamada@inbox.ru>
2023-09-03 22:41:43 +0300

    fix throw again

--- hrushchev.nikita/S2/main.cpp
+++ hrushchev.nikita/S2/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
         }
         else
         {
-          throw std::logic_error("invalig arg");
+          throw std::logic_error("invalig command");
         }
       }
     }
