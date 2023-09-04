9a771cbb276513c3d7983ef6c923fffe364fb576
KiraGelaz <kiragelaz@yandex.ru>
2023-09-04 10:36:33 +0300

    read file done

--- kryuchkova.kira/S2/read_dict.cpp
+++ kryuchkova.kira/S2/read_dict.cpp
@@ -3,5 +3,42 @@
 
 namespace kryuchkova
 {
-
+  Dictionary< std::string, Dictionary< int, std::string, std::less< > >, std::less< > > readDictOfDict(std::istream & in)
+  {
+    using dictionary = Dictionary< int, std::string, std::less< > >;
+    using dict_of_dict = Dictionary< std::string, dictionary, std::less< std::string > >;
+    using dictionary_elem = std::pair< int, std::string >;
+    using dict_of_dict_elem = std::pair< std::string, dictionary >;
+    dict_of_dict container;
+    try
+    {
+      while (in)
+      {
+        std::string name;
+        in >> name;
+        if (!in)
+        {
+          break;
+        }
+        dictionary dict;
+        int key = 0;
+        std::string val;
+        while (in)
+        {
+          in >> key >> val;
+          if (!in)
+          {
+            break;
+          }
+          dict.insert(dictionary_elem(key, val));
+        }
+        in.clear();
+        container.insert(dict_of_dict_elem(name, dict));
+        }
+    }
+    catch(const std::exception & e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+  }
 }
