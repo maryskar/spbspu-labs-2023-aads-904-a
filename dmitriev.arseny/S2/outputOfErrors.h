773b070273f5c0d1ec1a617c3f986a23830dbcb7
ArsDi <abstractlevl@gmail.com>
2023-09-06 09:38:53 +0300

    defenition

--- dmitriev.arseny/S2/outputOfErrors.h
+++ dmitriev.arseny/S2/outputOfErrors.h
@@ -1,1 +1,12 @@
-#pragma once
+#ifndef OUTPUTOFERRORS_H
+#define OUTPUTOFERRORS_H
+
+#include <iostream>
+
+namespace dmitriev
+{
+  void outOfInvalivdComandErr(std::ostream& out);
+  void outOfEmptyDataErr(std::ostream& out);
+}
+
+#endif
