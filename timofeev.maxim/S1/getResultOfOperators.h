7a2b00ff467aa63b372e98090c6135082b702796
Maxim <maks.timofeev9834@gmail.com>
2023-09-07 14:35:54 +0300

    fix

--- timofeev.maxim/S1/getResultOfOperators.h
+++ timofeev.maxim/S1/getResultOfOperators.h
@@ -2,10 +2,10 @@
 #define GETRESULTOFOPERATORS_H
 namespace timofeev
 {
-  long long getSum(ll one, ll two);
-  long long getMinus(ll one, ll two);
-  long long getDiv(ll one, ll two);
-  long long getMultip(ll one, ll two);
-  long long getRemander(ll one, ll two);
+  long long getSum(long long one, long long two);
+  long long getMinus(long long one, long long two);
+  long long getDiv(long long one, long long two);
+  long long getMultip(long long one, long long two);
+  long long getRemander(long long one, long long two);
 }
 #endif
