1b1734d7ba1856946f219cdcc9305cd5f5e64697
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-14 21:35:00 +0300

    fixed count

--- skarlygina.mariia/S1/count.cpp
+++ skarlygina.mariia/S1/count.cpp
@@ -3,24 +3,23 @@
 #include "calculator.h"
 long long switchSymbol(const std::string& operation, long long a, long long b)
 {
-  long long result;
   if (operation == "-")
   {
     return skarlygina::substraction(a, b);
   }
-  if (operation == "+")
+  else if (operation == "+")
   {
     return skarlygina::sum(a, b);
   }
-  if (operation == "*")
+  else if (operation == "*")
   {
     return skarlygina::multiplication(a, b);
   }
-  if (operation == "/")
+  else if (operation == "/")
   {
     return skarlygina::division(a, b);
   }
-  if (operation == "%")
+  else if (operation == "%")
   {
     return skarlygina::remainder(a, b);
   }
