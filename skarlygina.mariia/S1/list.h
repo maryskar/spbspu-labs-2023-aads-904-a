779a8cacfa367e3578f21d5a7b525555faaff1c7
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-14 19:54:00 +0300

    clear the list

--- skarlygina.mariia/S1/list.h
+++ skarlygina.mariia/S1/list.h
@@ -6,4 +6,15 @@ struct List
   T data;
   List< T >* next;
 };
+
+template< typename T >
+void clear(List< T >* first)
+{
+  while (first)
+  {
+    auto temp = first;
+    first = first->next;
+    delete temp;
+  }
+}
 #endif
