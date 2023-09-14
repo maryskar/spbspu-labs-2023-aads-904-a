0f78dec8610c3cba44320542b787a4c421ff32b0
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-14 13:16:00 +0300

    fixed stack

--- skarlygina.mariia/S1/stack.h
+++ skarlygina.mariia/S1/stack.h
@@ -11,98 +11,83 @@ public:
   Stack(const Stack< T >& other);
   Stack(const Stack< T >&& other);
   void push(const T& rhs);
-  T& get() const;
-  T& top() const;
+  const T& top() const;
   void pop();
+  const T& deep() const;
   bool isEmpty() const;
   ~Stack();
 private:
-  List< T >* head_;
-  List< T >* tail_;
+  List< T >* root_;
 };
 
 template< typename T >
 Stack< T >::Stack() :
-  head_(nullptr),
-  tail_(nullptr)
+  root_(nullptr)
 {}
 
 template< typename T >
 Stack< T >::Stack(const Stack< T >& other):
-  head_(other.head_),
-  tail_(other.tail_)
+  root_(other.root_)
 {}
 
 template< typename T >
 Stack< T >::Stack(const Stack< T >&& other) :
-  head_(other.head_),
-  tail_(other.tail_)
+  root_(other.root_)
 {
-  other.head_ = nullptr;
-  other.tail_ = nullptr;
+  other.root_ = nullptr;
 }
 
 template <class T>
-T& Stack< T >::top() const
+const T& Stack< T >::top() const
 {
   if (isEmpty())
   {
     throw std::runtime_error("Stack is empty");
   }
-  return head_->data;
+  return root_->data;
 }
 
 template< typename T>
 void Stack< T >::push(const T& rhs)
 {
-  List< T >* new_node = new List< T >{rhs, nullptr, nullptr};
-  if (head_ == nullptr)
-  {
-    head_ = new_node;
-    tail_ = new_node;
-  }
-  else
-  {
-    new_node->prev = tail_;
-    tail_->next = new_node;
-    tail_ = new_node;
-  }
-}
-
-template< typename T >
-T& Stack< T >::get() const
-{
-  if (tail_ == nullptr)
-  {
-    throw std::out_of_range("Stack is empty");
-  }
-  return tail_->data;
+  List< T >* new_node = new List< T >{rhs, nullptr};
+  new_node->data = rhs;
+  new_node->next = root_;
+  root_ = new_node;
 }
 
 template< typename T >
 void Stack< T >::pop()
 {
-  if (tail_ == nullptr)
+  if (isEmpty())
   {
     throw std::out_of_range("Stack is empty");
   }
-  List< T >* node_temp = tail_;
-  tail_ = tail_->prev;
-  if (tail_ != nullptr)
-  {
-    tail_->next = nullptr;
-  }
-  else
-  {
-    head_ = nullptr;
-  }
+  List< T >* node_temp = root_;
+  root_ = node_temp->next;
   delete node_temp;
+}
+
+template< typename T >
+const T& Stack< T >::deep() const
+{
+  if (isEmpty())
+  {
+    throw std::logic_error("Empty");
+  }
+  List< T >* current = this->root_;
+  while (current->next != nullptr)
+  {
+    current = current->next;
+  }
+  List< T >* deep = current;
+  return deep->data;
 }
 
 template< typename T >
 bool Stack< T >::isEmpty() const
 {
-  return head_ == nullptr;
+  return root_ == nullptr;
 }
 
 template< typename T>
