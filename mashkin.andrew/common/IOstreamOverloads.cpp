bda4ba8fb79c23919ce9345a09bcd79846b60cc5
beesaiboo <beedzibood@gmail.com>
2023-09-17 19:37:00 +0300

    improved IOstreamOverloads

--- mashkin.andrew/common/IOstreamOverloads.cpp
+++ mashkin.andrew/common/IOstreamOverloads.cpp
@@ -6,13 +6,9 @@
 
 namespace mashkin
 {
-  std::istream& operator>>(std::istream& inp, dictionary& dict)
+  template< class T >
+  std::istream& insertPairInDict(std::istream& inp, T& dict)
   {
-    std::istream::sentry sentry(inp);
-    if (!sentry)
-    {
-      return inp;
-    }
     int key = 0;
     std::string value;
     inp >> key;
@@ -29,21 +25,17 @@ namespace mashkin
     return inp;
   }
 
-  std::istream& operator>>(std::istream& inp, dictionaries& dicts)
+  template< class T >
+  std::istream& insertDictInDictionaries(std::istream& inp, T& dicts)
   {
-    std::istream::sentry sentry(inp);
-    if (!sentry)
-    {
-      return inp;
-    }
     std::string key;
     inp >> key;
     if (!inp)
     {
       return inp;
     }
-    dictionary value;
-    using inpIter = std::istream_iterator< dictionary >;
+    decltype(dicts.begin()->second) value;
+    using inpIter = std::istream_iterator< decltype(value) >;
     auto i = inpIter(inp);
     while (!inp.eof() && i != inpIter())
     {
@@ -54,17 +46,73 @@ namespace mashkin
     return inp;
   }
 
-  std::ostream& operator<<(std::ostream& out, const dictionary& dicts)
+  template< class T >
+  std::ostream& printDictionaries(std::ostream& out, const T& dicts)
+  {
+    for (auto it = dicts.begin(); it != dicts.end(); it++)
+    {
+      out << " " << it->first << " " << it->second;
+    }
+    return out;
+  }
+
+  std::istream& operator>>(std::istream& inp, AVL< int, std::string >& dict)
+  {
+    std::istream::sentry sentry(inp);
+    if (!sentry)
+    {
+      return inp;
+    }
+    return insertPairInDict(inp, dict);
+  }
+
+  std::istream& operator>>(std::istream& inp, AVL< std::string, AVL< int, std::string > >& dicts)
+  {
+    std::istream::sentry sentry(inp);
+    if (!sentry)
+    {
+      return inp;
+    }
+    return insertDictInDictionaries(inp, dicts);
+  }
+
+  std::ostream& operator<<(std::ostream& out, const AVL< int, std::string >& dicts)
   {
     std::ostream::sentry sentry(out);
     if (!sentry)
     {
       return out;
     }
-    for (auto it = dicts.begin(); it != dicts.end(); it++)
+    return printDictionaries(out, dicts);
+  }
+
+  std::istream& operator>>(std::istream& inp, Dictionary< int, std::string >& dict)
+  {
+    std::istream::sentry sentry(inp);
+    if (!sentry)
     {
-      out << " " << it->first << " " << it->second;
+      return inp;
     }
-    return out;
+    return insertPairInDict(inp, dict);
+  }
+
+  std::istream& operator>>(std::istream& inp, Dictionary< std::string, Dictionary< int, std::string > >& dicts)
+  {
+    std::istream::sentry sentry(inp);
+    if (!sentry)
+    {
+      return inp;
+    }
+    return insertDictInDictionaries(inp, dicts);
+  }
+
+  std::ostream& operator<<(std::ostream& out, const Dictionary< int, std::string >& dicts)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    return printDictionaries(out, dicts);
   }
 }
