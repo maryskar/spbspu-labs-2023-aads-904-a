dbd6a5cece96033b17ccb305445737be9fe9aa8f
Потапова <marina-potapova04@mail.ru>
2023-09-20 16:04:10 +0300

    add another input of file

--- potapova.marina/S2/main.cpp
+++ potapova.marina/S2/main.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
+#include <stdexcept>
 #include <fstream>
 #include "forwardList.h"
 #include "dictionary.h"
 
-int main()
+using namespace potapova;
+
+int main(int argc, char* argv[])
 {
+  std::istream* in_ptr = nullptr;
+  std::ifstream input_file;
+  if (argc > 2)
+  {
+    std::cerr << "Incorrect number of arguments\n";
+    return 1;
+  }
+  else if (argc == 2)
+  {
+    input_file.open(argv[1]);
+    if (!input_file.is_open())
+    {
+      std::cerr << "Failed to open file\n";
+      return 1;
+    }
+    in_ptr = &input_file;
+  }
+  else
+  {
+    std::cerr << "No file to open\n";
+    return 1;
+  }
+
   return 0;
 }
