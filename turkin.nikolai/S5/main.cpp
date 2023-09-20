dbcdd8fc8244ec89c8ebf8abd446b56c7d045cf7
Turkin Nikolai <nikolai.turkin.main@gmail.com>
2023-09-20 19:45:01 +0300

    changed traverse list

--- turkin.nikolai/S5/main.cpp
+++ turkin.nikolai/S5/main.cpp
@@ -6,6 +6,7 @@
 #include <file-work.hpp>
 
 #include "func-objects.hpp"
+#include "traverse-list.hpp"
 
 int main(int argc, char * argv[])
 {
@@ -31,32 +32,14 @@ int main(int argc, char * argv[])
     return 0;
   }
 
+  auto key_list = turkin::make_trvs< turkin::KeySum >();
+  auto str_list = turkin::make_trvs< turkin::StrSum >();
   turkin::KeySum key;
-  auto lnr_key = dict.traverse_lnr(key);
-  auto rnl_key = dict.traverse_rnl(key);
-  auto brh_key = dict.traverse_breadth(key);
-  turkin::AVLtree< std::string, decltype(lnr_key), std::less< > > traverse_key;
-  traverse_key.insert("ascending", lnr_key);
-  traverse_key.insert("descending", rnl_key);
-  traverse_key.insert("breadth", brh_key);
-
   turkin::StrSum str;
-  auto lnr_str = dict.traverse_lnr(str);
-  auto rnl_str = dict.traverse_rnl(str);
-  auto brh_str = dict.traverse_breadth(str);
-  turkin::AVLtree< std::string, decltype(lnr_str), std::less< > > traverse_str;
-  traverse_str.insert("ascending", lnr_str);
-  traverse_str.insert("descending", rnl_str);
-  traverse_str.insert("breadth", brh_str);
 
   try
   {
-    auto key_result = key.get();
-    auto str_result = str.get();
-    std::string type = argv[1];
-    key_result = traverse_key.at(type).get();
-    str_result = traverse_str.at(type).get();
-    std::cout << key_result << str_result << "\n";
+    std::cout << (dict.*key_list.at(argv[1]))(key).get() << (dict.*str_list.at(argv[1]))(str).get() << "\n";
   }
   catch (...)
   {
