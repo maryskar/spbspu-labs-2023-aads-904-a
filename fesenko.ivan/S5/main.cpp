8551520f74e1050e840f7cfdb39ff7b6c3edf7af
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-20 20:07:49 +0300

    Add check of input

--- fesenko.ivan/S5/main.cpp
+++ fesenko.ivan/S5/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
-
+  if (argc != 3) {
+    std::cerr << "Invalid number of arguments\n";
+    return 2;
+  }
+  std::ifstream in(argv[2]);
+  if (!in.is_open()) {
+    std::cerr << "Can`t open the file\n";
+    return 2;
+  }
 }
