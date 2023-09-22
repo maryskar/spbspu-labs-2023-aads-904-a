3e5bc808279bf7f46b76beb5aad3044730d01edb
Turkin Nikolai <nikolai.turkin.main@gmail.com>
2023-09-22 18:30:48 +0300

    added qsort

--- turkin.nikolai/S6/main.cpp
+++ turkin.nikolai/S6/main.cpp
@@ -1,8 +1,12 @@
+#include <deque>
 #include <iostream>
 #include <string>
 #include <cstddef>
 
+#include "qsort.hpp"
+
 #include <out-msg.hpp>
+#include <vector>
 
 int main(int argc, char * argv[])
 {
@@ -14,15 +18,9 @@ int main(int argc, char * argv[])
   
   std::string trvs_t = argv[2];
   std::string data_t = argv[3];
-  std::size_t amount = std::stoull(argv[4]);
-  
-  try
-  {
-    
-  }
-  catch (...)
-  {
-    turkin::outInvalidCMD(std::cerr);
-  }
-  std::cout << "hi\n";
+  //std::size_t amount = std::stoull(argv[4]);
+  std::deque< int > data = {2, 1, 235, 1234, 74, 2, 324};
+  turkin::quickSort<  >(data.begin(), data.end(), std::less< int >());
+  for (auto it: data) std::cout << it << " ";
+  std::cout << "\n";
 }
