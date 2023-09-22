f51ddf565e1c3cd354c520020d928a1673f5a081
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-09-23 00:08:49 +0300

    add print in commands.cpp

--- chulkov.leontii/S2/commands.cpp
+++ chulkov.leontii/S2/commands.cpp
@@ -10,4 +10,21 @@ namespace chulkov {
     return out << "<EMPTY>" << std::endl;
   }
 
+  std::ostream& print(std::ostream& out, const std::string& name, const Dicts& dicts)
+  {
+    auto it = dicts.cfind(name);
+    if (it == dicts.cend()) {
+      throw std::invalid_argument(MESSAGE);
+    }
+    if (it->second.isEmpty()) {
+      printEmptyCommand(out);
+      return out;
+    }
+    out << name;
+    for (std::pair< int, std::string >& data : it->second) {
+      out << ' ' << data.first << ' ' << data.second;
+    }
+    out << std::endl;
+    return out;
+  }
 }
