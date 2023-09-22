5a6b52a117457eef1661760fdcd3358b2d899e5a
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-09-23 02:52:51 +0300

    add getdicts in  getDicts.cpp

--- chulkov.leontii/S2/getDicts.cpp
+++ chulkov.leontii/S2/getDicts.cpp
@@ -5,5 +5,20 @@
 #include "Dictionary.h"
 
 namespace chulkov {
+  Dictionary< int, std::string > getDict(const FrdList< std::string >& in)
+  {
+    Dictionary< int, std::string > returnable;
+    int key = 0;
+    std::string string;
+    for (ForwardConstIter< std::string > i = (++in.cbegin()); i != in.cend(); ++i) {
+      if (std::isdigit(i->front())) {
+        key = std::stoi(*i);
+        continue;
+      }
+      string = *i;
+      returnable.push(key, string);
+    }
+    return returnable;
+  }
 
 }
