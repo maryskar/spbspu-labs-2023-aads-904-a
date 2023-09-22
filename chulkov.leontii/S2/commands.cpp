0c1419461b41a19e9d029ee15664c29defde10ec
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-09-23 02:48:00 +0300

    add complement in commands.cpp

--- chulkov.leontii/S2/commands.cpp
+++ chulkov.leontii/S2/commands.cpp
@@ -27,4 +27,43 @@ namespace chulkov {
     out << std::endl;
     return out;
   }
+
+  Dictionary< int, std::string > complement(const std::string& lhs, const std::string& rhs, const Dicts& dicts)
+  {
+    auto lhsCIterator = dicts.cfind(lhs);
+    auto rhsCIterator = dicts.cfind(rhs);
+    if ((lhsCIterator == dicts.cend()) || (rhsCIterator == dicts.cend())) {
+      throw std::invalid_argument(MESSAGE);
+    }
+    if (lhsCIterator->second.isEmpty() || rhsCIterator->second.isEmpty()) {
+      return lhsCIterator->second;
+    }
+    auto returnable = lhsCIterator->second;
+    auto rhsIterator = rhsCIterator->second.cbegin();
+    auto retIterator = returnable.begin();
+    std::less< int > cmp;
+    while ((retIterator != returnable.end()) && (rhsIterator != rhsCIterator->second.cend())) {
+      if (cmp(retIterator->first, rhsIterator->first)) {
+        while ((retIterator != returnable.end()) && cmp(retIterator->first, rhsIterator->first)) {
+          ++retIterator;
+        }
+      } else if (cmp(rhsIterator->first, retIterator->first)) {
+        while ((rhsIterator != rhsCIterator->second.cend()) && cmp(rhsIterator->first, retIterator->first)) {
+          ++rhsIterator;
+        }
+      }
+      if ((retIterator == returnable.end()) || (rhsIterator == rhsCIterator->second.cend())) {
+        return returnable;
+      }
+      if (!cmp(retIterator->first, rhsIterator->first) && !cmp(rhsIterator->first, retIterator->first)) {
+        ++retIterator;
+        returnable.pop(rhsIterator->first);
+        ++rhsIterator;
+      } else {
+        ++retIterator;
+        ++rhsIterator;
+      }
+    }
+    return returnable;
+  }
 }
