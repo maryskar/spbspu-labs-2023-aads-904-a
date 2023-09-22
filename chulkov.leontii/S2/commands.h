2955bfd3dab3f81ba41b922e28d27cc7e7533358
Leontii Chulkov <chulkov.la@edu.spbstu.ru>
2023-09-23 00:05:42 +0300

    add commands.h

---
+++ chulkov.leontii/S2/commands.h
@@ -0,0 +1,16 @@
+#ifndef S2_COMMANDS_H
+#define S2_COMMANDS_H
+#include <iostream>
+#include <string>
+#include "Dictionary.h"
+#include "ForwardList.h"
+
+namespace chulkov {
+  using Dicts = Dictionary< std::string, Dictionary< int, std::string > >;
+  std::ostream& print(std::ostream&, const std::string&, const Dicts&);
+  Dictionary< int, std::string > complement(const std::string&, const std::string&, const Dicts&);
+  Dictionary< int, std::string > intersect(const std::string&, const std::string&, const Dicts&);
+  Dictionary< int, std::string > unite(const std::string&, const std::string&, const Dicts&);
+}
+
+#endif
