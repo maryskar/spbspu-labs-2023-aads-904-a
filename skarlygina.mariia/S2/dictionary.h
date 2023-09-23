b1c4167b6957774979d0b3f186a301fa533def76
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-23 21:04:02 +0300

    namespaces forward and dict

--- skarlygina.mariia/S2/dictionary.h
+++ skarlygina.mariia/S2/dictionary.h
@@ -3,22 +3,24 @@
 #include "iterator.h"
 #include "citerator.h"
 
-template < typename Key, typename Value, typename Compare >
-class Dictionary
+namespace skarlygina
 {
-public:
-  Iterator< std::pair< Key, Value > > begin();
-  Iterator< std::pair< Key, Value > > end();
-  CIterator< std::pair< Key, Value > > begin() const;
-  CIterator< std::pair< Key, Value > > end() const;
-  CIterator< std::pair< Key, Value > > cbegin() const;
-  CIterator< std::pair< Key, Value > > cend() const;
+  template < typename Key, typename Value, typename Compare >
+  class Dictionary
+  {
+  public:
+    Iterator< std::pair< Key, Value > > begin();
+    Iterator< std::pair< Key, Value > > end();
+    CIterator< std::pair< Key, Value > > begin() const;
+    CIterator< std::pair< Key, Value > > end() const;
+    CIterator< std::pair< Key, Value > > cbegin() const;
+    CIterator< std::pair< Key, Value > > cend() const;
 
-  void push(const Key&, const Value&);
-  const Value& get(const Key&) const;
-  void pop(const Key&);
-  void clear();
-  bool is_empty() const noexcept;
-};
-
+    void push(const Key&, const Value&);
+    const Value& get(const Key&) const;
+    void pop(const Key&);
+    void clear();
+    bool is_empty() const noexcept;
+  };
+}
 #endif
--- skarlygina.mariia/S2/forwardlist.h
+++ skarlygina.mariia/S2/forwardlist.h
@@ -1,9 +1,26 @@
 #ifndef FORWARDLIST_H
 #define FORWARDLIST_H
+#include "iterator.h"
+#include "citerator.h"
 
-template < typename T >
-class ForwardList
-{};
+namespace skarlygina
+{
+  template < typename T >
+  class ForwardList
+  {
+    ForwardList();
+    ForwardList(const ForwardList< T >&);
+    ForwardList(ForwardList< T >&&);
+    ~ForwardList();
+
+    Iterator< T > begin();
+    Iterator< T > end();
+    CIterator< T > begin() const;
+    CIterator< T > end() const;
+    CIterator< T > cbegin() const;
+    CIterator< T > cend() const;
+  };
+}
 
 #endif
 
