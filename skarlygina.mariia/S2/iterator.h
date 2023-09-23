f3ec8bf8130a4959ba65c5e1832a165444eabb49
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-23 20:13:34 +0300

    iterator header

---
+++ skarlygina.mariia/S2/iterator.h
@@ -0,0 +1,60 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+#include <iterator>
+#include "citerator.h"
+#include "list.h"
+namespace skarlygina
+{
+  template < typename T >
+  class Iterator : public std::iterator< std::forward_iterator_tag, T >
+  {
+  public:
+    bool operator==(const Iterator&) const;
+    bool operator!=(const Iterator&) const;
+    explicit Iterator(const CIterator< T >&);
+    Iterator& operator++();
+    Iterator operator++(int);
+    T& operator*();
+    T* operator->();
+  private:
+    CIterator< T > iterator_;
+  };
+
+  template < typename T >
+  Iterator< T >::Iterator(const CIterator< T >& other) :
+    iterator_(other)
+  {}
+
+  template < typename T >
+  bool Iterator< T >::operator==(const Iterator< T >& other) const
+  {
+    return iterator_ == other.iterator_;
+  }
+  template < typename T >
+  bool Iterator< T >::operator!=(const Iterator< T >& other) const
+  {
+    return !(*this == other);
+  }
+  template < typename T >
+  Iterator< T >& Iterator< T >::operator++()
+  {
+    ++iterator_;
+    return *this;
+  }
+  template < typename T >
+  Iterator< T > Iterator< T >::operator++(int)
+  {
+    return Iterator< T >(iterator_++);
+  }
+  template < typename T >
+  T& Iterator< T >::operator*()
+  {
+    return const_cast<T&>(*iterator_);
+  }
+  template < typename T >
+  T* Iterator< T >::operator->()
+  {
+    return const_cast<T*>(std::addressof(*iterator_));
+  }
+}
+#endif // !ITERATOR_Ho,
