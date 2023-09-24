5f4ba9559610f36615b03dc4510bfa8e65884244
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-24 16:56:32 +0300

    Add enter after empty message

--- fesenko.ivan/S5/main.cpp
+++ fesenko.ivan/S5/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
     }
     if (tr.empty()) {
       fesenko::outEmptyMessage(std::cout);
+      std::cout << "\n";
     }
   } catch (const std::exception &e) {
     std::cerr << e.what() << "\n";
