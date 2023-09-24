50721214ae12b33b5f50b6035bafa52829bf33d7
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-24 16:19:17 +0300

    Finish main.cpp

--- fesenko.ivan/S5/main.cpp
+++ fesenko.ivan/S5/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <avl/AVL.h>
+#include <messages.h>
+#include "createTreeWithTraverses.h"
+#include "functionalObjects.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +14,36 @@ int main(int argc, char *argv[])
   std::ifstream in(argv[2]);
   if (!in.is_open()) {
     std::cerr << "Can`t open the file\n";
+    return 2;
+  }
+  using tree = fesenko::AVL< int, std::string, std::less< > >;
+  using tree_elem = std::pair< int, std::string >;
+  tree tr;
+  try {
+    tree tr;
+    int key = 0;
+    std::string value = "";
+    while (in) {
+      in >> key >> value;
+      if (!in) {
+        break;
+      }
+      tr.insert(tree_elem(key, value));
+    }
+    if (tr.empty()) {
+      fesenko::outEmptyMessage(std::cout);
+    }
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  try {
+    auto cmd_list = fesenko::createTreeWithTraverses< fesenko::StrSummator >();
+    fesenko::StrSummator strSum;
+    std::cout << (tr.*cmd_list.at("ascending"))(strSum).getSum() << "\n";
+  } catch (...) {
+    fesenko::outInvalidCommandMessage(std::cout);
+    std::cout << "\n";
     return 2;
   }
 }
