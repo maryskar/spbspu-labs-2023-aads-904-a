1eb009403f8bbf6e9e00001fa79d239d5781429d
Мария Скарлыгина <skarlygina04@yandex.ru>
2023-09-25 21:29:28 +0300

    first get words main

--- skarlygina.mariia/S2/main.cpp
+++ skarlygina.mariia/S2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "reading_dicts.h"
 
 namespace skarlygina
 {
@@ -22,5 +23,29 @@ int main(int argc, char* argv[])
     std::cerr << "Could not read the file\n";
     return 1;
   }
-  skarlygina::printInvalidCommand(std::cout);
+
+  auto data = skarlygina::getDictis(ifile);
+  ifile.close();
+  std::istream& in = std::cin;
+
+  while (!in.eof())
+  {
+    try
+    {
+      auto words = skarlygina::getWords(in, '\n');
+      if (words.isEmpty())
+      {
+        continue;
+      }
+      if (words.getSize() < 2)
+      {
+        throw std::invalid_argument("You need more args!\n");
+      }
+      
+    }
+    catch (const std::invalid_argument&)
+    {
+      skarlygina::printInvalidCommand(std::cout);
+    }
+  }
 }
