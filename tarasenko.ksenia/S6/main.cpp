6de02f4059baeb5eea44b64fc9b66694a331afb3
xeniaTa <kseniatarasenko1@gmail.com>
2023-09-27 02:48:53 +0300

    fix return errors

--- tarasenko.ksenia/S6/main.cpp
+++ tarasenko.ksenia/S6/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 #include "sort_commands.h"
 
 int main(int argc, char* argv[])
@@ -15,11 +16,16 @@ int main(int argc, char* argv[])
   try
   {
     auto num_of_val = std::stoll(argv[3]);
+    if (num_of_val < 1)
+    {
+      throw std::invalid_argument("Invalid command");
+    }
     tarasenko::Commands<> commands;
     commands.at(command)(std::cout, num_of_val);
   }
   catch (const std::exception& e)
   {
     std::cout << e.what() << "\n";
+    return 1;
   }
 }
