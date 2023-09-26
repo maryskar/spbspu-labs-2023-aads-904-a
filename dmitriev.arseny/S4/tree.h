348dc11217bc4a527ac835852ab6bc2c02905437
ArsDi <abstractlevl@gmail.com>
2023-09-26 09:12:42 +0300

    copy method for str realiz

--- dmitriev.arseny/S4/tree.h
+++ dmitriev.arseny/S4/tree.h
@@ -40,6 +40,48 @@ namespace dmitriev
 		delete root;
 	}
 
+	template< typename T >
+	Tree< T >* copy(Tree< T >* otherRoot)
+	{
+		if (isEmpty(otherRoot))
+		{
+			return nullptr;
+		}
+
+		Tree< T >* newNode = new Tree< T >{otherRoot->data, otherRoot->height, nullptr, nullptr, nullptr};
+
+		try
+		{
+			newNode->left = copy(otherRoot->left);
+		}
+		catch (const std::exception&)
+		{
+			delete newNode;
+			throw;
+		}
+		if (!isEmpty(newNode->left))
+		{
+			newNode->left->parent = newNode;
+		}
+
+		try
+		{
+			newNode->right = copy(otherRoot->right);
+		}
+		catch (const std::exception&)
+		{
+			clear(newNode->left);
+			delete newNode;
+			throw;
+		}
+		if (!isEmpty(newNode->right))
+		{
+			newNode->right->parent = newNode;
+		}
+
+		return newNode;
+	}
+
 }
 
 #endif
