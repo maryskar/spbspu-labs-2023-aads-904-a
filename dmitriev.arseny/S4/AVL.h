ddc5071503e193966f6e07d8327cb627b4cc7bf0
ArsDi <abstractlevl@gmail.com>
2023-09-26 09:17:37 +0300

    bi iterator basic init

--- dmitriev.arseny/S4/AVL.h
+++ dmitriev.arseny/S4/AVL.h
@@ -6,6 +6,25 @@
 namespace dmitriev
 {
 
+	template< typename Key, typename Value, typename Compare = std::less< Key > >
+	class AVL;
+
+	template< typename Key, typename Value, typename Compare = std::less< Key > >
+	class ConstBidirectionalIterator;
+
+	template< typename Key, typename Value, typename Compare = std::less< Key > >
+	class BidirectionalIterator: public std::iterator< std::bidirectional_iterator_tag, std::pair< Key, Value > >
+	{
+	public:
+		friend class AVL< Key, Value, Compare >;
+		friend class ConstBidirectionalIterator< Key, Value, Compare >;
+
+
+	private:
+		tree* m_ptr;
+
+	}
+
 	template< typename Key, typename Value, typename Compare >
 	class AVL
 	{
