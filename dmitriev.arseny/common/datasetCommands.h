005328a1bcc7022c3d304156b14c345f28b0cd6d
ArsDi <abstractlevl@gmail.com>
2023-09-26 14:21:51 +0300

    commands file rework

--- dmitriev.arseny/common/datasetCommands.h
+++ dmitriev.arseny/common/datasetCommands.h
@@ -2,19 +2,44 @@
 #define COMMANDS_H
 
 #include <iostream>
+#include "outputOfSpecialMessages.h"
 #include "dictionary.h"
+#include "AVL.h"
 
 namespace dmitriev
 {
-  void printDataset(const Dictionary< std::string, Dictionary< int, std::string > >& dataSet,
-    std::string name, std::ostream& out);
+  template< typename inputIterator >
+  void printDataset(inputIterator first,
+    inputIterator last,
+    std::string name,
+    std::ostream& out)
+  {
+    out << name;
 
-  void complementDataset(Dictionary< std::string, Dictionary< int, std::string > >& dataSet,
-    std::string name, std::string lhsName, std::string rhsName);
-  void intersectDataset(Dictionary< std::string, Dictionary< int, std::string > >& dataSet,
-    std::string name, std::string lhsName, std::string rhsName);
-  void unionDataset(Dictionary< std::string, Dictionary< int, std::string > >& dataSet,
-    std::string name, std::string lhsName, std::string rhsName);
+    for (; first != last; first++)
+    {
+      out << ' ' << first->first << ' ' << first->second;
+    }
+  }
+
+  template< typename dataset >
+  void complementDataset(dataset& dataSet,
+    std::string name, std::string lhsName, std::string rhsName)
+  {
+    dataSet[name] = complementDictionary(dataSet.at(lhsName), dataSet.at(rhsName));
+  }
+  template< typename dataset >
+  void intersectDataset(dataset& dataSet,
+    std::string name, std::string lhsName, std::string rhsName)
+  {
+    dataSet[name] = intersectDictionary(dataSet.at(lhsName), dataSet.at(rhsName));
+  }
+  template< typename dataset >
+  void unionDataset(dataset& dataSet,
+    std::string name, std::string lhsName, std::string rhsName)
+  {
+    dataSet[name] = unionDictionary(dataSet.at(lhsName), dataSet.at(rhsName));
+  }
 }
 
 #endif
