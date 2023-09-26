1b2ee1d69e524b23a4ff2f5bbaf1d09412327db1
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-27 00:55:52 +0300

    Add function to print containers

---
+++ fesenko.ivan/S6/print.h
@@ -0,0 +1,18 @@
+#ifndef PRINT_H
+#define PRINT_H
+#include <ostream>
+namespace fesenko
+{
+  template< typename ForwardIt >
+  std::ostream &print(ForwardIt first, ForwardIt last std::ostream &out)
+  {
+    for (auto it = first; it != last; it++) {
+      if (it != first) {
+        out << " ";
+      }
+      out << *it;
+    }
+    return out;
+  }
+}
+#endif
