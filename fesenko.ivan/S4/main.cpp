3eb60196da6c25468216dbca1358c6fffbdeeb5f
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-26 23:10:52 +0300

    Use a container with commands

--- fesenko.ivan/S4/main.cpp
+++ fesenko.ivan/S4/main.cpp
@@ -17,47 +17,42 @@ int main(int argc, char *argv[])
     return 2;
   }
   using tree = fesenko::AVL< int, std::string, std::less< > >;
-  using tree_of_tree = fesenko::AVL< std::string, tree, std::less< std::string > >;
+  using tree_of_tree = fesenko::AVL< std::string, tree, std::less< > >;
+  using tree_c = fesenko::AVL< std::string, tree (*)(const tree &, const tree &), std::less< > >;
   tree_of_tree container = fesenko::genDictOfDicts< tree_of_tree, tree >(in);
-  try {
-    while (std::cin) {
-      std::string command;
-      std::cin >> command;
-      if (!std::cin) {
-        break;
-      }
+  tree_c commands;
+  commands.insert(std::make_pair("complement", fesenko::make_complementation< tree >));
+  commands.insert(std::make_pair("intersect", fesenko::make_intersection< tree >));
+  commands.insert(std::make_pair("union", fesenko::make_union< tree >));
+  while (std::cin) {
+    std::string command;
+    std::cin >> command;
+    if (!std::cin) {
+      break;
+    }
+    try {
       if (command == "print") {
         std::string name;
         std::cin >> name;
         fesenko::print(std::make_pair(name, container.at(name)), std::cout);
         std::cout << "\n";
       } else {
+        auto func = commands.at(command);
         std::string newTreeName;
         std::string treeName1;
         std::string treeName2;
         std::cin >> newTreeName >> treeName1 >> treeName2;
         tree tree1 = container.at(treeName1);
         tree tree2 = container.at(treeName2);
-        tree newTree;
-        if (command == "complement") {
-          newTree = make_complementation(tree1, tree2);
-        } else if (command == "intersect") {
-          newTree = make_intersection(tree1, tree2);
-        } else if (command == "union") {
-          newTree = make_union(tree1, tree2);
-        } else {
-          fesenko::outInvalidCommandMessage(std::cout);
-          std::cout << "\n";
-          continue;
-        }
+        tree newTree = func(tree1, tree2);
         if (!container.insert(std::make_pair(newTreeName, newTree)).second) {
           auto it = container.find(newTreeName);
           container.at(it->first) = newTree;
         }
       }
+    } catch (...) {
+      fesenko::outInvalidCommandMessage(std::cout);
+      std::cout << "\n";
     }
-  } catch (...) {
-    fesenko::outInvalidCommandMessage(std::cout);
-    std::cout << "\n";
   }
 }
