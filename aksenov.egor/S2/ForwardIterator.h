55983340e4a93459202c40d6d8be70ad50a3210c
aksenovEA <aksenov200477@gmail.com>
2023-09-27 01:55:52 +0300

    interface for iterator

--- aksenov.egor/S2/ForwardIterator.h
+++ aksenov.egor/S2/ForwardIterator.h
@@ -1,4 +1,29 @@
 #ifndef FORWARDITERATOR_H
 #define FORWARDITERATOR_H
+namespace aksenov
+{
+  template< typename T >
+  class ForwardIterator
+  {
+  public:
+    ForwardIterator();
+    ~ForwardIterator() = default;
+    ForwardIterator(listT< T > *node);
+    ForwardIterator(const ForwardIterator< T > &) = default;
 
+    ForwardIterator< T > &operator=(const ForwardIterator< T > &rhs) = default;
+
+    ForwardIterator< T > &operator++();
+    ForwardIterator< T > &operator++(int);
+
+    T &operator*();
+    T *operator->();
+
+    bool operator!=(const ForwardIterator< T > &rhs) const;
+    bool operator==(const ForwardIterator< T > &rhs) const;
+  private:
+    listT< T > *node_;
+  };
+
+}
 #endif
