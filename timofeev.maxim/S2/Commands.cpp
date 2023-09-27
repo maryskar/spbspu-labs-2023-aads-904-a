6956ddc6deba99c0f17d1f8063f08af59e6c6d30
timofeev <maks.timofeev9834@gmail.com>
2023-09-27 14:53:05 +0300

    command add print

--- timofeev.maxim/S2/Commands.cpp
+++ timofeev.maxim/S2/Commands.cpp
@@ -1,10 +1,38 @@
 #include "Commands.h"
+#include "commandSet.h"
 namespace timofeev
 {
-  using dict = timofeev::Dictionary< size_t, std::string >;
-  using dictOfDicts = timofeev::Dictionary< std::string, dict >;
   void Print(std::istream& in, dictOfDicts& dict, std::ostream& out)
   {
-    
+    try
+    {
+      std::string dictName;
+      in >> dictName;
+      if (dict.contains(dictName))
+      {
+        dictionary dict_t = dict.at(dictName);
+        if (!dict_t.empty())
+        {
+          out << dictName;
+          for (auto & i : dict_t)
+          {
+            out << " " << i.first << " " << i.second;
+          }
+        }
+        else
+        {
+          out << "<EMPTY>" << '\n';
+          return;
+        }
+      }
+      else
+      {
+        return;
+      }
+    }
+    catch (...)
+    {
+      throw;
+    }
   }
 }
