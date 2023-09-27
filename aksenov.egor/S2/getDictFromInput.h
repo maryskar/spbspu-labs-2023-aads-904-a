8f4bfa4366449230148fffb0ded969ad296d4d82
aksenovEA <aksenov200477@gmail.com>
2023-09-27 23:34:05 +0300

    getting dict new wariant

--- aksenov.egor/S2/getDictFromInput.h
+++ aksenov.egor/S2/getDictFromInput.h
@@ -26,10 +26,10 @@ namespace aksenov
         {
           break;
         }
-        data.insert(std::make_pair(key, val));
+        data[key] = val;
       }
       in.clear();
-      res.insert(std::make_pair(name, data));
+      res[name] = data;
     }
     return res;
   }
