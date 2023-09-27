ac54d8369868453c9ec46f3a1c34c6bc69f8a61d
timofeev <maks.timofeev9834@gmail.com>
2023-09-27 03:25:58 +0300

    fix cg

--- timofeev.maxim/S1/getPostfixForm.cpp
+++ timofeev.maxim/S1/getPostfixForm.cpp
@@ -19,62 +19,56 @@ int getPriority(char op)
     throw std::logic_error("No opperand in string");
   }
 }
-namespace timofeev
+Queue< std::string > timofeev::getPostfixForm(std::string infix)
 {
-  Queue< std::string > getPostfixForm(std::string infix)
+  Queue<std::string> postfix;
+  Stack<char> opStack;
+  timofeev::Splitter split(infix);
+  while (!split.empty())
   {
-    Queue< std::string > postfix;
-    Stack< char > opStack;
-    timofeev::Splitter split(infix);
-    while (!split.empty())
+    const std::string &c = split.getSplit();
+    try
     {
-      const std::string &c = split.getSplit();
-      try
+      std::stoll(c);
+      postfix.push(c);
+    }
+    catch (const std::invalid_argument &)
+    {
+      if (c == "(")
       {
-        std::stoll(c);
-        postfix.push(c);
-      }
-      catch (const std::invalid_argument &)
+        opStack.push(c[0]);
+      } else if (c == ")")
       {
-        if (c == "(")
+        while (!opStack.isEmpty() && opStack.get() != '(')
         {
-          opStack.push(c[0]);
-        }
-        else if (c == ")")
-        {
-
-          while (!opStack.isEmpty() && opStack.get() != '(')
-          {
-            postfix.push(std::string(1, opStack.get()));
-            opStack.pop();
-          }
+          postfix.push(std::string(1, opStack.get()));
           opStack.pop();
         }
-        else
+        opStack.pop();
+      } else
+      {
+        if (!opStack.isEmpty())
         {
-          if (!opStack.isEmpty())
+          char p = opStack.get();
+          while (p != '(' && getPriority(p) >= getPriority(c[0]))
           {
-            char p = opStack.get();
-            while (p != '(' && getPriority(p) >= getPriority(c[0]))
+            postfix.push(std::string(1, p));
+            opStack.pop();
+            if (opStack.isEmpty())
             {
-              postfix.push(std::string(1, p));
-              opStack.pop();
-              if (opStack.isEmpty())
-              {
-                break;
-              }
-              p = opStack.get();
+              break;
             }
+            p = opStack.get();
           }
-          opStack.push(c[0]);
         }
+        opStack.push(c[0]);
       }
     }
-    while (!opStack.isEmpty())
-    {
-      postfix.push(std::string(1, opStack.get()));
-      opStack.pop();
-    }
-    return postfix;
   }
+  while (!opStack.isEmpty())
+  {
+    postfix.push(std::string(1, opStack.get()));
+    opStack.pop();
+  }
+  return postfix;
 }
