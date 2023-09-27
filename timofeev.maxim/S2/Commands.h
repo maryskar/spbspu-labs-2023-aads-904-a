c8ab52abbcd4186b3b630de41c7d70bb91e49ac4
timofeev <maks.timofeev9834@gmail.com>
2023-09-27 14:21:02 +0300

    Commands.h changes

--- timofeev.maxim/S2/Commands.h
+++ timofeev.maxim/S2/Commands.h
@@ -2,14 +2,14 @@
 #define COMMANDS_H
 #include "commandSet.h"
 #include "Dictionary.h"
+#include "iostream"
 namespace timofeev
 {
   using dictionary = timofeev::Dictionary< size_t, std::string >;
   using dictOfDicts = timofeev::Dictionary< std::string, dictionary >;
-  void Print(std::istream& in, dictOfDicts& dict, std::ostream& out);
   void Complement(std::istream& in, dictOfDicts& dict);
   void Intersect(std::istream& in, dictOfDicts& dict);
-  void Union(std::istream& in, dictOfDicts& dict;
+  void Union(std::istream& in, dictOfDicts& dict);
 }
 
-#endif //S2_COMMANDS_H
+#endif
