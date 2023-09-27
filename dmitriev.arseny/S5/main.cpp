7d9acdec160a4061e1880e5b57aea22de44be98f
ArsDi <abstractlevl@gmail.com>
2023-09-27 15:19:43 +0300

    main first try realiz

--- dmitriev.arseny/S5/main.cpp
+++ dmitriev.arseny/S5/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <AVL.h>
-#include <outputOfSpecialMessages.h>
-#include "stringCreator.h"
+#include "AVL.h"
 #include "traverseCommands.h"
+#include "stringCreator.h"
+#include <string>
 
 
 int main(int argc, char** argv)
 {
   using dictionary = dmitriev::AVL< const long long, std::string >;
-  using comands = dmitriev::AVL< std::string, dmitriev::StringCreator(*)(dictionary&, dmitriev::StringCreator) >;
+  using command = dmitriev::StringCreator(*)(const dictionary&, dmitriev::StringCreator);
+
 
   if (argc != 3)
   {
     std::cout << "incorrect argc count" << '\n';
     return 1;
   }
-  std::ifstream file (argv[2]);
+  std::ifstream file(argv[2]);
   if (!file.is_open())
   {
     std::cout << "problems with opening file" << '\n';
@@ -25,28 +25,113 @@ int main(int argc, char** argv)
   }
 
 
-  size_t key;
+  int key;
   std::string value;
   dictionary dict;
 
   while (file >> key >> value)
   {
     dict.insert({key, value});
-  }
 
+  }
   if (dict.isEmpty())
   {
-    dmitriev::outOfEmptyDataMsg() << '\n';
+    std::cout << "<EMPTY>" << '\n';
     return 0;
   }
 
-  comands cmds;
+  dmitriev::AVL< std::string, command > cmds;
   cmds["ascending"] = dmitriev::doAscending;
   cmds["descending"] = dmitriev::doDescending;
   cmds["breadth"] = dmitriev::doBreadth;
 
+  try
+  {
+    if (!cmds.isEmpty(cmds.find(argv[1])))
+    {
+      std::cout << cmds[argv[1]](dict, dmitriev::StringCreator()).result;
+      std::cout << "\n";
+    }
+    else
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+      return 1;
+    }
+  }
+  catch (const std::exception& exception)
+  {
+    std::cerr << exception.what() << "\n";
+    return 1;
+  }
 
 
+  return 0;
+}
+#include <iostream>
+#include <fstream>
+#include "AVL.h"
+#include "traverseCommands.h"
+#include "stringCreator.h"
+#include <string>
+
+
+int main(int argc, char** argv)
+{
+  using dictionary = dmitriev::AVL< const long long, std::string >;
+  using command = dmitriev::StringCreator(*)(const dictionary&, dmitriev::StringCreator);
+
+
+  if (argc != 3)
+  {
+    std::cout << "incorrect argc count" << '\n';
+    return 1;
+  }
+  std::ifstream file(argv[2]);
+  if (!file.is_open())
+  {
+    std::cout << "problems with opening file" << '\n';
+    return 1;
+  }
+
+
+  int key;
+  std::string value;
+  dictionary dict;
+
+  while (file >> key >> value)
+  {
+    dict.insert({key, value});
+
+  }
+  if (dict.isEmpty())
+  {
+    std::cout << "<EMPTY>" << '\n';
+    return 0;
+  }
+
+  dmitriev::AVL< std::string, command > cmds;
+  cmds["ascending"] = dmitriev::doAscending;
+  cmds["descending"] = dmitriev::doDescending;
+  cmds["breadth"] = dmitriev::doBreadth;
+
+  try
+  {
+    if (!cmds.isEmpty(cmds.find(argv[1])))
+    {
+      std::cout << cmds[argv[1]](dict, dmitriev::StringCreator()).result;
+      std::cout << "\n";
+    }
+    else
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+      return 1;
+    }
+  }
+  catch (const std::exception& exception)
+  {
+    std::cerr << exception.what() << "\n";
+    return 1;
+  }
 
 
   return 0;
