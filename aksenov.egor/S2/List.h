b11a3a4bd468049979eea88778ca95356f9aaa71
aksenovEA <aksenov200477@gmail.com>
2023-09-27 22:03:47 +0300

    list functions

--- aksenov.egor/S2/List.h
+++ aksenov.egor/S2/List.h
@@ -1,5 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
+#include<utility>
 namespace aksenov
 {
   template< typename T >
@@ -10,13 +11,27 @@ namespace aksenov
   };
 
   template< typename T >
+  void deleteList(listT< T > *head)
+  {
+    while (head)
+    {
+      auto todel = head;
+      head = head->next;
+      delete todel;
+    }
+  }
+
+
+  template< typename T >
   std::pair< listT< T > *, listT< T > * > copyList(const listT< T > *head)
   {
-    if (head == nullptr) {
+    if (head == nullptr)
+    {
       return {nullptr, nullptr};
     }
     listT< T > *newHead = nullptr;
-    try {
+    try
+    {
       newHead = new listT< T > {head->data, nullptr};
       head = head->next;
       auto *cur = newHead;
@@ -26,7 +41,8 @@ namespace aksenov
         head = head->next;
       }
       return {newHead, cur};
-    } catch (...) {
+    } catch (...)
+    {
       deleteList(newHead);
       throw;
     }
