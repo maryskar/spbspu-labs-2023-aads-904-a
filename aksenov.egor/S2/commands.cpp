e8580492ace50b91528cdbf0a4ecbc4370d13727
aksenovEA <aksenov200477@gmail.com>
2023-09-27 03:35:57 +0300

    intersect

--- aksenov.egor/S2/commands.cpp
+++ aksenov.egor/S2/commands.cpp
@@ -40,4 +40,24 @@ namespace aksenov
     dictionaries[newdataset] = result_dict;
   }
 
+  void intersect(const std::string& newdataset, const std::string& dataset1, const std::string& dataset2) {
+    if (dictionaries.find(dataset1) == dictionaries.end() || dictionaries.find(dataset2) == dictionaries.end()) {
+      std::cout << "<INVALID COMMAND>" << "\n";
+      return;
+    }
+
+    const dictionary& dict1 = dictionaries[dataset1];
+    const dictionary& dict2 = dictionaries[dataset2];
+
+    dictionary result_dict;
+
+    for (const auto& pair : dict1) {
+      if (dict2.find(pair.first) != dict2.end()) {
+        result_dict[pair.first] = pair.second;
+      }
+    }
+
+    dictionaries[newdataset] = result_dict;
+  }
+
 }
