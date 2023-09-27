67e7d6dfc9e78c132651f30c5d854b430dd0ccf0
ArsDi <abstractlevl@gmail.com>
2023-09-27 04:45:55 +0300

    main file work init

--- dmitriev.arseny/S5/main.cpp
+++ dmitriev.arseny/S5/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <AVL.h>
+
 
 int main()
 {
+  if (argc != 3)
+  {
+    std::cout << "incorrect argc count" << '\n';
+    return 1;
+  }
+  std::ifstream file (argv[2]);
+  if (!file.is_open())
+  {
+    std::cout << "problems with opening file" << '\n';
+    return 1;
+  }
+
+
+
+
 
 
   return 0;
