e4cb6452b54e38d6a9e0daf7071edcd1b93cde44
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-09-27 21:36:52 +0300

    Change fillRandom

--- fesenko.ivan/S6/fillRandom.h
+++ fesenko.ivan/S6/fillRandom.h
@@ -4,12 +4,13 @@
 #include <dictionary/forward-list/forwardList.h>
 namespace fesenko
 {
-  template< typename T >
-  void fillRandom(ForwardList< T > &container, size_t size)
+  template< typename Storage >
+  Storage fillRandom(size_t size)
   {
     std::random_device rd;
     std::mt19937 gen(rd());
-    if (std::is_same< typename ForwardList< T >::value_type, int >::value) {
+    Storage container;
+    if (std::is_same< typename Storage::value_type, int >::value) {
       std::uniform_int_distribution< int > dis(0, 100);
       for (size_t i = 0; i < size; i++) {
         container.push_back(dis(gen));
@@ -20,6 +21,7 @@ namespace fesenko
         container.push_back(dis(gen));
       }
     }
+    return container;
   }
 }
 #endif
