9a65c1e23c7205c76ac2786ca539562d2f3c40f2
timofeev <maks.timofeev9834@gmail.com>
2023-09-27 14:53:38 +0300

    command add Complement

--- timofeev.maxim/S2/Commands.cpp
+++ timofeev.maxim/S2/Commands.cpp
@@ -35,4 +35,53 @@ namespace timofeev
       throw;
     }
   }
+  void Complement(std::istream& in, dictOfDicts& dict)
+  {
+    try
+    {
+      std::string newDict, first, second;
+      in >> newDict >> first >> second;
+      if (!dict.contains(second) || !dict.contains(first))
+      {
+        throw std::logic_error("Logic error");
+      }
+      dictionary complDict;
+      const auto &firstDict = dict[first];
+      const auto &secondDict = dict[second];
+      for (const auto &entry: firstDict)
+      {
+        bool firstFlag = false;
+        for (const auto &tmp: secondDict)
+        {
+          if (entry.first == tmp.first)
+          {
+            firstFlag = true;
+            break;
+          }
+        }
+        if (!firstFlag)
+        {
+          complDict.insert(entry);
+        }
+      }
+      if (first == newDict)
+      {
+        dict[first].clear();
+        dict[first].insert(complDict.begin(), complDict.end());
+      }
+      else if (second == newDict)
+      {
+        dict[second].clear();
+        dict[second].insert(complDict.begin(), complDict.end());
+      }
+      else
+      {
+        dict.insert({newDict, complDict});
+      }
+    }
+    catch (...)
+    {
+      throw;
+    }
+  }
 }
