2ce1133eeef9d3204fa769e33dd59b1edcb6438e
Потапова <marina-potapova04@mail.ru>
2023-09-30 16:20:31 +0300

    [r] Change sign in compare function

--- potapova.marina/S2/main.cpp
+++ potapova.marina/S2/main.cpp
@@ -3,14 +3,16 @@
 #include <fstream>
 #include "forwardList.h"
 #include "dictionary.h"
+#include "workWithIO.h"
 
 bool compareFunc(const long long& first, const long long& second)
 {
-  return first < second;
+  return first > second;
 }
 
 int main(int argc, char* argv[])
 {
+  using namespace potapova;
   std::istream* in_ptr = nullptr;
   std::ifstream input_file;
   if (argc > 2)
@@ -35,6 +37,20 @@ int main(int argc, char* argv[])
   }
  
   bool compareFunc(const long long& first, const long long& second);
+
+  Dictionary< long long, std::string, compareFunc > dict1;
+  Dictionary< long long, std::string, compareFunc > dict2;
+  std::string first, second;
+
+  inputDict< compareFunc >(*in_ptr, dict1, first);
+  inputDict< compareFunc >(*in_ptr, dict2, second);
+
+  std::cout << dict1;
+  std::cout << dict2;
+
+  dict1.complement(dict2);
+
+  std::cout << dict1;
 
   return 0;
 }
