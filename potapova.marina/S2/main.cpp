94f9a082e100393d2074ac3267ae88c8f14f33aa
Потапова <marina-potapova04@mail.ru>
2023-09-30 20:28:51 +0300

    [f] Fix input

--- potapova.marina/S2/main.cpp
+++ potapova.marina/S2/main.cpp
@@ -10,32 +10,30 @@ bool compareFunc(const long long& first, const long long& second)
   return first > second;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   using namespace potapova;
-  const char* argv[]{"", "C:/Users/79213/Desktop/Marina/UniversitySPBSPU/aip_labs/spbspu-labs-2023-aads-904-a/potapova.marina/S2/in.txt"};
   std::istream* in_ptr = nullptr;
   std::ifstream input_file;
-  input_file.open(argv[1]);
-  if (!input_file.is_open())
+  if (argc > 2)
   {
-    std::cerr << "Failed to open file\n";
+    std::cerr << "Incorrect number of arguments\n";
     return 1;
   }
-  in_ptr = &input_file;
- 
-  bool compareFunc(const long long& first, const long long& second);
-
-  Dictionary< long long, std::string, compareFunc > dict1;
-  Dictionary< long long, std::string, compareFunc > dict2;
-  std::string first, second;
-
-  inputDict< compareFunc >(*in_ptr, dict1, first);
-  inputDict< compareFunc >(*in_ptr, dict2, second);
-
-
-
-  std::cout << dict1.join(dict2) << '\n';
-
+  else if (argc == 2)
+  {
+    input_file.open(argv[1]);
+    if (!input_file.is_open())
+    {
+      std::cerr << "Failed to open file\n";
+      return 1;
+    }
+    in_ptr = &input_file;
+  }
+  else
+  {
+    std::cerr << "No file to open\n";
+    return 1;
+  }
   return 0;
 }
