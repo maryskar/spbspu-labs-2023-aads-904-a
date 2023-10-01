03834bb6a0c8010b90d759aa3e841c46b0462865
Потапова <marina-potapova04@mail.ru>
2023-10-01 20:17:46 +0300

    [+] Share workWithIO on two files

---
+++ potapova.marina/S2/workWithIO.cpp
@@ -0,0 +1,25 @@
+#include "workWithIO.h"
+
+namespace potapova
+{
+  const char* parseKey(const char* cur_sym_ptr, long long& key)
+  {
+    char* end_conv_ptr = nullptr;
+    key = std::strtoll(cur_sym_ptr, &end_conv_ptr, 10);
+    if (errno == ERANGE || (*end_conv_ptr != ' ' && *end_conv_ptr != '\0'))
+    {
+      return nullptr;
+    }
+    return end_conv_ptr;
+  }
+
+  const char* parseValue(const char* cur_sym_ptr, std::string& value)
+  {
+    while (!(*cur_sym_ptr == ' ' || *cur_sym_ptr == '\0'))
+    {
+      value.push_back(*cur_sym_ptr);
+      ++cur_sym_ptr;
+    }
+    return cur_sym_ptr;
+  }
+}
--- potapova.marina/S2/workWithIO.h
+++ potapova.marina/S2/workWithIO.h
@@ -8,26 +8,8 @@
 
 namespace potapova
 {
-  const char* parseKey(const char* cur_sym_ptr, long long& key)
-  {
-    char* end_conv_ptr = nullptr;
-    key = std::strtoll(cur_sym_ptr, &end_conv_ptr, 10);
-    if (errno == ERANGE || (*end_conv_ptr != ' ' && *end_conv_ptr != '\0'))
-    {
-      return nullptr;
-    }
-    return end_conv_ptr;
-  }
-
-  const char* parseValue(const char* cur_sym_ptr, std::string& value)
-  {
-    while (!(*cur_sym_ptr == ' ' || *cur_sym_ptr == '\0'))
-    {
-      value.push_back(*cur_sym_ptr);
-      ++cur_sym_ptr;
-    }
-    return cur_sym_ptr;
-  }
+  static const char* parseKey(const char* cur_sym_ptr, long long& key);
+  static const char* parseValue(const char* cur_sym_ptr, std::string& value);
 
   template< bool (*Compare)(const long long&, const long long&) >
   std::istream& operator>>(std::istream& in, Dictionary< long long, std::string, Compare >& dict)
