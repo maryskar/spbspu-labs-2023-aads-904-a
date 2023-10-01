934ca70db2804abf6e7ea730fffdaa43c2008969
Потапова <marina-potapova04@mail.ru>
2023-10-01 19:41:46 +0300

    [+] Add input commands in main

--- potapova.marina/S2/main.cpp
+++ potapova.marina/S2/main.cpp
@@ -4,16 +4,21 @@
 #include "forwardList.h"
 #include "dictionary.h"
 #include "workWithIO.h"
+#include "runCommands.h"
 
 bool compareFunc(const long long& first, const long long& second)
 {
   return first > second;
 }
 
+bool compareDict(const std::string& first, const std::string& second)
+{
+  return first.size() > second.size();
+}
+
 int main(int argc, char* argv[])
 {
   using namespace potapova;
-  std::istream* in_ptr = nullptr;
   std::ifstream input_file;
   if (argc > 2)
   {
@@ -28,23 +33,29 @@ int main(int argc, char* argv[])
       std::cerr << "Failed to open file\n";
       return 1;
     }
-    in_ptr = &input_file;
   }
   else
   {
     std::cerr << "No file to open\n";
     return 1;
   }
-  Dictionary< long long, std::string, compareFunc > dict;
-  std::string in_dict;
+  std::string name;
+  Dictionary< std::string, Dictionary< long long, std::string, compareFunc >, compareDict > variables;
+  std::string command;
   try
   {
-    inputDict(*in_ptr, dict, in_dict);
+    while (!input_file.eof())
+    {
+      std::cin >> name;
+      std::cin >> variables[name];
+    }
+    std::cin >> command;
+    runCommand(command, variables);
   }
   catch (const std::exception& e)
   {
     std::cerr << "Error: " << e.what() << '\n';
     return 1;
-  }
+  }  
   return 0;
 }
