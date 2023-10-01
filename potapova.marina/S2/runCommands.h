3051baa85f1b8f7298527bea5933b1fe59113eab
Потапова <marina-potapova04@mail.ru>
2023-10-01 19:46:49 +0300

    [f] Fix CG

--- potapova.marina/S2/runCommands.h
+++ potapova.marina/S2/runCommands.h
@@ -5,8 +5,10 @@
 
 namespace potapova
 {
-  template< bool (*compareFunc)(const long long&, const long long&), bool (*compareDict)(const std::string& first, const std::string& second) >
-  void runCommand(std::string& command, Dictionary< std::string, Dictionary< long long, std::string, compareFunc >, compareDict > variables);
+  template< bool (*compareFunc)(const long long&, const long long&),
+      bool (*compareDict)(const std::string& first, const std::string& second) >
+  void runCommand(std::string& command,
+      Dictionary< std::string, Dictionary< long long, std::string, compareFunc >, compareDict > variables);
 }
 
 #endif
