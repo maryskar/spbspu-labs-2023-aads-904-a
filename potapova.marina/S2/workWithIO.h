27cb2001683138421d6346576413f29394fb5e9d
Потапова <marina-potapova04@mail.ru>
2023-10-01 22:37:41 +0300

    [+] Add noexcept

--- potapova.marina/S2/workWithIO.h
+++ potapova.marina/S2/workWithIO.h
@@ -1,13 +1,31 @@
-#ifndef WORKWITHIO_H
-#define WORKWITHIO_H
+#ifndef WORKWITHIO
+#define WORKWITHIO
 
 #include <istream>
 #include <ostream>
 #include <string>
 #include "dictionary.h"
 
-static const char* parseKey(const char* cur_sym_ptr, long long& key);
-static const char* parseValue(const char* cur_sym_ptr, std::string& value);
+static const char* parseKey(const char* cur_sym_ptr, long long& key) noexcept
+{
+  char* end_conv_ptr = nullptr;
+  key = std::strtoll(cur_sym_ptr, &end_conv_ptr, 10);
+  if (errno == ERANGE || (*end_conv_ptr != ' ' && *end_conv_ptr != '\0'))
+  {
+    return nullptr;
+  }
+  return end_conv_ptr;
+}
+
+static const char* parseValue(const char* cur_sym_ptr, std::string& value) noexcept
+{
+  while (!(*cur_sym_ptr == ' ' || *cur_sym_ptr == '\0'))
+  {
+    value.push_back(*cur_sym_ptr);
+    ++cur_sym_ptr;
+  }
+  return cur_sym_ptr;
+}
 
 namespace potapova
 {
