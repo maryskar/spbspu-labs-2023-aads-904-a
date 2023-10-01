9f34b5b5b8b406b91dc5850e6131c8a166d2da13
timofeev <maks.timofeev9834@gmail.com>
2023-10-01 07:03:47 +0300

    new dict

--- timofeev.maxim/S2/Dictionary.h
+++ timofeev.maxim/S2/Dictionary.h
@@ -1,6 +1,8 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
-#include "ForwardList.h"
+#include <functional>
+#include <utility>
+#include "forwardList.h"
 namespace timofeev
 {
   template< typename Key, typename Value, typename Compare = std::less< Key > >
@@ -11,107 +13,62 @@ namespace timofeev
     using iter = ForwardIterator< value_type >;
     using constIter = ForwardConstIterator< value_type >;
 
-    void push(Key k, Value v);
-
     Dictionary();
+    Dictionary(const Dictionary< Key, Value, Compare > &other);
+    Dictionary(Dictionary< Key, Value, Compare > &&other);
     ~Dictionary() = default;
 
-    Dictionary(const Dictionary &other);
-    Dictionary(Dictionary &&other) noexcept;
-
-    Dictionary< Key, Value, Compare > &operator=(const Dictionary &other);
-    Dictionary< Key, Value, Compare > &operator=(Dictionary &&other) noexcept;
+    Dictionary< Key, Value, Compare > &operator=(const Dictionary< Key, Value, Compare > &other);
+    Dictionary< Key, Value, Compare > &operator=(Dictionary< Key, Value, Compare > &&other);
 
     Value &at(const Key &key);
-    const Value& at(const Key& key) const;
-    Value& operator[](Key&& key);
+    const Value &at(const Key &key) const;
     Value &operator[](const Key &key);
+    Value &operator[](Key &&key);
 
-    iter begin();
-    constIter begin() const;
+    iter begin() noexcept;
+    constIter begin() const noexcept;
     constIter cbegin() const noexcept;
-
-    iter end();
-    constIter end() const;
+    iter end() noexcept;
+    constIter end() const noexcept;
     constIter cend() const noexcept;
 
+    std::pair< iter, bool > insert(const value_type &value);
+    std::pair< iter, bool > insert(const value_type &&value);
     iter insert(value_type &&value);
-    iter insert(const value_type &value);
-    iter insert( constIter pos, const value_type& value ); //
-    template< class P >
-    iter insert( constIter pos, P&& value );
-    template< typename InputIt >
-    void insert(InputIt first, InputIt last);
+    iter insert(constIter pos, const value_type &value );
+    template< typename P >
+    std::pair< iter, bool > insert(P &&value);
+    template< typename Input >
+    void insert(Input first, Input last);
 
     iter erase(iter pos);
     iter erase(constIter pos);
     iter erase(iter first, iter last);
     iter erase(constIter first, constIter last);
 
-    void swap(Dictionary< Key, Value, Compare > &other) noexcept;
-
-    size_t count(const Key &key) const;
-    iter find(const Key &key);
-    constIter find(const Key &key) const;
-    bool contains(const Key &key) const;
-
     bool empty() const noexcept;
     size_t size() const noexcept;
     void clear();
 
+    void swap(Dictionary< Key, Value, Compare > &other);
+
+    iter find(const Key &key);
+    constIter find(const Key &key) const;
+    size_t count(const Key &key) const;
+    bool contains(const Key &key) const;
+
+    void push(Key k, Value v);
   private:
     ForwardList< value_type > data_;
     Compare compare_;
-    size_t size_d;
+    size_t size_;
   };
 
-  template< typename Key, typename Value, typename Compare >
-  template< typename P >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary<Key, Value, Compare >::insert(constIter it, P &&value)
+  template<typename Key, typename Value, typename Compare>
+  void Dictionary<Key, Value, Compare>::push(Key k, Value v)
   {
-    auto existing = find(value.first);
-    if (existing != end())
-    {
-      return existing;
-    }
-    else
-    {
-      auto inserted = data_.insert(it.current(), std::forward<P>(value));
-      size_d++;
-      return typename Dictionary< Key, Value, Compare >::iter(inserted);
-    }
-  }
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary<Key, Value,
-    Compare>::insert(Dictionary::constIter pos, const value_type &value)
-  {
-    auto existing = find(value.first);
-    if (existing != end())
-    {
-      return existing;
-    }
-    else
-    {
-      auto inserted = data_.insert(pos.current(), value);
-      size_d++;
-      return typename Dictionary< Key, Value, Compare >::iter(inserted);
-    }
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  Value &Dictionary< Key, Value, Compare >::operator[](const Key &key)
-  {
-    try
-    {
-      return at(key);
-    }
-    catch (const std::out_of_range &e)
-    {
-      value_type newValue = std::make_pair(key, Value{});
-      data_.push_front(newValue);
-      size_d++;
-      return data_.front().second;
-    }
+    insert(std::make_pair(k,v));
   }
 
   template< typename Key, typename Value, typename Compare >
@@ -123,22 +80,22 @@ namespace timofeev
 
   template< typename Key, typename Value, typename Compare >
   typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
+    Compare >::erase(Dictionary::constIter pos)
+  {
+    return erase(constIter(pos));
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
     Compare >::erase(iter first, iter last)
   {
     while(first != last)
     {
       erase(first);
       first++;
-      size_d--;
+      size_--;
     }
     return iter(last.node_);
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
-    Compare >::erase(Dictionary::constIter pos)
-  {
-    return erase(constIter(pos));
   }
 
   template< typename Key, typename Value, typename Compare >
@@ -155,7 +112,7 @@ namespace timofeev
     {
       if (cur == pos)
       {
-        size_d--;
+        size_--;
         return data_.erase_after(prev);
       }
       cur++;
@@ -163,53 +120,19 @@ namespace timofeev
     }
     return end();
   }
-
+  
   template< typename Key, typename Value, typename Compare >
-  template<typename InputIt>
-  void Dictionary< Key, Value, Compare >::insert(InputIt first, InputIt last)
+  Value &Dictionary< Key, Value, Compare >::operator[](const Key &key)
   {
-    for (auto it = first; it != last; ++it)
-    {
-      insert(*it);
-    }
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::insert(const value_type &value)
-  {
-    if (begin() == end())
-    {
-      data_.push_front(value);
-      size_d++;
-      return begin();
-    }
-    else
-    {
-      auto it = find(value.first);
-      if (it == end())
-      {
-        data_.pushBack(value);
-        size_d++;
-        return begin();
-      }
-      else
-      {
-        it->second = value.second;
-        return it;
+    for(auto it = data_.begin(); it != data_.end(); ++it) {
+      if(it->first == key) {
+        return it->second;
       }
     }
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::insert(value_type &&value)
-  {
-    return insert(value);
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  void Dictionary< Key, Value, Compare >::push(Key k, Value v)
-  {
-    insert(value_type(k, v));
+    value_type newValue = std::make_pair(key, Value{});
+    data_.push_front(newValue);
+    size_++;
+    return data_.front().second;
   }
 
   template< typename Key, typename Value, typename Compare >
@@ -218,63 +141,65 @@ namespace timofeev
     return (*this)[key];
   }
 
-  template< typename Key, typename Value, typename Compare>
-  const Value &Dictionary< Key, Value, Compare >::at(const Key &key) const
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
+    Compare >::begin() noexcept
   {
-    for (auto it = cbegin(); it != cend(); it++)
-    {
-      if (!compare_(it->first, key) && !compare_(key, it->first))
-      {
-        return it->second;
-      }
-      else if (it == cend())
-      {
-        throw std::out_of_range("Out of range");
-      }
-    }
+    return data_.begin();
   }
 
   template< typename Key, typename Value, typename Compare >
-  Value &Dictionary< Key, Value, Compare >::at(const Key &key)
+  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value,
+    Compare >::cbegin() const noexcept
   {
-    iter it = find(key);
-    if (it == end())
-    {
-      throw std::out_of_range("no Key");
-    }
-    return it->second;
+    return data_.cbegin();
   }
 
   template< typename Key, typename Value, typename Compare >
-  bool Dictionary< Key, Value, Compare >::contains(const Key &key) const
+  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value,
+    Compare >::begin() const noexcept
   {
-    return count(key) > 0;
+    return cbegin();
   }
 
   template< typename Key, typename Value, typename Compare >
-  size_t Dictionary< Key, Value, Compare >::count(const Key &key) const
+  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
+    Compare >::end() noexcept
   {
-    auto it = find(key);
-    return (it != end()) ? 1ull : 0ull;
+    return data_.end();
   }
 
   template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::find(const Key &key) const
+  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value,
+    Compare >::cend() const noexcept
   {
-    auto it = begin();
-    while (it != end())
-    {
-      if (!compare_(it->first, key) && !compare_(key, it->first))
-      {
-        return it;
-      }
-      ++it;
-    }
+    return data_.cend();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value,
+    Compare >::end() const noexcept
+  {
     return cend();
   }
 
   template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::find(const Key &key)
+  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value,
+    Compare >::find(const Key &key) const
+  {
+    for(auto it = data_.cbegin(); it != data_.cend(); ++it)
+    {
+      if (!(compare_(it->first, key)) && !(compare_(key, it->first)))
+      {
+        return it;
+      }
+    }
+    return data_.cend();
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
+    Compare >::find(const Key &key)
   {
     for(auto it = data_.begin(); it != data_.end(); ++it)
     {
@@ -287,52 +212,24 @@ namespace timofeev
   }
 
   template< typename Key, typename Value, typename Compare >
-  void Dictionary< Key, Value, Compare >::swap(Dictionary< Key, Value, Compare > &other) noexcept
+  Value & Dictionary< Key, Value, Compare >::at(const Key &key)
   {
-    std::swap(data_, other.data_);
-    std::swap(compare_, other.compare_);
-    std::swap(size_d, other.size_d);
+    auto it = find(key);
+    if (it == data_.end())
+    {
+      throw std::out_of_range("out_of_range");
+    }
+    return it->second;
   }
 
   template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::cend() const noexcept
+  const Value & Dictionary< Key, Value, Compare >::at(const Key &key) const
   {
-    return data_.cend();
+    return const_cast< Dictionary< Key, Value, Compare >* >(this)->at(key);
   }
 
   template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::end() const
-  {
-    return cend();
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::end()
-  {
-    return data_.end();
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::cbegin() const noexcept
-  {
-    return data_.cbegin();
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::constIter Dictionary< Key, Value, Compare >::begin() const
-  {
-    return cbegin();
-  }
-
-  template< typename Key, typename Value, typename Compare >
-  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value, Compare >::begin()
-  {
-    return data_.begin();
-  }
-
-
-  template< typename Key, typename Value, typename Compare >
-  bool  Dictionary< Key, Value, Compare >::empty() const noexcept
+  bool Dictionary< Key, Value, Compare >::empty() const noexcept
   {
     return data_.empty();
   }
@@ -340,59 +237,179 @@ namespace timofeev
   template< typename Key, typename Value, typename Compare >
   size_t Dictionary< Key, Value, Compare >::size() const noexcept
   {
-    return size_d;
+    return size_;
   }
 
   template< typename Key, typename Value, typename Compare >
   void Dictionary< Key, Value, Compare >::clear()
   {
-    data_.Fclear();
-    size_d = 0;
+    data_.clear();
+    size_ = 0;
   }
 
-  template<typename Key, typename Value, typename Compare>
-  Dictionary<Key, Value, Compare> &Dictionary<Key, Value, Compare>::operator=(Dictionary &&other) noexcept
+  template< typename Key, typename Value, typename Compare >
+  void Dictionary< Key, Value, Compare >::swap(Dictionary< Key, Value, Compare > &other)
+  {
+    std::swap(data_, other.data_);
+    std::swap(compare_, other.compare_);
+    size_ = other.size_;
+    other.size_ = 0;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  size_t Dictionary< Key, Value, Compare >::count(const Key & key) const
+  {
+    auto it = find(key);
+    return (it != end()) ? 1ull : 0ull;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  bool Dictionary< Key, Value, Compare >::contains(const Key &key) const
+  {
+    return count(key) > 0;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  std::pair< typename Dictionary< Key, Value, Compare >::iter,
+    bool > Dictionary< Key, Value, Compare >::insert(const value_type &&value)
+  {
+    return insert(std::make_pair(std::move(value)));
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iter Dictionary< Key, Value,
+    Compare >::insert(value_type &&value)
+  {
+    return insert(value);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  std::pair< typename Dictionary< Key, Value, Compare >::iter,
+    bool > Dictionary< Key, Value, Compare >::insert(const value_type &value)
+  {
+    for(auto it = data_.begin(); it != data_.end(); ++it)
+    {
+      if(compare_(it->first, value.first))
+      {
+        continue;
+      }
+      else if(!compare_(it->first, value.first) && !compare_(value.first, it->first))
+      {
+        return {it, false};
+      }
+      else
+      {
+        auto tmp = data_.insert_after(it, value);
+        ++size_;
+        return {tmp, true};
+      }
+    }
+    data_.insert_after(end(), value);
+    ++size_;
+    return {data_.end(), true};
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  template< typename P >
+  std::pair< typename Dictionary< Key, Value, Compare >::iter, bool >
+  Dictionary<Key, Value, Compare>::insert(P &&value)
+  {
+    iter cur = data_.before_begin();
+    for (iter sup = begin(); sup != end(); sup++)
+    {
+      if (!compare_(value.first, sup->first) && !compare_(sup->first, value.first))
+      {
+        return std::make_pair(sup, false);
+      }
+      else if (compare_(value.first, sup->first))
+      {
+        iter inserted = data_.insert_after(cur, value);
+        size_++;
+        return std::make_pair(inserted, true);
+      }
+      cur++;
+    }
+    iter inserted = data_.insert_after(cur, value);
+    size_++;
+    return std::make_pair(inserted, true);
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  typename Dictionary< Key, Value, Compare >::iter
+  Dictionary< Key, Value, Compare >::insert(constIter pos, const value_type &value)
+  {
+    auto it = find(value.first);
+    if (it != end())
+    {
+      return it;
+    }
+    else
+    {
+      auto tmp = data_.insert(pos.node_, value);
+      size_++;
+      return iter(tmp);
+    }
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  template< typename Input >
+  void Dictionary< Key, Value, Compare >::insert(Input first, Input last)
+  {
+    auto it = first;
+    for (; it != last; ++it)
+    {
+      insert(*it);
+    }
+  }
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary(const Dictionary< Key, Value, Compare > &other):
+    data_(other.data_),
+    compare_(other.compare_),
+    size_(other.size())
+  {}
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare >::Dictionary(Dictionary< Key, Value, Compare > &&other):
+    data_(std::move(other.data_)),
+    compare_(std::move(other.compare_)),
+    size_(other.size_)
+  {
+    other.size_ = 0;
+  }
+
+  template< typename Key, typename Value, typename Compare >
+  Dictionary< Key, Value, Compare > & Dictionary< Key, Value,
+    Compare >::operator=(const Dictionary< Key, Value, Compare > &other)
   {
     if (this != &other)
     {
-      *this = other;
-    }
-    return *this;
-  }
-
-  template<typename Key, typename Value, typename Compare>
-  Dictionary<Key, Value, Compare> &Dictionary<Key, Value, Compare>::operator=(const Dictionary &other)
-  {
-    if (this != std::addressof(other))
-    {
       data_ = other.data_;
       compare_ = other.compare_;
-      size_d = other.size_d;
+      size_ = other.size_;
     }
     return *this;
   }
 
   template< typename Key, typename Value, typename Compare >
-  Dictionary< Key, Value, Compare >::Dictionary(Dictionary &&other) noexcept:
-    data_(std::move(other.data_)),
-    compare_(std::move(other.compare_)),
-    size_d(other.size_d)
+  Dictionary< Key, Value, Compare > & Dictionary< Key, Value,
+    Compare >::operator=(Dictionary< Key, Value, Compare > &&other)
   {
-    other.size_d = 0;
+    if (this != &other)
+    {
+      data_ = std::move(other.data_);
+      compare_ = std::move(other.compare_);
+      size_ = other.size_;
+      other.size_ = 0;
+    }
+    return *this;
   }
-
-  template< typename Key, typename Value, typename Compare >
-  Dictionary< Key, Value, Compare >::Dictionary(const Dictionary &other):
-    data_(other.data_),
-    compare_(other.compare_),
-    size_d(other.size_d)
-  {}
-
+  
   template< typename Key, typename Value, typename Compare >
   Dictionary< Key, Value, Compare >::Dictionary():
     data_(),
-    compare_(),
-    size_d(0)
+    compare_(Compare()),
+    size_(0)
   {}
+
 }
 #endif
