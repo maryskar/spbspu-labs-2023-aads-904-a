78343be9623e38851540fac0fca0de373ada150b
KiraGelaz <kiragelaz@yandex.ru>
2023-10-03 00:38:33 +0300

    fix smth

--- kryuchkova.kira/S2/forward_list_const_iterator.h
+++ kryuchkova.kira/S2/forward_list_const_iterator.h
@@ -42,21 +42,21 @@ namespace kryuchkova
   template < typename T >
   const T & ConstForwardIterator< T >::operator*() const
   {
-    assert(node_ != nullptr);
+    assert(node_->next_ != nullptr);
     return node_->data_;
   }
 
   template < typename T >
   const T * ConstForwardIterator< T >::operator->() const
   {
-    assert(node_->data_ != nullptr);
+    assert(node_->next_ != nullptr);
     return std::addressof(node_->data_);
   }
 
   template < typename T >
   ConstForwardIterator< T > & ConstForwardIterator< T >::operator++()
   {
-    assert(node_->data_ != nullptr);
+    assert(node_->next_ != nullptr);
     node_ = node_->next_;
     return *this;
   }
@@ -64,7 +64,7 @@ namespace kryuchkova
   template < typename T >
   ConstForwardIterator< T > ConstForwardIterator< T >::operator++(int)
   {
-    assert(node_->data_ != nullptr);
+    assert(node_->next_ != nullptr);
     this_t temp(*this);
     ++(*this);
     return temp;
