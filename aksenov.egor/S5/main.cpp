93aa33f3b2c3ed7eb1842bc2cb055b208e6acb2c
aksenovEA <aksenov200477@gmail.com>
2023-10-09 00:43:08 +0300

    function for overflow

--- aksenov.egor/S5/main.cpp
+++ aksenov.egor/S5/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <limits>
+
 
 int main()
 {
---
+++ aksenov.egor/common/checkOverflow.h
@@ -0,0 +1,9 @@
+#ifndef CHECKOVERFLOW_H
+#define CHECKOVERFLOW_H
+
+namespace aksenov
+{
+  using value = long long;
+  bool isOverflov(value op1, value, op2, char sign);
+}
+#endif
