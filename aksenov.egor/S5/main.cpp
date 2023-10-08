5b485159a5d1f57d20fc94f9c39b5695cd625f1c
aksenovEA <aksenov200477@gmail.com>
2023-10-09 01:04:33 +0300

    file check

--- aksenov.egor/S5/main.cpp
+++ aksenov.egor/S5/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <limits>
+#include <fstream>
+#include <string>
+#include <AVL/AVL.h>
 
-int main()
+int main(int args, char *argv[])
 {
-  std::cout << "empty" << "\n";
+  std::ifstream inp;
+  if (argc == 3)
+  {
+    inputFile.open(argv[2]);
+    if (!inputFile.is_open())
+    {
+      std::cerr << "Cannot open file\n";
+      return 1;
+    }
+  }
+  else
+  {
+    std::cerr << "Wrong number of args\n";
+    return 1;
+  }
+
 }
