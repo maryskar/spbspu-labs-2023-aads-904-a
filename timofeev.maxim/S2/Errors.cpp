bedbc246f86c7919982e6888fbe393d97b1f8fa1
timofeev <maks.timofeev9834@gmail.com>
2023-10-15 21:13:45 +0300

    fix Errors

--- timofeev.maxim/S2/Errors.cpp
+++ timofeev.maxim/S2/Errors.cpp
@@ -3,10 +3,10 @@ namespace errors
 {
   void printInvalid(std::ostream& out)
   {
-    out << "<INVALID COMMAND>" << '\n';
+    out << "<INVALID COMMAND>";
   }
   void printEmpty(std::ostream& out)
   {
-    out << "<EMPTY>" << '\n';
+    out << "<EMPTY>";
   }
 }
