83f066fbc36f4d1c74b6c8a3bca4cc92a092e696
ArsDi <abstractlevl@gmail.com>
2023-10-23 14:02:12 +0300

    namespace adding

--- dmitriev.arseny/S6/fillDequeData.h
+++ dmitriev.arseny/S6/fillDequeData.h
@@ -4,38 +4,40 @@
 #include <random>
 #include <deque>
 
-template< typename T >
-std::deque< T > fiillDequeRandom(size_t count)
+namespace dmitriev
 {
-  std::random_device rd;
-  std::mt19937 mt(rd());
-
-  std::deque< T >  newData;
-
-  if (std::is_same< T, int >::value)
+  template< typename T >
+  std::deque< T > fiillDequeRandom(size_t count)
   {
-    std::uniform_int_distribution< int > dist(0, 1000);
-    for (size_t i = 0; i < count; i++)
+    std::random_device rd;
+    std::mt19937 mt(rd());
+
+    std::deque< T >  newData;
+
+    if (std::is_same< T, int >::value)
     {
-      newData.push_back(dist(mt));
+      std::uniform_int_distribution< int > dist(0, 1000);
+      for (size_t i = 0; i < count; i++)
+      {
+        newData.push_back(dist(mt));
+      }
     }
-  }
-  else if (std::is_same< T, float >::value)
-  {
-    std::uniform_real_distribution< float > dist(0.0, 1000.0);
-    for (size_t i = 0; i < count; i++)
+    else if (std::is_same< T, float >::value)
     {
-      newData.push_back(dist(mt));
+      std::uniform_real_distribution< float > dist(0.0, 1000.0);
+      for (size_t i = 0; i < count; i++)
+      {
+        newData.push_back(dist(mt));
+      }
     }
-  }
-  //else
-  //{
-  //  throw std::invalid_argument("incorrect data type");
-  //}
+    //else
+    //{
+    //  throw std::invalid_argument("incorrect data type");
+    //}
 
 
-  return newData;
+    return newData;
+  }
 }
-
 
 #endif
