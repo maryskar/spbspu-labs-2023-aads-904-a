16061a8f81fb38afd7d19261fd28ed2e28d23d42
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-10-31 14:42:02 +0300

    Make projection on capacity for hash function

--- fesenko.ivan/FA/hash.cpp
+++ fesenko.ivan/FA/hash.cpp
@@ -1,6 +1,13 @@
 #include "hash.h"
 
-uint32_t fesenko::generate_jenkins_hash(const std::string key)
+uint32_t makeHashInCapacity(uint32_t hash, uint32_t capacity)
+{
+  uint64_t capacity64 = static_cast< uint64_t >(capacity);
+  uint64_t result = static_cast< uint64_t >(hash) * capacity64;
+  return static_cast< uint32_t >((result >> 32) + 1) - 1;
+}
+
+uint32_t fesenko::generate_jenkins_hash(const std::string key, uint32_t capacity)
 {
   size_t len = key.size();
   uint32_t hash = 0;
@@ -12,5 +19,5 @@ uint32_t fesenko::generate_jenkins_hash(const std::string key)
   hash += (hash << 3);
   hash ^= (hash >> 11);
   hash += (hash << 15);
-  return hash;
+  return makeHashInCapacity(hash, capacity);
 }
--- fesenko.ivan/FA/hash.h
+++ fesenko.ivan/FA/hash.h
@@ -4,6 +4,6 @@
 #include <cstdint>
 namespace fesenko
 {
-  uint32_t generate_jenkins_hash(const std::string key);
+  uint32_t generate_jenkins_hash(const std::string key, uint32_t capacity);
 }
 #endif
