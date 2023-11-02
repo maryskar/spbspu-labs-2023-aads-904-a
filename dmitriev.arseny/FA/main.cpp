093150cc1b42d5b3cf8bc9beca781361da390107
ArsDi <abstractlevl@gmail.com>
2023-11-02 22:14:26 +0300

    namespace adding

--- dmitriev.arseny/FA/main.cpp
+++ dmitriev.arseny/FA/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "unorderedMap.h"
 
 int main()
 {
--- dmitriev.arseny/FA/unorderedMap.h
+++ dmitriev.arseny/FA/unorderedMap.h
@@ -2,28 +2,34 @@
 #define UNORDEREDMAP_H
 
 #include <forwardList.h>
+#include "mapNode.h"
 
-template < typename Key, typename Value, typename Compare = std::equal_to< Key >, typename Hash = std::hash< Key > >
-class UnorderedMap
+namespace dmitriev
 {
-public:
-  using dataPair = std::pair< const Key, Value >;
-  using dataType = MapNode< Key, Value >;
-  using fList = dmitriev::ForwardList< dataType >;
-  using iterator = typename fList::iterator;
-  using constIterator = typename fList::constIterator;
+  template < typename Key, typename Value, typename Compare = std::equal_to< Key >, typename Hash = std::hash< Key > >
+  class UnorderedMap
+  {
+  public:
+    using dataPair = std::pair< const Key, Value >;
+    using dataType = MapNode< Key, Value >;
+    using fList = ForwardList< dataType >;
+    using iterator = typename fList::iterator;
+    using constIterator = typename fList::constIterator;
 
 
-private:
-  fList m_values;
+  private:
+    fList m_values;
 
-  size_t m_size;
-  size_t m_capacity;
-  iterator* m_arr;
+    size_t m_size;
+    size_t m_capacity;
+    iterator* m_arr;
 
-  Compare m_cmp;
-  Hash m_hash;
+    Compare m_cmp;
+    Hash m_hash;
 
+  };
 }
+
+
 
 #endif
