e14443af59354ae283fa0839df00a38bc082aab1
Ivan Fesenko <fesenko.ivan2004@mail.ru>
2023-11-04 16:19:38 +0300

    Add operator[]

--- fesenko.ivan/FA/hashTable.h
+++ fesenko.ivan/FA/hashTable.h
@@ -3,13 +3,16 @@
 #include <vector>
 #include <string>
 #include "wordType.h"
+#include "hash.h"
 namespace fesenko
 {
   class HashTable
   {
    public:
     using this_t = HashTable;
-    using data_t = std::vector< WordType >;
+    using key_type = std::string;
+    using mapped_type = WordType;
+    using data_t = std::vector< mapped_type >;
     HashTable();
     HashTable(const this_t &) = default;
     HashTable(this_t &&);
@@ -19,6 +22,8 @@ namespace fesenko
     bool empty() const noexcept;
     size_t size() const noexcept;
     size_t max_size() const noexcept;
+    mapped_type &operator[](const key_type &);
+    mapped_type &operator[](key_type &&);
    private:
     size_t size_;
     size_t capacity_;
@@ -75,6 +80,17 @@ namespace fesenko
   size_t HashTable::max_size() const noexcept
   {
     return capacity_;
+  }
+
+  WordType &HashTable::operator[](const key_type &key)
+  {
+    uint32_t index = generate_jenkins_hash(key, capacity_);
+    return data_[index];
+  }
+
+  WordType &HashTable::operator[](key_type &&key)
+  {
+    return (*this)[key];
   }
 }
 #endif
